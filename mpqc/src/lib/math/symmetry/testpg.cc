4248966e76fb4f41b85a7b7d7bb486fec33e4418
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1994-06-15 16:47:06 +0000

    add some new stuff
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%40167

--- mpqc/src/lib/math/symmetry/testpg.cc
+++ mpqc/src/lib/math/symmetry/testpg.cc
@@ -5,5 +5,11 @@ main(int argc, char *argv[])
 {
   PointGroup pg(argv[1]);
 
-  pg.char_table().print();
+  //pg.char_table().print();
+
+  CharacterTable ct = pg.char_table();
+  CharacterTable ct2;
+  ct2=ct;
+
+  ct.print();
 }

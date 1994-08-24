e7bf027727477cd6490818697dd0fcb7016c43b1
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1994-08-24 19:45:38 +0000

    include math/symmetry/LIBS.h
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%40276

--- mpqc/src/lib/chemistry/qc/dmtsym/LIBS.h
+++ mpqc/src/lib/chemistry/qc/dmtsym/LIBS.h
@@ -1,5 +1,6 @@
 chemistry/qc/dmtsym/libdmtsym.a
 #include <math/array/LIBS.h>
 #include <math/dmt/LIBS.h>
+#include <math/symmetry/LIBS.h>
 #include <chemistry/qc/intv2/LIBS.h>
 #include <util/sgen/LIBS.h>

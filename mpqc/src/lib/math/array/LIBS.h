8abae38b24e3bf62aef833d5d605e30497a5904b
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1994-08-26 23:42:34 +0000

    bio no longer exits
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%40329

--- mpqc/src/lib/math/array/LIBS.h
+++ mpqc/src/lib/math/array/LIBS.h
@@ -1,3 +1,2 @@
 math/array/libarray.a
-#include <util/bio/LIBS.h>
 #include <util/sgen/LIBS.h>
--- mpqc/src/lib/util/sgen/LIBS.h
+++ mpqc/src/lib/util/sgen/LIBS.h
@@ -1,4 +1,3 @@
 util/sgen/libsgen.a
 #include <comm/picl/ext/LIBS.h>
 #include <util/keyval/LIBS.h>
-#include <util/bio/LIBS.h>

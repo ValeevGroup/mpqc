021c0de748da5fda412bae0509d3145c38dd268f
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1994-11-19 02:09:19 +0000

    no_two_electron breaks mpqcic
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%40548

--- mpqc/src/lib/chemistry/qc/basis/LIBS.h
+++ mpqc/src/lib/chemistry/qc/basis/LIBS.h
@@ -1,7 +1,5 @@
 chemistry/qc/basis/libbasis.a
 #include <chemistry/molecule/LIBS.h>
-#define NO_TWO_ELECTRON
 #include <chemistry/qc/integral/LIBS.h>
-#undef NO_TWO_ELECTRON
 #include <math/scmat/LIBS.h>
 #include <util/container/LIBS.h>

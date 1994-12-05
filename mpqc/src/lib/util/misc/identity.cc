fcbdab26eb9dd68554caa35c6af7896e56e857c9
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1994-12-05 17:50:15 +0000

    Added the Identity base class which permits portable ordering and comparison of objects.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%40555

---
+++ mpqc/src/lib/util/misc/identity.cc
@@ -0,0 +1,11 @@
+
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include <util/misc/identity.h>
+
+Identity::~Identity()
+{
+}
+
---
+++ mpqc/src/lib/util/misc/identity.h
@@ -0,0 +1,54 @@
+
+#ifndef _util_misc_identity_h
+#define _util_misc_identity_h
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+// Identifier's are used to distinguish and order objects.  On
+// many architectures a pointer to the object will suffice, but
+// the C++ standard only guarantees that this works for two pointers
+// pointing within the same structure or array.  Classes need to
+// inherit from Identity to use this mechanism.  Identity,
+// Identifier, and the shorthand boolean operations may have to
+// be modified for some different architectures.
+
+class Identity;
+class Identifier {
+  private:
+    const void* id;
+  public:
+    Identifier(): id(0) {}
+    Identifier(const Identity* i): id((void*)i) {}
+    Identifier(const Identifier& i): id(i.id) {}
+    operator = (const Identifier& i) { id = i.id; }
+    ~Identifier() {}
+    
+    operator < (const Identifier&i) const { return id < i.id; }
+    operator > (const Identifier&i) const { return id > i.id; }
+    operator == (const Identifier&i) const { return id == i.id; }
+    operator <= (const Identifier&i) const { return id <= i.id; }
+    operator >= (const Identifier&i) const { return id >= i.id; }
+    operator != (const Identifier&i) const { return id != i.id; }
+};
+
+class Identity {
+  public:
+    virtual ~Identity();
+    Identifier identifier() { return this; }
+};
+
+// shorthand boolean operation for pointer arguments
+inline int lt(const Identity*i, const Identity*j) { return i < j; }
+inline int gt(const Identity*i, const Identity*j) { return i > j; }
+inline int le(const Identity*i, const Identity*j) { return i <= j; }
+inline int ge(const Identity*i, const Identity*j) { return i >= j; }
+inline int eq(const Identity*i, const Identity*j) { return i == j; }
+inline int ne(const Identity*i, const Identity*j) { return i != j; }
+inline int cmp(const Identity*i, const Identity*j)
+{
+  return (i==j)?0:((i<j)?-1:1);
+}
+
+#endif

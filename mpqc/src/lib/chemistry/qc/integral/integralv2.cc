0518e74076a4bf2d7ff98548dd89f6a9d4e9f33b
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1995-03-05 06:02:56 +0000

    Added the GaussianDipoleIntv2 and GaussianEfieldDotVectorIntv2 operations.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%40690

--- mpqc/src/lib/chemistry/qc/integral/integralv2.cc
+++ mpqc/src/lib/chemistry/qc/integral/integralv2.cc
@@ -7,7 +7,10 @@
 #include "integral.h"
 #include "integralv2.h"
 
-OneBodyIntv2::OneBodyIntv2(const GaussianBasisSet*bs,const Molecule*mol):
+////////////////////////////////////////////////////////////////////////////
+// OneBodyIntv2
+
+OneBodyIntv2::OneBodyIntv2(const RefGaussianBasisSet&bs,const RefMolecule&mol):
   OneBodyInt(bs)
 {
   c = bs->convert_to_centers_t(mol);
@@ -24,7 +27,31 @@ OneBodyIntv2::~OneBodyIntv2()
   free(c);
 }
 
-GaussianOverlapIntv2::GaussianOverlapIntv2(const GaussianBasisSet*bs_):
+////////////////////////////////////////////////////////////////////////////
+// OneBody3Intv2
+
+OneBody3Intv2::OneBody3Intv2(const RefGaussianBasisSet&bs,
+                             const RefMolecule&mol):
+  OneBody3Int(bs)
+{
+  c = bs->convert_to_centers_t(mol);
+  int_normalize_centers(c);
+  int_initialize_1e(0,0,c,c);
+  int_initialize_offsets1(c,c);
+}
+
+OneBody3Intv2::~OneBody3Intv2()
+{
+  int_done_offsets1(c,c);
+  int_done_1e();
+  free_centers(c);
+  free(c);
+}
+
+////////////////////////////////////////////////////////////////////////////
+// GaussianOverlapIntv2
+
+GaussianOverlapIntv2::GaussianOverlapIntv2(const RefGaussianBasisSet&bs_):
   OneBodyIntv2(bs_,0)
 {
 }
@@ -38,8 +65,11 @@ void GaussianOverlapIntv2::compute_shell(int i,int j,double*buf)
   int_shell_overlap(c,c,buf,i,j);
 }
 
-GaussianKineticIntv2::GaussianKineticIntv2(const GaussianBasisSet*bs_,
-                                           const Molecule*m):
+////////////////////////////////////////////////////////////////////////////
+// GaussianKineticIntv2
+
+GaussianKineticIntv2::GaussianKineticIntv2(const RefGaussianBasisSet&bs_,
+                                           const RefMolecule&m):
   OneBodyIntv2(bs_,m)
 {
 }
@@ -53,9 +83,12 @@ void GaussianKineticIntv2::compute_shell(int i,int j,double*buf)
   int_shell_kinetic(c,c,buf,i,j);
 }
 
+////////////////////////////////////////////////////////////////////////////
+// GaussianPointChargeIntv2
+
 GaussianPointChargeIntv2::GaussianPointChargeIntv2(PointBag_double*charges,
-						   const GaussianBasisSet*bs_,
-						   const Molecule*m):
+						   const RefGaussianBasisSet&bs_,
+						   const RefMolecule&m):
   OneBodyIntv2(bs_,m)
 {
   ncharge = charges->length();
@@ -93,12 +126,113 @@ void GaussianPointChargeIntv2::compute_shell(int i,int j,double*buf)
   int_shell_point_charge(c,c,buf,i,j,ncharge,charge,position);
 }
 
-GaussianNuclearIntv2::GaussianNuclearIntv2(const GaussianBasisSet*bs_,
-				           const Molecule*m):
+////////////////////////////////////////////////////////////////////////////
+// GaussianNuclearIntv2
+
+GaussianNuclearIntv2::GaussianNuclearIntv2(const RefGaussianBasisSet&bs_,
+				           const RefMolecule&m):
 GaussianPointChargeIntv2(m->charges(),bs_,m)
 {
 }
 
 GaussianNuclearIntv2::~GaussianNuclearIntv2()
 {
+}
+
+////////////////////////////////////////////////////////////////////////////
+// GaussianEfieldDotVectorIntv2
+
+GaussianEfieldDotVectorIntv2::GaussianEfieldDotVectorIntv2(
+    const RefGaussianBasisSet&bs_,
+    const RefMolecule&m,
+    double *p,
+    double *v):
+  OneBodyIntv2(bs_,m)
+{
+  int biggest_shell = bs_->max_nfunction_in_shell();
+  if (biggest_shell) {
+      buffer3_ = new double[biggest_shell * biggest_shell * 3];
+    }
+  else {
+      buffer3_ = 0;
+    }
+  if (p) position(p);
+  if (v) vector(v);
+}
+
+GaussianEfieldDotVectorIntv2::~GaussianEfieldDotVectorIntv2()
+{
+  if (buffer3_) delete[] buffer3_;
+}
+
+void
+GaussianEfieldDotVectorIntv2::position(const double *a)
+{
+  for (int i=0; i<3; i++) position_[i] = a[i];
+}
+
+void
+GaussianEfieldDotVectorIntv2::vector(const double *a)
+{
+  for (int i=0; i<3; i++) vector_[i] = a[i];
+}
+
+void
+GaussianEfieldDotVectorIntv2::compute_shell(int i,int j,double*buf)
+{
+  int nbfi = basis()->operator()(i).nfunction();
+  int nbfj = basis()->operator()(j).nfunction();
+  int nint = nbfi*nbfj;
+  int nint3 = nint*3;
+  double *tmp;
+  int ii,jj;
+
+  tmp = buffer3_;
+  for (ii=0;ii<nint3;ii++) *tmp++ = 0.0;
+
+  int_accum_shell_efield(c,c,buffer3_,i,j,position_);
+
+  tmp = buffer3_;
+  for (ii=0; ii<nint; ii++) {
+      buf[ii] = 0.0;
+      for (jj=0; jj<3; jj++) {
+          buf[ii] += *tmp++ * vector_[jj];
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////
+// GaussianDipoleIntv2
+
+GaussianDipoleIntv2::GaussianDipoleIntv2(const RefGaussianBasisSet&bs_,
+                                         const RefMolecule&m,
+                                         const double* o):
+  OneBody3Intv2(bs_,m)
+{
+  if (o) origin(o);
+}
+
+GaussianDipoleIntv2::~GaussianDipoleIntv2()
+{
+}
+
+void
+GaussianDipoleIntv2::origin(const double *a)
+{
+  for (int i=0; i<3; i++) origin_[i] = a[i];
+}
+
+void
+GaussianDipoleIntv2::compute_shell(int i,int j,double*buf)
+{
+  int nbfi = basis()->operator()(i).nfunction();
+  int nbfj = basis()->operator()(j).nfunction();
+  int nint3 = nbfi*nbfj*3;
+  double *tmp;
+  int ii;
+
+  tmp = buf;
+  for (ii=0;ii<nint3;ii++) *tmp++ = 0.0;
+
+  int_accum_shell_dipole(c,c,buf,i,j,origin_);
 }
--- mpqc/src/lib/chemistry/qc/integral/integralv2.h
+++ mpqc/src/lib/chemistry/qc/integral/integralv2.h
@@ -13,8 +13,18 @@ class OneBodyIntv2:
  protected:
   struct struct_centers* c;
  public:
-  OneBodyIntv2(const GaussianBasisSet*,const Molecule*);
+  OneBodyIntv2(const RefGaussianBasisSet&,const RefMolecule&);
   ~OneBodyIntv2();
+};
+
+class OneBody3Intv2:
+  public OneBody3Int
+{
+ protected:
+  struct struct_centers* c;
+ public:
+  OneBody3Intv2(const RefGaussianBasisSet&,const RefMolecule&);
+  ~OneBody3Intv2();
 };
 
 class GaussianOverlapIntv2:
@@ -22,7 +32,7 @@ class GaussianOverlapIntv2:
 {
  private:
  public:
-  GaussianOverlapIntv2(const GaussianBasisSet*);
+  GaussianOverlapIntv2(const RefGaussianBasisSet&);
   ~GaussianOverlapIntv2();
   void compute_shell(int,int,double*);
 };
@@ -32,7 +42,7 @@ class GaussianKineticIntv2:
 {
  private:
  public:
-  GaussianKineticIntv2(const GaussianBasisSet*,const Molecule*);
+  GaussianKineticIntv2(const RefGaussianBasisSet&,const RefMolecule&);
   ~GaussianKineticIntv2();
   void compute_shell(int,int,double*);
 };
@@ -45,7 +55,7 @@ class GaussianPointChargeIntv2:
   double** position;
   double* charge;
  public:
-  GaussianPointChargeIntv2(PointBag_double*,const GaussianBasisSet*,const Molecule*);
+  GaussianPointChargeIntv2(PointBag_double*,const RefGaussianBasisSet&,const RefMolecule&);
   ~GaussianPointChargeIntv2();
   void compute_shell(int,int,double*);
 };
@@ -54,8 +64,38 @@ class GaussianNuclearIntv2:
   public GaussianPointChargeIntv2
 {
  public:
-  GaussianNuclearIntv2(const GaussianBasisSet*,const Molecule*);
+  GaussianNuclearIntv2(const RefGaussianBasisSet&,const RefMolecule&);
   ~GaussianNuclearIntv2();
+};
+
+class GaussianEfieldDotVectorIntv2: public OneBodyIntv2
+{
+  private:
+    double *buffer3_; // a larger buffer is needed than that provided
+    double position_[3];
+    double vector_[3];
+  public:
+    GaussianEfieldDotVectorIntv2(const RefGaussianBasisSet&,
+                                 const RefMolecule&,
+                                 double *postion = 0,
+                                 double *vector = 0);
+    ~GaussianEfieldDotVectorIntv2();
+    void position(const double*);
+    void vector(const double*);
+    void compute_shell(int,int,double*);
+};
+
+class GaussianDipoleIntv2: public OneBody3Intv2
+{
+  private:
+    double origin_[3];
+  public:
+    GaussianDipoleIntv2(const RefGaussianBasisSet&,
+                        const RefMolecule&,
+                        const double *origin = 0);
+    ~GaussianDipoleIntv2();
+    void origin(const double*);
+    void compute_shell(int,int,double*);
 };
 
 #endif

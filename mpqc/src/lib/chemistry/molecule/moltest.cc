794ea73a0a0092bb501b1fb7f1ed44cbf89fe574
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1995-06-08 22:57:27 +0000

    Got the test program working again.  Added more test cases to the input.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%40799

--- mpqc/src/lib/chemistry/molecule/moltest.cc
+++ mpqc/src/lib/chemistry/molecule/moltest.cc
@@ -26,16 +26,16 @@ main()
 
   RefMolecule mol = new Molecule(pkv);
 
-  mol_cleanup_molecule(*mol);
+  mol_cleanup_molecule(mol);
   printf("Clean Molecule:\n");
   mol->print();
 
-  mol_transform_to_principal_axes(*mol);
+  mol_transform_to_principal_axes(mol);
   printf("Clean Molecule wrt principal axes:\n");
   mol->print();
 
-  int nunique = mol_num_unique_atoms(*mol);
-  int * unique_atoms = mol_find_unique_atoms(*mol);
+  int nunique = mol_num_unique_atoms(mol);
+  int * unique_atoms = mol_find_unique_atoms(mol);
 
   printf("nunique=%d: ",nunique);
   for (int i=0; i < nunique; i++) printf(" %d",unique_atoms[i]+1);
@@ -45,8 +45,9 @@ main()
 
   RefRender ren = kv->describedclassvalue("renderer");
   RefRenderedObject renmol = kv->describedclassvalue("renderedmolecule");
-  ren->render(renmol);
-exit(0);
+  if (ren.nonnull() && renmol.nonnull()) ren->render(renmol);
+
+  //exit(0);
 
   printf("getting simp:\n");
   RefKeyVal ppkv = new PrefixKeyVal("simp",kv);
@@ -72,30 +73,46 @@ exit(0);
   cout << "difference between test and finite displacement bmatrix:\n";
   (fd_bmatrix - bmatrix).print();
 
+  cout.flush();
+  cerr.flush();
+  fflush(stdout);
+  fflush(stderr);
+  
   // now we get ambitious
   RefMolecularCoor mc = kv->describedclassvalue("molcoor");
 
+  cout.flush();
+  cerr.flush();
+  fflush(stdout);
+  fflush(stderr);
+
   mc->print();
 
-  do_displacement(mc,0);
-  do_displacement(mc,1);
-  do_displacement(mc,2);
-  do_displacement(mc,3);
+  cout.flush();
+  cerr.flush();
+  fflush(stdout);
+  fflush(stderr);
+
+//   do_displacement(mc,0);
+//   do_displacement(mc,1);
+//   do_displacement(mc,2);
+//   do_displacement(mc,3);
 
   RefSymmSCMatrix hessian(mc->dim());
   mc->guess_hessian(hessian);
 
-  cout << "The guess hessian:\n";
-  hessian.print();
+//   cout << "The guess hessian:\n";
+//   hessian.print();
 
   RefMolecularEnergy me = kv->describedclassvalue("energy");
-  me->print();
+  if (me.nonnull()) me->print();
 }
 
 
 void
 do_displacement(RefMolecularCoor&mc,int i)
 {
+  if (i>=mc->dim().n()) return;
   // now try to displace the geometry
   RefSCVector internal(mc->dim());
   mc->to_internal(internal);

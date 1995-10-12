a5db61e5bd9a7d9cf38c1ac645b9244b8d0bec91
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1995-10-12 20:39:56 +0000

    Remove obsolete spcsscf.gbl.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%40913

--- mpqc/src/lib/chemistry/qc/force/libforce.h
+++ mpqc/src/lib/chemistry/qc/force/libforce.h
@@ -4,6 +4,5 @@
 #include <chemistry/qc/force/read.h>
 #include <chemistry/qc/force/mpcsscf.gbl>
 #include <chemistry/qc/force/mposscf.gbl>
-#include <chemistry/qc/force/spcsscf.gbl>
 
 #endif /* _libforce_h */

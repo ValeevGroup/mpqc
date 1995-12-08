7d410ae089c20b8defd3bfc4f4b6e3bb01ef92a8
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1995-12-08 16:29:59 +0000

    add petite list check
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401015

--- mpqc/src/lib/chemistry/qc/basis/btest.cc
+++ mpqc/src/lib/chemistry/qc/basis/btest.cc
@@ -3,6 +3,8 @@
 #include <util/keyval/keyval.h>
 #include <chemistry/molecule/molecule.h>
 #include <chemistry/qc/basis/basis.h>
+#include <chemistry/qc/basis/petite.h>
+#include <chemistry/qc/integral/integralv2.h>
 
 int
 main(int, char**)
@@ -11,6 +13,34 @@ main(int, char**)
 
   for (int i=0; i<keyval->count("test"); i++) {
       RefGaussianBasisSet gbs = keyval->describedclassvalue("test", i);
+
+      PetiteList pl(gbs);
+      pl.print();
+      
+      RefSCMatrix aotoso = pl.aotoso();
+      aotoso.print("aotoso");
+
+      RefSCMatrix sotoao = aotoso.i();
+      sotoao.print("sotoao");
+
+      RefSymmSCMatrix s(gbs->basisdim());
+      s.assign(0.0);
+
+      RefSCElementOp op = new GaussianOverlapIntv2(gbs);
+      s.element_op(op);
+      op=0;
+
+      s.print("overlap");
+
+      RefSymmSCMatrix ss = s.clone();
+      ss.assign(0.0);
+      ss.accumulate_transform(aotoso.t(),s);
+      ss.print("blocked ss");
+      
+      s.assign(0.0);
+      s.accumulate_transform(sotoao.t(),ss);
+      s.print("unblocked s");
+      
       gbs->print();
 
       fflush(stdout);

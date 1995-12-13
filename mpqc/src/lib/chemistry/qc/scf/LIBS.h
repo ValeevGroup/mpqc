c8147547cf90a95cedc30b0b434459be4bee5729
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1995-12-13 04:43:59 +0000

    get rid of references to dmt stuff
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401023

--- mpqc/src/lib/chemistry/qc/scf/LIBS.h
+++ mpqc/src/lib/chemistry/qc/scf/LIBS.h
@@ -1,7 +1,5 @@
 libscf.LIBSUF
 #include <chemistry/qc/wfn/LIBS.h>
-#include <chemistry/qc/dmtscf/LIBS.h>
-#include <chemistry/qc/force/LIBS.h>
 #include <chemistry/molecule/LIBS.h>
 #include <util/misc/LIBS.h>
 #include <util/keyval/LIBS.h>

788d46019892d974b5f162f18c2205dc538a4f26
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-02-09 17:38:56 +0000

    added timing stuff, and changes for symmetry
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401104

--- mpqc/src/lib/chemistry/qc/basis/btest.cc
+++ mpqc/src/lib/chemistry/qc/basis/btest.cc
@@ -6,6 +6,9 @@
 #include <chemistry/qc/basis/petite.h>
 #include <chemistry/qc/integral/integralv2.h>
 
+#include <util/misc/timer.h>
+#include <util/group/picl.h>
+
 int
 main(int, char *argv[])
 {
@@ -13,50 +16,79 @@ main(int, char *argv[])
   
   RefKeyVal keyval = new ParsedKeyVal(filename);
 
+  int nproc, me, host, top, ord, dir;
+  RefMessageGrp grp = MessageGrp::get_default_messagegrp();
+  open0_messagegrp(&nproc, &me, &host, grp);
+  setarc0(&nproc,&top,&ord,&dir);
+
   for (int i=0; i<keyval->count("test"); i++) {
       RefGaussianBasisSet gbs = keyval->describedclassvalue("test", i);
 
-      gbs->molecule()->print();
-      printf("\n");
+      if (SymmGaussianBasisSet::castdown(gbs)) {
+        SymmGaussianBasisSet *sgbs = SymmGaussianBasisSet::castdown(gbs);
+        
+        CharacterTable ct = gbs->molecule()->point_group().char_table();
+        PetiteList& pl = sgbs->petite_list();
 
-      PetiteList pl(gbs);
-      pl.print(stdout,0);
+        pl.print(stdout,0);
+
+        tim_enter("aotoso");
+        RefSCMatrix aotoso(pl.AO_basisdim(), pl.SO_basisdim());
+        RefSCElementOp sotrans = new AOSO_Transformation(gbs);
+        aotoso.element_op(sotrans);
+        sotrans=0;
+        tim_exit("aotoso");
+
+        tim_enter("overlap");
+        RefSymmSCMatrix s(pl.AO_basisdim());
+        s.assign(0.0);
+        RefSCElementOp op =
+          new GaussianOverlapIntv2(gbs, new SymmOneBodyIntIter(gbs));
+        s.element_op(op);
+        op=0;
+        tim_exit("overlap");
+
+        //s.print("overlap");
+        tim_print(0);
+
+        tim_enter("symmetrize");
+        RefSymmSCMatrix ss(pl.SO_basisdim());
+        pl.symmetrize(s,ss);
+        tim_exit("symmetrize");
+        //ss.print("blocked ss");
+        tim_print(0);
+
+        RefSCMatrix sotoao = aotoso.i();
+        RefSymmSCMatrix us = s.clone();
+        us.assign(0.0);
+        us.accumulate_transform(sotoao.t(),ss);
+        //us.print("us");
+
+        tim_enter("full overlap");
+        op = new GaussianOverlapIntv2(gbs);
+        s.assign(0.0);
+        s.element_op(op);
+        op=0;
+        tim_exit("full overlap");
+        //s.print("real overlap");
+        tim_print(0);
+
+        RefSymmSCMatrix uss = ss.clone();
+        uss.assign(0.0);
+        tim_enter("transform");
+        uss.accumulate_transform(aotoso.t(),s);
+        tim_exit("transform");
+        //uss.print("uss");
+        tim_print(0);
+        
+        //uss = uss-ss;
+        //uss.print("uss - ss");
       
-      RefSCMatrix aotoso(pl.AO_basisdim(), pl.SO_basisdim());
-      RefSCElementOp sotrans = new AOSO_Transformation(gbs);
-      aotoso.element_op(sotrans);
-      sotrans=0;
-      aotoso.print("aotoso");
-
-      RefSymmSCMatrix s(pl.AO_basisdim());
-      s.assign(0.0);
-#if 1
-      RefSCElementOp op = new GaussianOverlapIntv2(gbs);
-      s.element_op(op);
-#else
-      RefSCElementOp op = new GaussianKineticIntv2(gbs);
-      s.element_op(op);
-      op=0;
-      op = new GaussianNuclearIntv2(gbs);
-      s.element_op(op);
-#endif      
-      op=0;
-
-      RefSymmSCMatrix ss(pl.SO_basisdim());
-      ss.assign(0.0);
-      ss.accumulate_transform(aotoso.t(),s);
-
-      ss.print("blocked ss");
-
-      RefSCMatrix sotoao = aotoso.i();
-      RefSymmSCMatrix us = s.clone();
-      us.assign(0.0);
-      us.accumulate_transform(sotoao.t(),ss);
+        //us = us-s;
+        //us.print("us - s");
+      }
       
-      us = us-s;
-      us.print("us - s");
-      
-      gbs->print();
+      //gbs->print();
 
       fflush(stdout);
       cout.flush();
@@ -65,7 +97,7 @@ main(int, char *argv[])
       gbs.save_state(out);
       StateInText in("btest.out");
       gbs.restore_state(in);
-      gbs->print();
+      //gbs->print();
     }
 
   return 0;

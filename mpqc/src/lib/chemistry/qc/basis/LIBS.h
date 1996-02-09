cee5fe0e20167aa6ed302ab337a82e4feff0a922
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-02-09 17:33:13 +0000

    change integral to intv2
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401101

--- mpqc/src/lib/chemistry/qc/basis/LIBS.h
+++ mpqc/src/lib/chemistry/qc/basis/LIBS.h
@@ -1,5 +1,5 @@
 libbasis.LIBSUF
 #include <chemistry/molecule/LIBS.h>
-#include <chemistry/qc/integral/LIBS.h>
+#include <chemistry/qc/intv2/LIBS.h>
 #include <math/scmat/LIBS.h>
 #include <util/container/LIBS.h>

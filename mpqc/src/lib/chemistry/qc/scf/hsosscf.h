78b29a4735871971e85635d571eae3d86e2e7335
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-05-28 18:02:20 +0000

    convert to new SCF class interface
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401299

--- mpqc/src/lib/chemistry/qc/scf/hsosscf.h
+++ mpqc/src/lib/chemistry/qc/scf/hsosscf.h
@@ -6,84 +6,69 @@
 #pragma interface
 #endif
 
-#include <math/scmat/elemop.h>
-#include <math/scmat/block.h>
-#include <math/scmat/blkiter.h>
-#include <math/optimize/scextrap.h>
-#include <chemistry/qc/wfn/obwfn.h>
-#include <chemistry/qc/wfn/effh.h>
-
-#include <math/array/math_lib.h>
-#include <chemistry/qc/intv2/int_libv2.h>
+#include <chemistry/qc/scf/scf.h>
 
 ////////////////////////////////////////////////////////////////////////////
 
-class HSOSSCF: public OneBodyWavefunction
-{
+class HSOSSCF: public SCF {
 #   define CLASSNAME HSOSSCF
 #   define HAVE_KEYVAL_CTOR
 #   define HAVE_STATEIN_CTOR
 #   include <util/state/stated.h>
 #   include <util/class/classd.h>
- protected:
-    RefSelfConsistentExtrapolation _extrap;
-    RefSCExtrapData _data;
-    RefSCExtrapError _error;
-
-    RefAccumDIH _accumdih;
-    RefAccumDDH _accumddh;
-    RefAccumEffectiveH _accumeffh;
-
-    RefSymmSCMatrix _fock;
-    RefSymmSCMatrix _op_fock;
-    RefDiagSCMatrix _fock_evals;
-    
-    int _ndocc;
-    int _nsocc;
-    int _density_reset_freq;
-
-    int _maxiter;
-    int _eliminate;
-
-    char *ckptdir;
-    char *fname;
-
+ private:
     // these are temporary data, so they should not be checkpointed
-    RefSymmSCMatrix _gr_dens;
-    RefSymmSCMatrix _gr_dens_diff;
-    RefSymmSCMatrix _gr_gmat;
-    RefSymmSCMatrix _gr_op_dens;
-    RefSymmSCMatrix _gr_op_dens_diff;
-    RefSymmSCMatrix _gr_op_gmat;
-    RefSymmSCMatrix _gr_hcore;
-    RefSCMatrix _gr_vector;
+    RefSymmSCMatrix cl_fock_;
+    RefSymmSCMatrix cl_dens_;
+    RefSymmSCMatrix cl_dens_diff_;
+    RefSymmSCMatrix cl_gmat_;
+    RefSymmSCMatrix cl_hcore_;
+
+    RefSymmSCMatrix op_fock_;
+    RefSymmSCMatrix op_dens_;
+    RefSymmSCMatrix op_dens_diff_;
+    RefSymmSCMatrix op_gmat_;
     
     void init();
-    virtual void compute();
-    virtual void do_vector(double&,double&);
-    virtual void form_ao_fock(centers_t *, double*);
-    virtual double scf_energy();
-    virtual void do_gradient(const RefSCVector&);
-    void form_density(const RefSCMatrix& vec,
-                              const RefSymmSCMatrix& density,
-                              const RefSymmSCMatrix& density_diff,
-                              const RefSymmSCMatrix& open_density,
-                              const RefSymmSCMatrix& open_density_diff);
+    
+ protected:
+    int ndocc_;
+    int nsocc_;
+
+    // scf things
+    void init_vector();
+    void done_vector();
+    void reset_density();
+    double new_density();
+    double scf_energy();
+
+    void ao_fock();
+    void make_contribution(int,int,int,int,double,int);
+
+    RefSCExtrapError extrap_error();
+    RefSCExtrapData extrap_data();
+    RefSymmSCMatrix effective_fock();
+    
+    // gradient things
+    void init_gradient();
+    void done_gradient();
+
+    RefSymmSCMatrix lagrangian();
+    RefSymmSCMatrix gradient_density();
+    void make_gradient_contribution();
+
+    // hessian things
+    void init_hessian();
+    void done_hessian();
     
   public:
     HSOSSCF(StateIn&);
-    HSOSSCF(const HSOSSCF&);
     HSOSSCF(const RefKeyVal&);
-    HSOSSCF(const OneBodyWavefunction&);
     ~HSOSSCF();
 
-    HSOSSCF& operator=(const HSOSSCF&);
-    
     void save_data_state(StateOut&);
 
     void print(ostream&o=cout);
-
-    RefSCMatrix eigenvectors();
 
     double occupation(int vectornum);
 

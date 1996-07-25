67f9eec64b2ddbb23b25f92cf2565d39cddd1eb6
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-07-25 21:36:55 +0000

    add functions to insert copyleft statement
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401506

--- mpqc/lib/elisp/SC.el
+++ mpqc/lib/elisp/SC.el
@@ -127,3 +127,131 @@
     )
   thisdir
 )
+
+;;
+;; stuff for inserting copyleft notices
+;;
+
+(defvar copyleft-owner "Limit Point Systems, Inc."
+  "This is the owner of the copyleft.  Defaults to LPS.")
+
+(defun set-copyleft-owner (owner)
+  "Set the copyleft-owner variable."
+  (interactive (list (read-from-minibuffer "Copyleft Owner: "
+                                           copyleft-owner nil nil nil)))
+  (setq copyleft-owner owner))
+
+(defvar copyleft-author user-full-name
+  "This is the author of the file.  Defaults to the user editing the file.")
+
+(defun set-copyleft-author (author)
+  "Set the copyleft-author variable."
+  (interactive (list (read-from-minibuffer "Author: "
+                                           copyleft-author nil nil nil)))
+  (setq copyleft-author author))
+
+(defvar copyleft-address user-mail-address
+  "This is the email address of the author of the file.  Defaults to the
+   address of the user editing the file.")
+
+(defun set-copyleft-address (address)
+  "Set the copyleft-address variable."
+  (interactive (list (read-from-minibuffer "E-mail address: "
+                                           copyleft-address nil nil nil)))
+  (setq copyleft-address address))
+
+(defvar copyleft-maintainer "LPS"
+  "This is the official maintaner of the file. Defaults to LPS")
+
+(defun set-copyleft-maintainer (maintainer)
+  "Set the copyleft-maintainer variable."
+  (interactive (list (read-from-minibuffer "Maintainer: "
+                                           copyleft-maintainer nil nil nil)))
+  (setq copyleft-maintainer maintainer))
+
+(defvar copyleft-default-comment-start "#"
+  "The default symbol to use to begin a comment. Defaults to \"#\".")
+
+(defvar copyleft-default-comment-cont "#"
+  "The default symbol to use to continue a comment. Defaults to \"#\".")
+
+(defvar copyleft-default-comment-end "#"
+  "The default symbol to use to end a comment. Defaults to \"#\".")
+
+(defun copyleft-set-comments (start cont end)
+  "Set the comment symbols.
+
+   (copyleft-set-comments START CONT END)"
+  (interactive (list (read-from-minibuffer "Comment start: "
+                              copyleft-default-comment-start nil nil nil)
+                     (read-from-minibuffer "Comment continue: "
+                              copyleft-default-comment-cont nil nil nil)
+                     (read-from-minibuffer "Comment end: "
+                              copyleft-default-comment-end nil nil nil)
+                     ))
+
+  (setq copyleft-default-comment-start start)
+  (setq copyleft-default-comment-cont cont)
+  (setq copyleft-default-comment-end end)
+)
+
+(defun insert-copyleft ()
+  "Insert the notice."
+  (interactive)
+  (set-window-point (display-buffer (current-buffer)) (point-min))
+  (cond ((eq major-mode 'c++-mode)
+         (setq comment-start "//")
+         (setq comment-cont "//")
+         (setq comment-end "//")
+         )
+        ((eq major-mode 'c-mode)
+         (setq comment-start "/*")
+         (setq comment-cont " *")
+         (setq comment-end " */")
+         )
+        ((eq major-mode 'emacs-lisp-mode)
+         (setq comment-start ";;")
+         (setq comment-cont ";;")
+         (setq comment-end ";;")
+         )
+        ('t
+         (setq comment-start copyleft-default-comment-start)
+         (setq comment-cont copyleft-default-comment-cont)
+         (setq comment-end copyleft-default-comment-end)
+         )
+        )
+
+  (setq description (read-from-minibuffer "Description: "))
+
+  (insert comment-start "\n")
+  (insert comment-cont " " (file-name-nondirectory buffer-file-name) 
+      (cond ((not (string= description "")) (concat " --- " description "\n"))
+            ('t "\n")))
+  (insert comment-cont "\n")
+  (insert comment-cont " Copyright (C) "
+          (substring (current-time-string) 20) " "
+          copyleft-owner "\n")
+  (insert comment-cont "\n")
+  (insert comment-cont " Author: " copyleft-author " <" copyleft-address ">\n")
+  (insert comment-cont " Maintainer: " copyleft-maintainer "\n")
+  (insert comment-cont "\n")
+  (insert comment-cont " This file is part of the SC Toolkit.\n")
+  (insert comment-cont "\n")
+
+  (insert comment-cont " The SC Toolkit is free software; you can redistribute it and/or modify\n")
+  (insert comment-cont " it under the terms of the GNU Library General Public License as published by\n")
+  (insert comment-cont " the Free Software Foundation; either version 2, or (at your option)\n")
+  (insert comment-cont " any later version.\n")
+  (insert comment-cont "\n")
+
+  (insert comment-cont " The SC Toolkit is distributed in the hope that it will be useful,\n")
+  (insert comment-cont " but WITHOUT ANY WARRANTY; without even the implied warranty of\n")
+  (insert comment-cont " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n")
+  (insert comment-cont " GNU Library General Public License for more details.\n")
+  (insert comment-cont "\n")
+
+  (insert comment-cont " You should have received a copy of the GNU Library General Public License\n")
+  (insert comment-cont " along with the SC Toolkit; see the file COPYING.LIB.  If not, write to\n")
+  (insert comment-cont " the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.\n")
+  (insert comment-end "\n")
+)

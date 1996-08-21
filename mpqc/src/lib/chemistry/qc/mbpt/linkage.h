d76e126aa58e8b3a4fc7d45549ff62e82fe18289
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-08-21 22:11:13 +0000

    add linkage.h
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401547

---
+++ mpqc/src/lib/chemistry/qc/mbpt/linkage.h
@@ -0,0 +1,15 @@
+
+#ifndef _chemistry_qc_mbpt_linkage_h
+#define _chemistry_qc_mbpt_linkage_h
+
+#ifndef __PIC__
+
+#include <chemistry/qc/mbpt/mbpt.h>
+
+#include <chemistry/qc/scf/linkage.h>
+
+const ClassDesc &mbpt_force_link_a_ = MBPT2::class_desc_;
+
+#endif /* __PIC__ */
+
+#endif

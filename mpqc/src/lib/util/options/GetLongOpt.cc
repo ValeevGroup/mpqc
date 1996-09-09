1ea51ab694ed301dd229422235e6faa9d71077a9
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-09-09 21:18:53 +0000

    Avoid a compiler complaint.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401557

--- mpqc/src/lib/util/options/GetLongOpt.cc
+++ mpqc/src/lib/util/options/GetLongOpt.cc
@@ -221,7 +221,7 @@ GetLongOpt::setcell(Cell *c, char *valtoken, char *nexttoken, char *name)
 	 cerr << optmarker << c->option << "\n";
 	 return -1;	/* unsolicited value specification */
       }
-      c->value = 1;
+      c->value = (char*)1;
       return 0;
    case GetLongOpt::OptionalValue :
       if ( *valtoken == '=' ) {

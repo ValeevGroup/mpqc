db395f3896da4b5c7534ad313620afe54c4dae2f
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-09-13 01:21:31 +0000

    Minor update.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401560

--- mpqc/src/bin/mpqc/disclaimer.h
+++ mpqc/src/bin/mpqc/disclaimer.h
@@ -6,11 +6,12 @@
 
 static char *disclaimer[] = {
   "!!!!!!! WARNING !!!!!!!!!! WARNING !!!!!!!!!!! WARNING !!!!!!!\n",
-  "This is a direct scf program which utilizes quantum chemistry",
-  "libraries developed at the Univerity of Georgia, Sandia National",
-  "Laboratories, and the National Institutes of Health.  It is free.",
-  "You need pay a license to no one to use this program.  You may",
-  "modify it at will.  We don\'t care.  Just understand one thing...",
+  "This is a quantum chemistry program which utilizes libraries",
+  "developed at the Univerity of Georgia, Sandia National",
+  "Laboratories, Stanford University, and the National Institutes",
+  "of Health.  It is free.  You need pay a license fee to no one",
+  "to use this program.  You may modify it at will.  We don\'t",
+  "care.  Just understand one thing...\n",
   "This program comes with NO WARRANTY.  The answers it produces are",
   "NOT guaranteed to be better than random numbers.  If you lose a",
   "million because MPQC messes up, it\'s you that\'s out the million,",
@@ -19,7 +20,8 @@ static char *disclaimer[] = {
   "including nothing.  Also, if you modify this code and publish",
   "results obtained with it, please mention in your paper that you",
   "modified the code so we won\'t be blamed if your results are garbage.\n",
-  "!!!!!!! WARNING !!!!!!!!!! WARNING !!!!!!!!!!! WARNING !!!!!!!\n"
+  "!!!!!!! WARNING !!!!!!!!!! WARNING !!!!!!!!!!! WARNING !!!!!!!\n",
+  0
 };
 
 static void
@@ -27,7 +29,7 @@ print_disclaimer(ostream& out)
 {
   out << node0 << endl;
   
-  for (int i=0; i < 15 ; i++)
+  for (int i=0; disclaimer[i]; i++)
     out << node0 << indent << disclaimer[i] << endl;
 }
 

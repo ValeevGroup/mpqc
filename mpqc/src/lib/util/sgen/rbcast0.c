d3e8b28c978bf0f00fa9eef95e28f05fb7cdd2a6
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-10-25 19:38:41 +0000

    add copyleft notice and emacs local variables
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401618

--- mpqc/src/lib/util/sgen/rbcast0.c
+++ mpqc/src/lib/util/sgen/rbcast0.c
@@ -1,5 +1,34 @@
+/*
+ * rbcast0.c
+ *
+ * Copyright (C) 1996 Limit Point Systems, Inc.
+ *
+ * Author: Curtis Janssen <cljanss@ca.sandia.gov>
+ * Maintainer: LPS
+ *
+ * This file is part of the SC Toolkit.
+ *
+ * The SC Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * The SC Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with the SC Toolkit; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * The U.S. Government is granted a limited license as per AL 91-7.
+ */
 
 /* $Log$
+ * Revision 1.5  1996/10/25 19:38:38  etseidl
+ * add copyleft notice and emacs local variables
+ *
  * Revision 1.4  1995/03/18 00:11:27  cljanss
  * Using util/group to provide picl support.  Deleted the comm directory.
  *

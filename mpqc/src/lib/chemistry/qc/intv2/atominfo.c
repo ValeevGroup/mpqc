8e1fe7f8bd6acdca2b1de3cab47a86a0833819b8
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-10-25 23:28:21 +0000

    add copyleft notice
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401637

--- mpqc/src/lib/chemistry/qc/intv2/atominfo.c
+++ mpqc/src/lib/chemistry/qc/intv2/atominfo.c
@@ -1,5 +1,34 @@
+/*
+ * atominfo.c
+ *
+ * Copyright (C) 1996 Limit Point Systems, Inc.
+ *
+ * Author: Curtis Janssen <cljanss@ca.sandia.gov>
+ * Maintainer: LPS
+ *
+ * This file is part of the SC Toolkit.
+ *
+ * The SC Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * The SC Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with the SC Toolkit; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * The U.S. Government is granted a limited license as per AL 91-7.
+ */
 
 /* $Log$
+ * Revision 1.5  1996/10/25 23:27:49  etseidl
+ * add copyleft notice
+ *
  * Revision 1.4  1995/03/17 01:49:18  cljanss
  * Removed -I. and -I$(SRCDIR) from the default include path in
  * GlobalMakefile to avoid name conflicts with system include files.
--- mpqc/src/lib/chemistry/qc/intv2/fjttable.h
+++ mpqc/src/lib/chemistry/qc/intv2/fjttable.h
@@ -1,5 +1,34 @@
+/*
+ * fjttable.h
+ *
+ * Copyright (C) 1996 Limit Point Systems, Inc.
+ *
+ * Author: Curtis Janssen <cljanss@ca.sandia.gov>
+ * Maintainer: LPS
+ *
+ * This file is part of the SC Toolkit.
+ *
+ * The SC Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * The SC Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with the SC Toolkit; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * The U.S. Government is granted a limited license as per AL 91-7.
+ */
 
 /* $Log$
+ * Revision 1.3  1996/10/25 23:28:04  etseidl
+ * add copyleft notice
+ *
  * Revision 1.2  1993/12/30 13:32:49  etseidl
  * mostly rcs id stuff
  *

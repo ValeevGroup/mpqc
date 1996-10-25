0623b7eef770f098c15a34908ebfa735aac7f5f0
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-10-25 21:09:52 +0000

    modify copyright
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401627

--- mpqc/src/lib/math/symmetry/irrep.cc
+++ mpqc/src/lib/math/symmetry/irrep.cc
@@ -1,3 +1,30 @@
+//
+// irrep.cc
+//
+// Modifications are
+// Copyright (C) 1996 Limit Point Systems, Inc.
+//
+// Author: Edward Seidl <seidl@janed.com>
+// Maintainer: LPS
+//
+// This file is part of the SC Toolkit.
+//
+// The SC Toolkit is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// The SC Toolkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the SC Toolkit; see the file COPYING.LIB.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// The U.S. Government is granted a limited license as per AL 91-7.
+//
 
 /* irrep.cc -- implementation of the point group classes
  *
@@ -15,7 +42,7 @@
  *      Division of Computer Research and Technology
  *      National Institutes of Health
  *      Bethesda, Maryland 20892
- *      Internet: seidl@janed.com
+ *      Internet: seidl@alw.nih.gov
  *      June, 1993
  */
 

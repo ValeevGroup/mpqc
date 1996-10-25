7b4945e55092ee3a7c36a56af48d805b0249d431
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-10-25 21:49:20 +0000

    add copyleft notice
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401629

--- mpqc/src/lib/math/optimize/kvopt.cc
+++ mpqc/src/lib/math/optimize/kvopt.cc
@@ -1,5 +1,31 @@
+//
+// kvopt.cc
+//
+// Copyright (C) 1996 Limit Point Systems, Inc.
+//
+// Author: Curtis Janssen <cljanss@ca.sandia.gov>
+// Maintainer: LPS
+//
+// This file is part of the SC Toolkit.
+//
+// The SC Toolkit is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// The SC Toolkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the SC Toolkit; see the file COPYING.LIB.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// The U.S. Government is granted a limited license as per AL 91-7.
+//
 
-#include <stdio.h>
+#include <util/misc/formio.h>
 #include <math/optimize/opt.h>
 #include <util/keyval/keyval.h>
 
@@ -7,7 +33,7 @@ int
 main(int argc, char** argv)
 {
   if (argc != 3) {
-      fprintf(stderr,"Usage: %s inputfile keyword\n", argv[0]);
+      cerr << scprintf("Usage: %s inputfile keyword\n", argv[0]);
       exit(1);
     }
   char* inputfile = argv[1];
@@ -15,8 +41,8 @@ main(int argc, char** argv)
 
   FILE*fp = fopen(inputfile,"r");
   if (!fp) {
-      fprintf(stderr,"%s: error opening input file \"%s\"\n",
-              argv[0], inputfile);
+      cerr << scprintf("%s: error opening input file \"%s\"\n",
+                       argv[0], inputfile);
       perror("fopen");
       exit(1);
     }
@@ -24,32 +50,32 @@ main(int argc, char** argv)
   RefParsedKeyVal keyval = new ParsedKeyVal(fp);
 
   if (!keyval->exists(keyword)) {
-      fprintf(stderr,"%s: keyword \"%s\" doesn't exist in file \"%s\"\n",
-              argv[0], keyword, inputfile);
+      cerr << scprintf("%s: keyword \"%s\" doesn't exist in file \"%s\"\n",
+                       argv[0], keyword, inputfile);
       exit(1);
     }
 
   if (!keyval->classname(keyword)) {
-      fprintf(stderr,"%s: keyword \"%s\" in file \"%s\" is not an object\n",
-              argv[0], keyword, inputfile);
+      cerr << scprintf("%s: keyword \"%s\" in file \"%s\" is not an object\n",
+                       argv[0], keyword, inputfile);
       exit(1);
     }
 
   RefDescribedClass dc = keyval->describedclassvalue(keyword);
 
   if (dc.null()) {
-      fprintf(stderr,"%s: keyword \"%s\" in file \"%s\" could not be"
-              " converted into a DescribedClass object\n",
-              argv[0], keyword, inputfile);
+      cerr << scprintf("%s: keyword \"%s\" in file \"%s\" could not be"
+                       " converted into a DescribedClass object\n",
+                       argv[0], keyword, inputfile);
       exit(1);
     }
 
   RefOptimize opt = dc;
 
   if (opt.null()) {
-      fprintf(stderr,"%s: keyword \"%s\" in file \"%s\" could not be"
-              " converted into an Optimize object\n",
-              argv[0], keyword, inputfile);
+      cerr << scprintf("%s: keyword \"%s\" in file \"%s\" could not be"
+                       " converted into an Optimize object\n",
+                       argv[0], keyword, inputfile);
       exit(1);
     }
 
@@ -63,3 +89,4 @@ main(int argc, char** argv)
 // Local Variables:
 // mode: c++
 // eval: (c-set-style "CLJ")
+// End:

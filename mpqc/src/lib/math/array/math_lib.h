a0569a9d4a62462fcff1dfe907222ed3fbf7ecdd
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-10-25 21:02:56 +0000

    add copyleft notice
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401626

--- mpqc/src/lib/math/array/math_lib.h
+++ mpqc/src/lib/math/array/math_lib.h
@@ -1,5 +1,34 @@
+/*
+ * math_lib.h
+ *
+ * Copyright (C) 1996 Limit Point Systems, Inc.
+ *
+ * Author: Curtis Janssen <cljanss@ca.sandia.gov>
+ * Maintainer: LPS
+ *
+ * This file is part of the SC Toolkit.
+ *
+ * The SC Toolkit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * The SC Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with the SC Toolkit; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * The U.S. Government is granted a limited license as per AL 91-7.
+ */
 
 /* $Log$
+ * Revision 1.6  1996/10/25 21:00:47  etseidl
+ * add copyleft notice
+ *
  * Revision 1.5  1995/10/11 21:13:54  cljanss
  * ParsedKeyVal now uses iostream instead of stdio.  Various other cleanups.
  *

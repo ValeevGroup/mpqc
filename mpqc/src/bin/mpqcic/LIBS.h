2e1bd845c368f1ed9588b2fe16dda352bb8ff110
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1996-12-20 22:29:45 +0000

    mpqcic is now obsolete
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401728

--- mpqc/src/bin/mpqcic/LIBS.h
+++
@@ -1,7 +0,0 @@
-#include <chemistry/qc/mbpt/LIBS.h>
-#include <chemistry/qc/dmtscf/LIBS.h>
-#include <chemistry/qc/force/LIBS.h>
-#include <chemistry/molecule/LIBS.h>
-#include <chemistry/qc/basis/LIBS.h>
-#include <util/misc/LIBS.h>
-#include <util/group/LIBS.h>

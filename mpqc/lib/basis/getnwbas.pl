aa1cd5b923017995d51abed50a9d8c6f5507887e
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1997-01-09 18:43:28 +0000

    Corrected 4-31G, 4-31G*, and 4-31G** basis sets. Split getnwbas.pl into the part that gets the basis and the part that parses the basis (parsenwbas.pl).
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401771

--- mpqc/lib/basis/getnwbas.pl
+++ mpqc/lib/basis/getnwbas.pl
@@ -4,7 +4,7 @@ $port = 2080;
 $them = 'www.emsl.pnl.gov';
 $script = "/cgi-bin/run-bsform-post";
 #$port = 80;
-#$them = 'munin.ca.sandia.gov';
+#$them = 'localhost';
 #$script = "/cgi-bin/echocat";
 
 $AF_INET = 2;
@@ -18,34 +18,35 @@ sub dokill {
 
 $sockaddr = 'S n a4 x8';
 
-
 chop ($hostname = `hostname`);
 
 ($name, $aliases, $proto) = getprotobyname('tcp');
 ($name, $aliases, $port) = getservbyname($port,'tcp')
   unless $port =~ /^\d+$/;
 ($name, $aliases, $type, $len, $thataddr) = gethostbyname($them);
+printf "name=$name, alias=$alias, type=$type, len=$len, thataddr=0x%x\n", unpack('I',$thataddr);
 ($name, $aliases, $type, $len, $thisaddr) = gethostbyname($hostname);
+printf "name=$name, alias=$alias, type=$type, len=$len, thisaddr=0x%x\n", unpack('I',$thisaddr);
 
 $this = pack($sockaddr, $AF_INET, 0, $thisaddr);
 $that = pack($sockaddr, $AF_INET, $port, $thataddr);
 
 if (socket(S, $AF_INET, $SOCK_STREAM, $proto)) {
-  print "socket ok\n";
+  #print "socket ok\n";
 }
 else {
   die $!;
 }
 
 if (bind(S, $this)) {
-  print "bind ok\n";
+  #print "bind ok\n";
 }
 else {
   die $!;
 }
 
 if (connect(S, $that)) {
-  print "connect ok\n";
+  #print "connect ok\n";
 }
 else {
   die $!;
@@ -64,118 +65,12 @@ $basisurl =~ s/\+/%2B/g;
 $basisurl =~ s/\(/\%28/g;
 $basisurl =~ s/\)/\%29/g;
 $basisurl =~ s/,/\%2C/g;
-printf "Basis is %s\n", $basis;
 $data = sprintf "BasisSets=%s&Atoms=H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr&Optimize=on&ECP=on&Codes=NWChem&Email=%s", $basisurl, $email;
 printf S "POST %s HTTP/1.0\n", $script;
 printf S "Content-length: %d\n", length($data);
 printf S "Content-type: application/x-www-form-urlencoded\n";
 printf S "\n";
 printf S "%s EOD\n", $data;
-
-$name{"H"}="hydrogen";
-$name{"He"}="helium";
-$name{"Li"}="lithium";
-$name{"Be"}="beryllium";
-$name{"B"}="boron";
-$name{"C"}="carbon";
-$name{"N"}="nitrogen";
-$name{"O"}="oxygen";
-$name{"F"}="fluorine";
-$name{"Ne"}="neon";
-$name{"Na"}="sodium";
-$name{"Mg"}="magnesium";
-$name{"Al"}="aluminum";
-$name{"Si"}="silicon";
-$name{"P"}="phosphorus";
-$name{"S"}="sulfur";
-$name{"Cl"}="chlorine";
-$name{"Ar"}="argon";
-$name{"K"}="potassium";
-$name{"Ca"}="calcium";
-$name{"Sc"}="scandium";
-$name{"Ti"}="titanium";
-$name{"V"}="vanadium";
-$name{"Cr"}="chromium";
-$name{"Mn"}="manganese";
-$name{"Fe"}="iron";
-$name{"Co"}="cobalt";
-$name{"Ni"}="nickel";
-$name{"Cu"}="copper";
-$name{"Zn"}="zinc";
-$name{"Ga"}="gallium";
-$name{"Ge"}="germanium";
-$name{"As"}="arsenic";
-$name{"Se"}="selenium";
-$name{"Br"}="bromine";
-$name{"Kr"}="krypton";
-$name{"Rb"}="rubidium";
-$name{"Sr"}="strontium";
-$name{"Y"}="yttrium";
-$name{"Zr"}="zirconium";
-$name{"Nb"}="niobium";
-$name{"Mo"}="molybdenum";
-$name{"Tc"}="technetium";
-$name{"Ru"}="ruthenium";
-$name{"Rh"}="rhodium";
-$name{"Pd"}="palladium";
-$name{"Ag"}="silver";
-$name{"Cd"}="cadminium";
-$name{"In"}="indium";
-$name{"Sn"}="tin";
-$name{"Sb"}="antimony";
-$name{"Te"}="tellurium";
-$name{"I"}="iodine";
-$name{"Xe"}="xenon";
-$name{"Cs"}="cesium";
-$name{"Ba"}="barium";
-$name{"La"}="lanthanium";
-$name{"Ce"}="cerium";
-$name{"Pr"}="praseodymium";
-$name{"Nd"}="neodymium";
-$name{"Pm"}="promethium";
-$name{"Sm"}="samarium";
-$name{"Eu"}="europium";
-$name{"Gd"}="gadolinium";
-$name{"Tb"}="terbium";
-$name{"Dy"}="dysprosium";
-$name{"Ho"}="holmium";
-$name{"Er"}="erbium";
-$name{"Tm"}="thulium";
-$name{"Yb"}="ytterbium";
-$name{"Lu"}="lutetium";
-$name{"Hf"}="hafnium";
-$name{"Ta"}="tantalum";
-$name{"W"}="tungsten";
-$name{"Re"}="rhenium";
-$name{"Os"}="osmium";
-$name{"Ir"}="iridium";
-$name{"Pt"}="platinum";
-$name{"Au"}="gold";
-$name{"Hg"}="mercury";
-$name{"Tl"}="thallium";
-$name{"Pb"}="lead";
-$name{"Bi"}="bismuth";
-$name{"Po"}="polonium";
-$name{"At"}="astatine";
-$name{"Rn"}="radon";
-$name{"Fr"}="francium";
-$name{"Ra"}="radium";
-$name{"Ac"}="actinium";
-$name{"Th"}="thorium";
-$name{"Pa"}="protactinium";
-$name{"U"}="uranium";
-$name{"Np"}="neptunium";
-$name{"Pu"}="plutonium";
-$name{"Am"}="americium";
-$name{"Cm"}="curium";
-$name{"Bk"}="berkelium";
-$name{"Cf"}="californium";
-$name{"Es"}="einsteinum";
-$name{"Fm"}="fermium";
-$name{"Md"}="mendelevium";
-$name{"No"}="nobelium";
-$name{"Lr"}="lawrencium";
-
 
 $basisname = $basis;
 $basisname =~ tr/A-Z/a-z/;
@@ -185,125 +80,13 @@ $basisname =~ tr/\(/L/;
 $basisname =~ tr/\)/R/;
 $basisname =~ tr/,/_/;
 $basisname =~ tr/ /_/;
-$basisfile = ">$basisname.kv";
+$basisfile = ">$basisname.nw";
+printf "Basis will be put in %s.nw ...", $basisname;
 #$basisfile = "|cat";
-$atom = none;
-$retrieve = 0;
-$pure = 0;
 open(BASIS, $basisfile);
-printf BASIS "basis:(\n";
 while (<S>) {
-  #  print;
-  #  next;
-  GOTLINE:
-  #printf "-----> %s\n", $_;
-  if (/<pre>(BASIS.* +[^ ]* +)([A-Z]*)/) {
-    $retrieve = 1;
-    $line = "$1$2";
-    if ($2 eq "SPHERICAL") {
-      $pure = 1;
-    }
-    #printf "%s\n", $line;
-    printf BASIS "%%%s\n", $line;
-  }
-  elsif (/^<p>END/) {
-    $retrieve = 0;
-  }
-  elsif (/^<p>\#(.*)/) {
-    printf BASIS "%%%s\n", $1;
-  }
-  elsif ($retrieve == 1) {
-    /^<p>(.*)/;
-    $_ = $1;
-    #printf "%s\n", $_;
-    if (/^ *([A-Z][a-z]*) +([A-Za-z]+)/) {
-      if (!($1 eq $atom)) {
-        if ($atom eq none) {
-          $atom = $1;
-          $am = $2;
-          &start_atom;
-          &start_shell;
-        }
-        else {
-          &finish_shell;
-          &finish_atom;
-          $atom = $1;
-          $am = $2;
-          &start_atom;
-          &start_shell;
-        }
-      }
-      else {
-        &finish_shell;
-        $am = $2;
-        &start_shell;
-      }
-    goto GOTLINE;
-    }
-    else {
-      printf BASIS "   %s\n", $1;
-    }
-  }
+    print BASIS;
 }
-if (!($atom eq none)) {
-  &finish_shell;
-  &finish_atom;
-}
-printf BASIS ")\n";
 close(BASIS);
 close(S);
-
-
-sub start_atom {
-  printf BASIS " %s: \"%s\": [\n", $name{$atom}, $basis;
-}
-
-sub finish_atom {
-  printf BASIS " ]\n";
-}
-
-sub start_shell {
-  while (<S>) {
-    last;
-  }
-  @coefandexp = split(' ',$_);
-  $ncoef = $#coefandexp - 1;
-  ($amlower = $am) =~ tr/A-Z/a-z/;
-  printf BASIS "  (type:";
-  if ($amlower eq "sp") {
-    printf BASIS " [am = p am = s]\n";
-  }
-  else {
-    printf BASIS " [", $amlower;
-    $icoef = 0;
-    while ($icoef < $ncoef) {
-      if ($icoef != 0) {
-        printf BASIS " ";
-      }
-      if ($amlower eq "s" || $amlower eq "p" || !$pure) {
-        printf BASIS "am = %s", $amlower;
-      }
-      else {
-        printf BASIS "(am = %s puream = 1)", $amlower;
-      }
-      $icoef++;
-    }
-    printf BASIS "]\n", $amlower;
-  }
-  printf BASIS "   {exp";
-  if ($amlower eq "sp") {
-    printf BASIS " coef:1 coef:0";
-  }
-  else {
-    $icoef = 0;
-    while ($icoef < $ncoef) {
-      printf BASIS " coef:%d", $icoef;
-      $icoef++;
-    }
-  }
-  printf BASIS "} = {\n";
-}
-
-sub finish_shell {
-  printf BASIS "   })\n";
-}
+printf " done.\n";
---
+++ mpqc/lib/basis/parsenwbas.pl
@@ -0,0 +1,254 @@
+#!/usr/bin/perl
+
+$basis = $ARGV[0];
+
+$name{"H"}="hydrogen";
+$name{"He"}="helium";
+$name{"Li"}="lithium";
+$name{"Be"}="beryllium";
+$name{"B"}="boron";
+$name{"C"}="carbon";
+$name{"N"}="nitrogen";
+$name{"O"}="oxygen";
+$name{"F"}="fluorine";
+$name{"Ne"}="neon";
+$name{"Na"}="sodium";
+$name{"Mg"}="magnesium";
+$name{"Al"}="aluminum";
+$name{"Si"}="silicon";
+$name{"P"}="phosphorus";
+$name{"S"}="sulfur";
+$name{"Cl"}="chlorine";
+$name{"Ar"}="argon";
+$name{"K"}="potassium";
+$name{"Ca"}="calcium";
+$name{"Sc"}="scandium";
+$name{"Ti"}="titanium";
+$name{"V"}="vanadium";
+$name{"Cr"}="chromium";
+$name{"Mn"}="manganese";
+$name{"Fe"}="iron";
+$name{"Co"}="cobalt";
+$name{"Ni"}="nickel";
+$name{"Cu"}="copper";
+$name{"Zn"}="zinc";
+$name{"Ga"}="gallium";
+$name{"Ge"}="germanium";
+$name{"As"}="arsenic";
+$name{"Se"}="selenium";
+$name{"Br"}="bromine";
+$name{"Kr"}="krypton";
+$name{"Rb"}="rubidium";
+$name{"Sr"}="strontium";
+$name{"Y"}="yttrium";
+$name{"Zr"}="zirconium";
+$name{"Nb"}="niobium";
+$name{"Mo"}="molybdenum";
+$name{"Tc"}="technetium";
+$name{"Ru"}="ruthenium";
+$name{"Rh"}="rhodium";
+$name{"Pd"}="palladium";
+$name{"Ag"}="silver";
+$name{"Cd"}="cadminium";
+$name{"In"}="indium";
+$name{"Sn"}="tin";
+$name{"Sb"}="antimony";
+$name{"Te"}="tellurium";
+$name{"I"}="iodine";
+$name{"Xe"}="xenon";
+$name{"Cs"}="cesium";
+$name{"Ba"}="barium";
+$name{"La"}="lanthanium";
+$name{"Ce"}="cerium";
+$name{"Pr"}="praseodymium";
+$name{"Nd"}="neodymium";
+$name{"Pm"}="promethium";
+$name{"Sm"}="samarium";
+$name{"Eu"}="europium";
+$name{"Gd"}="gadolinium";
+$name{"Tb"}="terbium";
+$name{"Dy"}="dysprosium";
+$name{"Ho"}="holmium";
+$name{"Er"}="erbium";
+$name{"Tm"}="thulium";
+$name{"Yb"}="ytterbium";
+$name{"Lu"}="lutetium";
+$name{"Hf"}="hafnium";
+$name{"Ta"}="tantalum";
+$name{"W"}="tungsten";
+$name{"Re"}="rhenium";
+$name{"Os"}="osmium";
+$name{"Ir"}="iridium";
+$name{"Pt"}="platinum";
+$name{"Au"}="gold";
+$name{"Hg"}="mercury";
+$name{"Tl"}="thallium";
+$name{"Pb"}="lead";
+$name{"Bi"}="bismuth";
+$name{"Po"}="polonium";
+$name{"At"}="astatine";
+$name{"Rn"}="radon";
+$name{"Fr"}="francium";
+$name{"Ra"}="radium";
+$name{"Ac"}="actinium";
+$name{"Th"}="thorium";
+$name{"Pa"}="protactinium";
+$name{"U"}="uranium";
+$name{"Np"}="neptunium";
+$name{"Pu"}="plutonium";
+$name{"Am"}="americium";
+$name{"Cm"}="curium";
+$name{"Bk"}="berkelium";
+$name{"Cf"}="californium";
+$name{"Es"}="einsteinum";
+$name{"Fm"}="fermium";
+$name{"Md"}="mendelevium";
+$name{"No"}="nobelium";
+$name{"Lr"}="lawrencium";
+
+$basisname = $basis;
+$basisname =~ tr/A-Z/a-z/;
+$basisname =~ tr/+/P/;
+$basisname =~ tr/\*/S/;
+$basisname =~ tr/\(/L/;
+$basisname =~ tr/\)/R/;
+$basisname =~ tr/,/_/;
+$basisname =~ tr/ /_/;
+#$basisfile = "|cat";
+$atom = none;
+$retrieve = 0;
+$pure = 0;
+printf "Reading NWChem basis from %s.nw\n", $basisname;
+printf "Writing MPQC basis to %s.kv\n", $basisname;
+open(NWCHEMBASIS, "<$basisname.nw");
+open(MPQCBASIS, ">$basisname.kv");
+#open(MPQCBASIS, "|cat");
+$firstatom=1;
+$savedcomments="";
+while (<NWCHEMBASIS>) {
+  #  print;
+  #  next;
+  GOTLINE:
+  #printf "-----> %s\n", $_;
+  if (/<pre>(BASIS.* +[^ ]* +)([A-Z]*)/) {
+    $retrieve = 1;
+    $line = "$1$2";
+    if ($2 eq "SPHERICAL") {
+      $pure = 1;
+    }
+    #printf "%s\n", $line;
+    printf MPQCBASIS "%%%s\n", $line;
+  }
+  elsif (/^<p>END/) {
+    $retrieve = 0;
+  }
+  elsif (/^<p>\#(.*)/) {
+    my $comment = $1;
+    $comment =~ s/ *$//;
+    $savedcomments = sprintf("%s%%%s\n", $savedcomments, $comment);
+  }
+  elsif ($retrieve == 1) {
+    /^<p>(.*)/;
+    $_ = $1;
+    #printf "%s\n", $_;
+    if (/^ *([A-Z][a-z]*) +([A-Za-z]+)/) {
+      if (!($1 eq $atom)) {
+        if ($atom eq none) {
+          $atom = $1;
+          $am = $2;
+          &start_atom;
+          &start_shell;
+        }
+        else {
+          &finish_shell;
+          &finish_atom;
+          $atom = $1;
+          $am = $2;
+          &start_atom;
+          &start_shell;
+        }
+      }
+      else {
+        &finish_shell;
+        $am = $2;
+        &start_shell;
+      }
+    goto GOTLINE;
+    }
+    else {
+      printf MPQCBASIS "   %s\n", $1;
+    }
+  }
+}
+if (!($atom eq none)) {
+  &finish_shell;
+  &finish_atom;
+}
+printf MPQCBASIS "%s", $savedcomments;
+$savedcomments = "";
+printf MPQCBASIS ")\n";
+close(MPQCBASIS);
+close(NWCHEMBASIS);
+
+
+sub start_atom {
+  if ($firstatom) {
+    print MPQCBASIS "basis:(\n";
+    $firstatom=0;
+  }
+  printf MPQCBASIS "%s", $savedcomments;
+  $savedcomments = "";
+  printf MPQCBASIS " %s: \"%s\": [\n", $name{$atom}, $basis;
+}
+
+sub finish_atom {
+  printf MPQCBASIS " ]\n";
+}
+
+sub start_shell {
+  printf MPQCBASIS "%s", $savedcomments;
+  $savedcomments = "";
+  while (<NWCHEMBASIS>) {
+    last;
+  }
+  @coefandexp = split(' ',$_);
+  $ncoef = $#coefandexp - 1;
+  ($amlower = $am) =~ tr/A-Z/a-z/;
+  printf MPQCBASIS "  (type:";
+  if ($amlower eq "sp") {
+    printf MPQCBASIS " [am = p am = s]\n";
+  }
+  else {
+    printf MPQCBASIS " [", $amlower;
+    $icoef = 0;
+    while ($icoef < $ncoef) {
+      if ($icoef != 0) {
+        printf MPQCBASIS " ";
+      }
+      if ($amlower eq "s" || $amlower eq "p" || !$pure) {
+        printf MPQCBASIS "am = %s", $amlower;
+      }
+      else {
+        printf MPQCBASIS "(am = %s puream = 1)", $amlower;
+      }
+      $icoef++;
+    }
+    printf MPQCBASIS "]\n", $amlower;
+  }
+  printf MPQCBASIS "   {exp";
+  if ($amlower eq "sp") {
+    printf MPQCBASIS " coef:1 coef:0";
+  }
+  else {
+    $icoef = 0;
+    while ($icoef < $ncoef) {
+      printf MPQCBASIS " coef:%d", $icoef;
+      $icoef++;
+    }
+  }
+  printf MPQCBASIS "} = {\n";
+}
+
+sub finish_shell {
+  printf MPQCBASIS "   })\n";
+}

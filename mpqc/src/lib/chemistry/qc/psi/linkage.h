cd858c985f89ec3848830cfded69f264478e469c
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1997-03-21 17:15:37 +0000

    This is for forcing linkage with the psi libraries.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%401817

---
+++ mpqc/src/lib/chemistry/qc/psi/linkage.h
@@ -0,0 +1,45 @@
+//
+// linkage.h
+//
+// Copyright (C) 1996 Limit Point Systems, Inc.
+//
+// Author: Edward Seidl <seidl@janed.com>
+// Maintainer: LPS
+//
+// This file is part of the SC Toolkit.
+//
+// The SC Toolkit is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// The SC Toolkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the SC Toolkit; see the file COPYING.LIB.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// The U.S. Government is granted a limited license as per AL 91-7.
+//
+
+#ifndef _chemistry_qc_scf_linkage_h
+#define _chemistry_qc_scf_linkage_h
+
+#ifndef __PIC__
+
+#include <chemistry/qc/psi/psi.h>
+#include <chemistry/qc/psi/psicc.h>
+#include <chemistry/qc/psi/psici.h>
+
+const ClassDesc &psi_force_link_a_ = PSISCF::class_desc_;
+const ClassDesc &psi_force_link_b_ = PSI_CCSD::class_desc_;
+const ClassDesc &psi_force_link_c_ = PSI_CCSD_T::class_desc_;
+const ClassDesc &psi_force_link_d_ = PSI_CCSDT::class_desc_;
+const ClassDesc &psi_force_link_e_ = PSI_CI::class_desc_;
+
+#endif /* __PIC__ */
+
+#endif

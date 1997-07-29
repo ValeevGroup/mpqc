b8b7d42679c61931cf1b8707513ae4fbef625c30
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1997-07-29 02:09:00 +0000

    trivial changes that may speed things up
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%402050

--- mpqc/src/lib/math/scmat/vector3_i.h
+++ mpqc/src/lib/math/scmat/vector3_i.h
@@ -39,18 +39,19 @@ INLINE void
 SCVector3::spherical_coord(double theta, double phi,
                            double r)
 {
-    _v[0]=r*sin(theta)*cos(phi);
-    _v[1]=r*sin(theta)*sin(phi);
-    _v[2]=r*cos(theta);
-
+  double rsin_theta = r*sin(theta);
+  _v[0]=rsin_theta*cos(phi);
+  _v[1]=rsin_theta*sin(phi);
+  _v[2]=r*cos(theta);
 }
 
 INLINE  double
 SCVector3::dist(const SCVector3 &s) const
 {
-    return sqrt((_v[0]-s._v[0])*(_v[0]-s._v[0])+
-                (_v[1]-s._v[1])*(_v[1]-s._v[1])+
-                (_v[2]-s._v[2])*(_v[2]-s._v[2]));
+  double x,y,z;
+  return sqrt((x=_v[0]-s._v[0])*x +
+              (y=_v[1]-s._v[1])*y +
+              (z=_v[2]-s._v[2])*z);
 }
 
 #undef INLINE

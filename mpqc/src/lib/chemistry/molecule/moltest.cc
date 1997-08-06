eba8bd71a1596b77ee95478dc0ec5ff0f1977af5
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1997-08-06 20:15:01 +0000

    Now possible to turn off certain tests by not giving the keyword.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%402067

--- mpqc/src/lib/chemistry/molecule/moltest.cc
+++ mpqc/src/lib/chemistry/molecule/moltest.cc
@@ -58,89 +58,100 @@ main(int argc, char **argv)
   else {
       kv = new ParsedKeyVal(SRCDIR "/moltest.in");
     }
-  RefKeyVal pkv(new PrefixKeyVal("molecule",kv));
 
   RefMolecule mol = kv->describedclassvalue("molecule");
+  if (mol.nonnull()) {
+      cout << "-------------- testing molecule --------------" << endl;
+      mol->cleanup_molecule();
+      cout << "Clean Molecule:\n";
+      mol->print();
 
-  mol->cleanup_molecule();
-  cout << "Clean Molecule:\n";
-  mol->print();
+      mol->transform_to_principal_axes();
+      cout << "Clean Molecule wrt principal axes:\n";
+      mol->print();
 
-  mol->transform_to_principal_axes();
-  cout << "Clean Molecule wrt principal axes:\n";
-  mol->print();
+      int nunique = mol->num_unique_atoms();
+      int * unique_atoms = mol->find_unique_atoms();
 
-  int nunique = mol->num_unique_atoms();
-  int * unique_atoms = mol->find_unique_atoms();
+      cout << "nunique=" << nunique << ":";
+      for (i=0; i < nunique; i++) cout << " " << unique_atoms[i]+1;
+      cout << endl;
 
-  cout << "nunique=%d: ",nunique;
-  for (i=0; i < nunique; i++) cout << " " << unique_atoms[i]+1;
-  cout << endl;
+      mol->point_group().char_table().print();
+    }
 
-  mol->point_group().char_table().print();
-
+  cout << "-------------- initializing render tests --------------" << endl;
   RefRender ren = kv->describedclassvalue("renderer");
   RefRenderedObject renmol = kv->describedclassvalue("renderedmolecule");
-  if (ren.nonnull() && renmol.nonnull()) ren->render(renmol);
+  if (ren.nonnull() && renmol.nonnull()) {
+      cout << "-------------- testing renderer --------------" << endl;
+      ren->render(renmol);
+    }
 
   //exit(0);
 
-  cout << "getting simp:\n";
   RefSetIntCoor simp = kv->describedclassvalue("simp");
-  RefIntCoorGen gen = kv->describedclassvalue("generator");
-  if (gen.nonnull()) {
-      gen->print();
+  if (simp.nonnull()) {
+      cout << "-------------- testing simp  --------------" << endl;
+      RefIntCoorGen gen = kv->describedclassvalue("generator");
+      if (gen.nonnull()) {
+          gen->print();
+        }
+      cout << "simp before update:\n";
+      simp->print(mol);
+      simp->update_values(mol);
+      cout << "simp:\n";
+      simp->print(mol);
     }
-  cout << "simp before update:\n";
-  simp->print(mol);
-  simp->update_values(mol);
-  cout << "simp:\n";
-  simp->print(mol);
 
   // compare the analytic bmatrix to the finite displacement bmatrix
   RefSetIntCoor bmat_test = kv->describedclassvalue("bmat_test");
-  RefSCMatrixKit kit = SCMatrixKit::default_matrixkit();
-  RefSCDimension dnc(new SCDimension(bmat_test->n()));
-  RefSCDimension dn3(new SCDimension(mol->natom()*3));
-  RefSCMatrix bmatrix(dnc,dn3,kit);
-  RefSCMatrix fd_bmatrix(dnc,dn3,kit);
-  cout << "testing bmat with:\n";
-  bmat_test->update_values(mol);
-  bmat_test->print();
-  bmat_test->bmat(mol,bmatrix);
-  bmat_test->fd_bmat(mol,fd_bmatrix);
-  cout << "test bmatrix:\n";
-  bmatrix.print();
-  cout << "fd bmatrix:\n";
-  fd_bmatrix.print();
-  RefSCMatrix diff = fd_bmatrix - bmatrix;
-  cout << "difference between test and finite displacement bmatrix:\n";
-  diff.print();
-  cout << "% difference between test and finite displacement bmatrix:\n";
-  for (i=0; i<diff.nrow(); i++) {
-      for (int j=0; j<diff.ncol(); j++) {
-          double denom = fabs(fd_bmatrix(i,j));
-          double num = fabs(diff(i,j));
-          if (denom < 0.000001) denom = 0.000001;
-          if (num < 0.00001) diff(i,j) = 0.0;
-          else diff(i,j) = 100.0 * fabs(diff(i,j))/denom;
-        }
-    }
-  diff.print();
-
-  cout << "testing for translational invariance of each coordinate:" << endl;
-  for (i=0; i<bmat_test->n(); i++) {
-      cout << "  coor " << scprintf("%2d",i) << ":";
-      for (int j=0; j<3; j++) {
-          double sum = 0.0;
-          for (int k=0; k<mol->natom(); k++) {
-              sum += bmatrix(i,k*3+j);
+  if (bmat_test.nonnull()) {
+      cout << "-------------- bmat_test  --------------" << endl;
+      RefSCMatrixKit kit = SCMatrixKit::default_matrixkit();
+      RefSCDimension dnc(new SCDimension(bmat_test->n()));
+      RefSCDimension dn3(new SCDimension(mol->natom()*3));
+      RefSCMatrix bmatrix(dnc,dn3,kit);
+      RefSCMatrix fd_bmatrix(dnc,dn3,kit);
+      cout << "testing bmat with:\n";
+      bmat_test->update_values(mol);
+      bmat_test->print();
+      bmat_test->bmat(mol,bmatrix);
+      bmat_test->fd_bmat(mol,fd_bmatrix);
+      cout << "test bmatrix:\n";
+      bmatrix.print();
+      cout << "fd bmatrix:\n";
+      fd_bmatrix.print();
+      RefSCMatrix diff = fd_bmatrix - bmatrix;
+      cout << "difference between test and finite displacement bmatrix:\n";
+      diff.print();
+      cout << "% difference between test and finite displacement bmatrix:\n";
+      for (i=0; i<diff.nrow(); i++) {
+          for (int j=0; j<diff.ncol(); j++) {
+              double denom = fabs(fd_bmatrix(i,j));
+              double num = fabs(diff(i,j));
+              if (denom < 0.000001) denom = 0.000001;
+              if (num < 0.00001) diff(i,j) = 0.0;
+              else diff(i,j) = 100.0 * fabs(diff(i,j))/denom;
             }
-          cout << scprintf(" % 16.12f",sum);
         }
-      cout << endl;
+      diff.print();
+
+      cout << "testing for translational invariance of each coordinate:"
+           << endl;
+      for (i=0; i<bmat_test->n(); i++) {
+          cout << "  coor " << scprintf("%2d",i) << ":";
+          for (int j=0; j<3; j++) {
+              double sum = 0.0;
+              for (int k=0; k<mol->natom(); k++) {
+                  sum += bmatrix(i,k*3+j);
+                }
+              cout << scprintf(" % 16.12f",sum);
+            }
+          cout << endl;
+        }
+      bmatrix.gi().print("The inverse bmatrix");
     }
-  bmatrix.gi().print("The inverse bmatrix");
 
   cout.flush();
   cerr.flush();
@@ -151,6 +162,7 @@ main(int argc, char **argv)
   cerr.flush();
 
   if (mc.nonnull()) {
+      cout << "-------------- testing molcoor  --------------" << endl;
       mc->print();
 
       cout.flush();
@@ -161,6 +173,7 @@ main(int argc, char **argv)
       // do_displacement(mc,2);
       // do_displacement(mc,3);
 
+      RefSCMatrixKit kit = SCMatrixKit::default_matrixkit();
       RefSymmSCMatrix hessian(mc->dim(),kit);
       mc->guess_hessian(hessian);
 
@@ -170,11 +183,13 @@ main(int argc, char **argv)
 
   RefMolecularEnergy me = kv->describedclassvalue("energy");
   if (me.nonnull()) {
+      cout << "-------------- testing energy  --------------" << endl;
       me->print();
     }
 
   RefMolecularFrequencies mf = kv->describedclassvalue("freq");
   if (mf.nonnull()) {
+      cout << "-------------- testing freq  --------------" << endl;
       mf->compute_displacements();
     }
 

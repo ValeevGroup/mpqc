98427d732c0faccaf8b45c36907768ad239b9d22
etseidl@0fb12191-db4b-0410-8866-e78deaee71e5 <etseidl@0fb12191-db4b-0410-8866-e78deaee71e5>
1997-11-12 02:03:56 +0000

    add font-lock support
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%402220

--- mpqc/lib/elisp/keyval.el
+++ mpqc/lib/elisp/keyval.el
@@ -18,7 +18,35 @@
   (setq major-mode 'keyval-mode
 	mode-name "KeyVal")
   (keyval-setup)
+  (make-local-variable 'font-lock-defaults)
+  (setq font-lock-defaults '(keyval-font-lock-keywords t t))
   (run-hooks 'keyval-mode-hook))
 
-(defun keyval-setup ()
-  )
+(defun keyval-setup ())
+
+(defvar keyval-key-face 'keyval-key-face
+  "Face for keys in KeyVal file.")
+(make-face keyval-key-face)
+(make-face-bold keyval-key-face)
+(set-face-foreground keyval-key-face "Cyan")
+
+(defvar keyval-classtype-face 'keyval-classtype-face
+  "Face for names of classes in KeyVal file.")
+(make-face keyval-classtype-face)
+(set-face-foreground keyval-classtype-face "Green")
+
+(defvar keyval-reference-face 'keyval-reference-face
+  "Face for references in KeyVal file.")
+(make-face keyval-reference-face)
+(set-face-foreground keyval-reference-face "Orange")
+(set-face-underline-p keyval-reference-face t)
+
+(defvar keyval-font-lock-keywords
+  '(("%.*" . font-lock-comment-face)
+    ("<.*>" . keyval-classtype-face)
+    ("\"[^\"\n]+\"" . font-lock-string-face)
+    ("$[A-Za-z0-9_\.:*+-/]*" . keyval-reference-face)
+    ("{ *\\([A-Za-z0-9_\.*+-/ ]*\\>\\) *} *=" (1 keyval-key-face))
+    ("\\([A-Za-z0-9_\.*+-/]*\\>\\) *=" (1 keyval-key-face))
+    )
+  "Default expressions to highlight in KeyVal mode.")
--- mpqc/lib/elisp/mpqc.el
+++ mpqc/lib/elisp/mpqc.el
@@ -18,8 +18,48 @@
   (setq major-mode 'mpqc-mode
 	mode-name "Mpqc")
   (mpqc-setup)
+  (make-local-variable 'font-lock-defaults)
+  (setq font-lock-defaults '(mpqc-font-lock-keywords t))
   (run-hooks 'mpqc-mode-hook))
 
 (defun mpqc-setup ()
   (toggle-read-only 1)
   )
+
+(defvar mpqc-success-face 'mpqc-success-face
+  "Face for usable mpqc output.")
+(make-face mpqc-success-face)
+(make-face-bold mpqc-success-face)
+(set-face-foreground mpqc-success-face "Cyan")
+
+(defvar mpqc-info-face 'mpqc-info-face
+  "Face for informational mpqc output.")
+(make-face mpqc-info-face)
+(make-face-bold mpqc-info-face)
+(set-face-foreground mpqc-info-face "Green")
+
+(defvar mpqc-warning-face 'mpqc-warning-face
+  "Face for warnings in MPQC output.")
+(make-face mpqc-warning-face)
+(set-face-foreground mpqc-warning-face "Orange")
+(set-face-underline-p mpqc-warning-face t)
+
+(defvar mpqc-font-lock-keywords
+  '(("converged scf energy.*" . mpqc-success-face)
+    ("Converged.*Internal Coordinates" . mpqc-success-face)
+    ("Nonconverged.*Internal Coordinates" . mpqc-warning-face)
+    ("Fixed.*Internal Coordinates" . mpqc-warning-face)
+    ("Initial.*Internal Coordinates" . mpqc-info-face)
+    ("Updated.*Internal Coordinates" . mpqc-info-face)
+    ("internal coordinates" . mpqc-info-face)
+    ("max of 1/2 idisp.*" . mpqc-info-face)
+    ("DEBUG.*" . mpqc-warning-face)
+    ("WARNING.*" . mpqc-warning-face)
+    ("NOTICE.*" . mpqc-warning-face)
+    ("Too many geometry.*" . mpqc-warning-face)
+    ("TORS" . mpqc-info-face)
+    ("BEND" . mpqc-info-face)
+    ("STRE" . mpqc-info-face)
+    ("OUTP" . mpqc-info-face)
+    )
+  "Default expressions to highlight in MPQC mode.")

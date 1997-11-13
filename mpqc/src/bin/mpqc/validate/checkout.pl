18ca982478fc30a510e564cfc261e3bbf9597edf
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1997-11-13 11:06:23 +0000

    Compare MP excitation amplitude information.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%402230

--- mpqc/src/bin/mpqc/validate/checkout.pl
+++ mpqc/src/bin/mpqc/validate/checkout.pl
@@ -7,6 +7,8 @@ require QCResult;
 my $file1 = shift;
 my $file2 = shift;
 
+my $log10 = log(10.0);
+
 # for AIX, which isn't processing the {,} in the argument
 if ($file1 =~ /(.*){(.*),(.*)}(.*)/) {
     $file1 = "$1$2$4";
@@ -25,7 +27,6 @@ sub check {
     my $file = $fileout;
     $file =~ s/\.out$//;
     my $filein = "$file.qci";
-    my $log10 = log(10.0);
 
     my $result = new QCResult("$filein","$fileout");
     my $ok = "failed";
@@ -75,14 +76,7 @@ sub check {
             printf " %s", $compareok;
             if ($cresult->ok() && $result->ok()) {
                 #printf " %14.8f %14.8f", $result->energy(),$cresult->energy();
-                my $diff = abs($result->energy()-$cresult->energy());
-                my $ldiff;
-                if ($diff == 0) {
-                    $ldiff = 99;
-                }
-                else {
-                    $ldiff = -log($diff)/$log10;
-                }
+                my $ldiff = compare_numbers($result->energy(),$cresult->energy());
                 printf " E:%2d", $ldiff;
                 print "*" if ($ldiff <= 6);
                 if ($result->input()->gradient()
@@ -105,6 +99,60 @@ sub check {
                     printf " Freq:% 2d", $maxerror;
                     print "*" if ($maxerror <= -2);
                 }
+                if ($result->s2norm() && $cresult->s2norm()) {
+                    my $maxerror = compare_numbers($result->s2norm(),
+                                                   $cresult->s2norm());
+                    printf " S2N:%d", $maxerror;
+                    print "*" if ($maxerror <= 8);
+                }
+                if ($result->s2large_coef() && $cresult->s2large_coef()) {
+                    my $maxerror = compare_vecs($result->s2large_coef(),
+                                                $cresult->s2large_coef());
+                    printf " S2L:%d", $maxerror;
+                    print "*" if ($maxerror <= 8);
+                    my $xok = compare_string_vecs($result->s2large_i(),
+                                                  $cresult->s2large_i())
+                        && compare_string_vecs($result->s2large_a(),
+                                               $cresult->s2large_a());
+                    #printf "coef\n";
+                    #print_vec($result->s2large_coef());
+                    #printf "i\n";
+                    #print_string_vec($result->s2large_i());
+                    #printf "a\n";
+                    #print_string_vec($result->s2large_a());
+                    if ($xok) { print " X:OK" }
+                    else { print " X:*" }
+                }
+                if ($result->d1large_coef() && $cresult->d1large_coef()) {
+                    my $maxerror = compare_vecs($result->d1large_coef(),
+                                                $cresult->d1large_coef());
+                    printf " D1L:%d", $maxerror;
+                    print "*" if ($maxerror <= 8);
+                    my $xok = compare_string_vecs($result->d1large_i(),
+                                                  $cresult->d1large_i())
+                        && compare_string_vecs($result->d1large_j(),
+                                               $cresult->d1large_j())
+                        && compare_string_vecs($result->d1large_a(),
+                                               $cresult->d1large_a())
+                        && compare_string_vecs($result->d1large_b(),
+                                               $cresult->d1large_b())
+                        && compare_string_vecs($result->d1large_spin(),
+                                               $cresult->d1large_spin());
+                    if ($xok) { print " X:OK" }
+                    else { print " X:*" }
+                    #printf "coef\n";
+                    #print_vec($result->d1large_coef());
+                    #printf "i\n";
+                    #print_string_vec($result->d1large_i());
+                    #printf "j\n";
+                    #print_string_vec($result->d1large_j());
+                    #printf "a\n";
+                    #print_string_vec($result->d1large_a());
+                    #printf "b\n";
+                    #print_string_vec($result->d1large_b());
+                    #printf "spin\n";
+                    #print_string_vec($result->d1large_spin());
+                }
             }
             else {
                 printf " cannot compare since one calc failed";
@@ -123,6 +171,20 @@ sub tofilename {
     $raw =~ s/-//g;
     $raw =~ s/\*/s/g;
     $raw;
+}
+
+sub compare_numbers {
+    my $num1 = shift;
+    my $num2 = shift;
+    my $diff = abs($num1-$num2);
+    my $ldiff;
+    if ($diff == 0) {
+        $ldiff = 99;
+    }
+    else {
+        $ldiff = -log($diff)/$log10;
+    }
+    $ldiff;
 }
 
 sub compare_vecs {
@@ -154,11 +216,40 @@ sub compare_vecs {
     $maxerror;
 }
 
+# returns 1 if the vecs are identical
+sub compare_string_vecs {
+    my $v1ref = shift;
+    my $v2ref = shift;
+    my @v1 = @{$v1ref};
+    my @v2 = @{$v2ref};
+    my $nv1 = @v1;
+    my $nv2 = @v2;
+    if ($nv1 != $nv2) {
+        printf "compare_vecs: vecs not of equal length\n";
+        exit 1;
+    }
+    my $e1, $e2;
+    while (($e1 = shift @v1)
+           &&($e2 = shift @v2)) {
+        if ($e1 ne $e2) { return 0; }
+    }
+    1;
+}
+
 sub print_vec {
     my $v1ref = shift;
     my @v1 = @{$v1ref};
     my $e1;
     while ($e1 = shift @v1) {
         printf " %12.8f\n", $e1;
+    }
+}
+
+sub print_string_vec {
+    my $v1ref = shift;
+    my @v1 = @{$v1ref};
+    my $e1;
+    while ($e1 = shift @v1) {
+        printf " %s\n", $e1;
     }
 }

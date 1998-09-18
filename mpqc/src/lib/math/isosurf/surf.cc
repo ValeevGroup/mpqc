396939c5a66a893f886bf6ef39ad14545d9d81c4
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1998-09-18 23:36:48 +0000

    Set the length of the vertex edge and triangle arrays before assigned to the arrays.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%402431

--- mpqc/src/lib/math/isosurf/surf.cc
+++ mpqc/src/lib/math/isosurf/surf.cc
@@ -683,16 +683,19 @@ TriangulatedSurface::recompute_index_maps()
   _index_to_edge.clear();
   _index_to_triangle.clear();
 
+  _index_to_vertex.set_length(_vertices.length());
   for (i=0, iv = _vertices.begin(); iv != _vertices.end(); i++, iv++) {
       _vertex_to_index[*iv] = i;
       _index_to_vertex[i] = *iv;
     }
 
+  _index_to_edge.set_length(_edges.length());
   for (i=0, ie = _edges.begin(); ie != _edges.end(); i++, ie++) {
       _edge_to_index[*ie] = i;
       _index_to_edge[i] = *ie;
     }
 
+  _index_to_triangle.set_length(_triangles.length());
   for (i=0, it = _triangles.begin(); it != _triangles.end(); i++, it++) {
       _triangle_to_index[*it] = i;
       _index_to_triangle[i] = *it;

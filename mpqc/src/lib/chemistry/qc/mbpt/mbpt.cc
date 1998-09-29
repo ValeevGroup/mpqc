c8c85662b35ced8415a6fb5ac346b8b376762851
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
1998-09-29 00:48:13 +0000

    Don't use an uninitialized variable in gradient_implemented.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%402441

--- mpqc/src/lib/chemistry/qc/mbpt/mbpt.cc
+++ mpqc/src/lib/chemistry/qc/mbpt/mbpt.cc
@@ -284,9 +284,10 @@ MBPT2::obsolete()
 int
 MBPT2::gradient_implemented() const
 {
+  int nb = basis()->nbasis();
   int n = 0;
 
-  for (int i=0; i<nbasis; i++) {
+  for (int i=0; i<nb; i++) {
     if (reference_->occupation(i) == 1.0) n++;
     }
 

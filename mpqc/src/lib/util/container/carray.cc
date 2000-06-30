a7bf9e17523563740852ac15dca05edb40eb7b2f
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
2000-06-30 19:35:05 +0000

    Added c-style 2 & 3 dimensional array new and delete helper functions.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%402807

---
+++ mpqc/src/lib/util/container/carray.cc
@@ -0,0 +1,19 @@
+
+#include <scconfig.h>
+#include <util/container/carray.h>
+
+#ifdef EXPLICIT_TEMPLATE_INSTANTIATION
+template void delete_c_array2<int>(int**);
+template int** new_c_array2<int>(int,int);
+template int** new_zero_c_array2<int>(int,int);
+template void delete_c_array3<int>(int***);
+template int*** new_c_array3<int>(int,int,int);
+template int*** new_zero_c_array3<int>(int,int,int);
+
+template void delete_c_array2<double>(double**);
+template double** new_c_array2<double>(int,int);
+template double** new_zero_c_array2<double>(int,int);
+template void delete_c_array3<double>(double***);
+template double*** new_c_array3<double>(int,int,int);
+template double*** new_zero_c_array3<double>(int,int,int);
+#endif

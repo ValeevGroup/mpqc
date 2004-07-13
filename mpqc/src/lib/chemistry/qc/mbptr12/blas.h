c857594e9da6ea18b3917eab781ecf657fee30ca
valeev@0fb12191-db4b-0410-8866-e78deaee71e5 <valeev@0fb12191-db4b-0410-8866-e78deaee71e5>
2004-07-13 18:03:01 +0000

    Added blas.h.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%403695

---
+++ mpqc/src/lib/chemistry/qc/mbptr12/blas.h
@@ -0,0 +1,7 @@
+extern "C" {
+#include <chemistry/qc/mbptr12/f77sym.h>
+extern void F77_DGEMM(const char*, const char*, const int*,
+const int*, const int*, const double*, const double*, const int*,
+const double*, const int*, const double*, double*, const int*);
+}
+

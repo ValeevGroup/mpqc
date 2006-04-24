936e8425a2cf8d94cc6b2a1220f9ed16f3780c13
evaleev@0fb12191-db4b-0410-8866-e78deaee71e5 <evaleev@0fb12191-db4b-0410-8866-e78deaee71e5>
2006-04-24 15:55:10 +0000

    Detect abab case.
    --HG--
    branch : libint2
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/branches/Libint2-branch%407916

--- mpqc/src/lib/chemistry/qc/basis/gpetite.cc
+++ mpqc/src/lib/chemistry/qc/basis/gpetite.cc
@@ -181,6 +181,10 @@ sc::construct_gpetite(const Ref<GaussianBasisSet> &b1,
     canonical_aaaa c4(b1,b2,b3,b4);
     return new GPetite4<canonical_aaaa>(b1,b2,b3,b4,c4);
   }
+  else if (b1 == b3 && b2 == b4) {
+    canonical_abab c4(b1,b2,b3,b4);
+    return new GPetite4<canonical_abab>(b1,b2,b3,b4,c4);
+  }
   else if (b1 == b2 && b3 != b4) {
     canonical_aabc c4(b1,b2,b3,b4);
     return new GPetite4<canonical_aabc>(b1,b2,b3,b4,c4);

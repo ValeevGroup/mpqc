a36ebaeab681251b0480073f8059d5f676ffdd1b
jpkenny@0fb12191-db4b-0410-8866-e78deaee71e5 <jpkenny@0fb12191-db4b-0410-8866-e78deaee71e5>
2006-05-12 18:41:02 +0000

    mods for cca reorg
    --HG--
    branch : libint2
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/branches/Libint2-branch%407943

--- mpqc/src/bin/mpqc/LIBS.h
+++ mpqc/src/bin/mpqc/LIBS.h
@@ -15,13 +15,11 @@
 #  include <chemistry/qc/libint2/LIBS.h>
 #endif
 #ifdef HAVE_SC_SRC_LIB_CHEMISTRY_CCA
-#  include <chemistry/qc/intcca/LIBS.h>
+#  include <chemistry/cca/int/LIBS.h>
+#  include <chemistry/cca/server/LIBS.h>
 #  include <util/misc/LIBS.h>
 #endif
 #include <chemistry/qc/mbpt/LIBS.h>
 #include <chemistry/qc/dft/LIBS.h>
 #include <chemistry/qc/scf/LIBS.h>
 #include <util/options/LIBS.h>
-#ifdef HAVE_SC_SRC_LIB_CHEMISTRY_CCA
-  #include <chemistry/cca/LIBS.h>
-#endif

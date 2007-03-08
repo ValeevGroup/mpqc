8ebbc037b674d4c25d13f55314272ec297d21c6e
None@0fb12191-db4b-0410-8866-e78deaee71e5 <None@0fb12191-db4b-0410-8866-e78deaee71e5>
2007-03-08 21:16:37 +0000

    This commit was manufactured by cvs2svn to create tag 'libint2-branch-before-f12tf12fix'.
    --HG--
    branch : libint2
    extra : close : 1
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/tags/libint2-branch-before-f12tf12fix%408178

--- mpqc/src/lib/util/render/LIBS.h
+++
@@ -1,5 +0,0 @@
-libSCrender.LIBSUF
-#include <util/misc/LIBS.h>
-#include <util/keyval/LIBS.h>
-#include <util/class/LIBS.h>
-#include <util/container/LIBS.h>
--- mpqc/src/lib/util/state/LIBS.h
+++
@@ -1,4 +0,0 @@
-libSCstate.LIBSUF
-#include <util/keyval/LIBS.h>
-#include <util/class/LIBS.h>
-#include <util/container/LIBS.h>

371d0fdffe2d173e4373a3f41df16bdfdc6ef3c6
None@0fb12191-db4b-0410-8866-e78deaee71e5 <None@0fb12191-db4b-0410-8866-e78deaee71e5>
2007-09-05 16:17:24 +0000

    This commit was manufactured by cvs2svn to create tag 'libint2-branch-ccsd-pt2r12'.
    --HG--
    branch : libint2
    extra : close : 1
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/tags/libint2-branch-ccsd-pt2r12%408372

--- mpqc/src/lib/chemistry/cca/int/LIBS.h
+++
@@ -1,6 +0,0 @@
-libSCccaint.LIBSUF
-#include <util/keyval/LIBS.h>
-#include <util/misc/LIBS.h>
-#include <chemistry/qc/basis/LIBS.h>
-#include <chemistry/qc/intv3/LIBS.h>
-#include <chemistry/cca/client/LIBS.h>
--- mpqc/src/lib/util/ref/LIBS.h
+++
@@ -1,1 +0,0 @@
-libSCref.LIBSUF

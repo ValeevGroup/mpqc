98cbd00efa239fb1bb7d114576ebc305f107a7fe
cljanss@0fb12191-db4b-0410-8866-e78deaee71e5 <cljanss@0fb12191-db4b-0410-8866-e78deaee71e5>
2009-02-22 17:59:24 +0000

    several documentation improvements
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%409044

--- mpqc/src/lib/util/options/GetLongOpt.h
+++ mpqc/src/lib/util/options/GetLongOpt.h
@@ -13,8 +13,11 @@
 
 namespace sc {
 
+/// Parse command line options.
 class GetLongOpt {
 public:
+  /// Used by the enroll member to specify whether or not a value is
+  /// expected or optional.
    enum OptType { 
       NoValue, OptionalValue, MandatoryValue
    };
@@ -41,17 +44,40 @@ private:
   char *basename(char * const p) const;
   int setcell(Cell *c, const char *valtoken, const char *nexttoken, const char *p);
 public:
+   /// Initialize the object.
+   /// @param optmark the option flag marker (default is <tt>-</tt>).
    GetLongOpt(const char optmark = '-');
    ~GetLongOpt();
 
+   /// Parse command line options.
+   /// @param argc the number of arguments, as passed to <tt>main</tt>
+   /// @param argv the arguments, as passed to <tt>main</tt>
+   /// @return the index to the start of arguments that were not
+   ///         processed (an error occurred if the return value is < 1)
    int parse(int argc, char * const *argv);
+   /// Parse options in a string.
+   /// @param str the string to be parsed
+   /// @param p a prefix that will be prefixed to error messages
+   /// @return the index to the start of arguments that were not
+   ///         processed (an error occurred if the return value is < 1)
    int parse(char * const str, char * const p);
 
+   /// Enroll an option.
+   /// @param opt the option name
+   /// @param t whether or not a value is expected
+   /// @param desc a description of the option
+   /// @param val a default value for the option with an optional value
    int enroll(const char * const opt, const OptType t,
       const char * const desc, const char * const val);
+   /// Retrieve an option.
+   /// @param opt the name of the option
    const char *retrieve(const char * const opt) const;
 
+   /// Print usage information.
+   /// @param outfile stream to use for printing (default: <tt>std::cout</tt>)
    void usage(std::ostream &outfile = std::cout) const;
+   /// Initialize usage synopsis.
+   /// @param str the usage synopsis
    void usage(const char *str)		{ ustring = str; }
 };
 

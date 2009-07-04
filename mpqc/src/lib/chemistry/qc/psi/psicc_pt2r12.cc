eaecfbcb5bf6cef1138372bf99387a40839ad04d
evaleev@0fb12191-db4b-0410-8866-e78deaee71e5 <evaleev@0fb12191-db4b-0410-8866-e78deaee71e5>
2009-07-04 15:06:31 +0000

    Raised print level for MPQC->Psi orbital transform matrices.
    --HG--
    extra : convert_revision : svn%3A0fb12191-db4b-0410-8866-e78deaee71e5/trunk%409162

--- mpqc/src/lib/chemistry/qc/psi/psicc_pt2r12.cc
+++ mpqc/src/lib/chemistry/qc/psi/psicc_pt2r12.cc
@@ -346,7 +346,7 @@ void PsiCCSD_PT2R12::compute() {
                                         localkit);
     MPQC2PSI_tform_va[spin] = transform(*vir_act_sb_psi, *vir_act,
                                         localkit);
-    if (debug() >= DefaultPrintThresholds::diagnostics) {
+    if (debug() >= DefaultPrintThresholds::mostN2) {
       MPQC2PSI_tform_oa[spin].print(prepend_spincase(spin,"MPQC->Psi3 transform for active occupied orbitals").c_str());
       MPQC2PSI_tform_va[spin].print(prepend_spincase(spin,"MPQC->Psi3 transform for active virtual orbitals").c_str());
     }

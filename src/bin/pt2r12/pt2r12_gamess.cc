ae022301af22957402f1f3c17c05a4735adad540
evaleev <eduard@valeyev.net>
2011-07-08 10:53:15 -0400

    Added code to test bootstrapping PT2R12 code.

--- src/bin/pt2r12/pt2r12_gamess.cc
+++ src/bin/pt2r12/pt2r12_gamess.cc
@@ -5,9 +5,9 @@
 #include <chemistry/qc/basis/orthog.h>
 #include <chemistry/qc/basis/files.h>
 #include <chemistry/qc/intv3/intv3.h>
-#include <chemistry/qc/lcao/fockbuild.h>
-#include <chemistry/qc/lcao/fockdist.h>
-#include <chemistry/qc/lcao/clhfcontrib.h>
+#include <chemistry/qc/lcao/wfnworld.h>
+#include <chemistry/qc/mbptr12/ref.h>
+#include <chemistry/qc/scf/clhf.h>
 #include <math/optimize/scextrap.h>
 #include <math/optimize/diis.h>
 #include <math/optimize/scextrapmat.h>
@@ -33,7 +33,7 @@ int main_gamess(int argc, char **argv)
   int optind = opt.parse(argc, argv);
   std::string inputfile;
   if (argc - optind == 0) {
-    inputfile = "hf.in";
+    inputfile = "pt2r12.in";
   }
   else if (argc - optind == 1) {
     inputfile = argv[optind];
@@ -48,6 +48,7 @@ int main_gamess(int argc, char **argv)
   {
     molecule->add_atom(8, 0.0, 0.0, 0.0);
   }
+  molecule->set_point_group(new PointGroup("d2h"));
 
   // initialize basis set given name
   Ref<GaussianBasisSet> basis;
@@ -59,12 +60,10 @@ int main_gamess(int argc, char **argv)
     basis = new GaussianBasisSet(kv);
   }
 
-
-  Ref<sc::KeyVal> keyval = init.init(inputfile);
-
+  // print environment
   Ref<sc::ThreadGrp> thr = sc::ThreadGrp::get_default_threadgrp();
   Ref<sc::MessageGrp> msg = sc::MessageGrp::get_default_messagegrp();
-
+  Ref<sc::Integral> integral = sc::Integral::get_default_integral()->clone();
   if (opt.retrieve("verbose")) {
     sc::ExEnv::out0() << indent
                       << "nthread = " << thr->nthread() << std::endl;
@@ -72,142 +71,60 @@ int main_gamess(int argc, char **argv)
                       << "nnode   = " << msg->n() << std::endl;
   }
 
-  //Ref<GaussianBasisSet> basis;
-  //basis << keyval->describedclassvalue("basis");
-
-  // make an integral factory and get the petite list
-  Ref<Integral> integral = new IntegralV3(basis);
-  Ref<PetiteList> pl = integral->petite_list();
-  integral->set_storage(32000000);
-  Integral::set_default_integral(integral);
-
-  // construct the overlap matrix, s
-  RefSymmSCMatrix s_skel(basis->basisdim(), basis->matrixkit());
-  Ref<SCElementOp> s_op
-      = new OneBodyIntOp(new SymmOneBodyIntIter(integral->overlap(), pl));
-
-  s_skel.assign(0.0);
-  s_skel.element_op(s_op);
-  s_op=0;
-
-  RefSymmSCMatrix s_SO(pl->SO_basisdim(), basis->so_matrixkit());
-  pl->symmetrize(s_skel,s_SO);
-
-  // construct the basis set orthogonalizer
-  Ref<OverlapOrthog> orthog
-      = new OverlapOrthog(OverlapOrthog::Symmetric,
-                          s_SO,
-                          basis->so_matrixkit(),
-                          1.0e-6 /*lindep tolerance*/,
-                          0 /* debug */
-          );
-
-  // construct the core hamiltonian (nuclear attraction + kinetic energy)
-  RefSymmSCMatrix hcore_skel(basis->basisdim(), basis->matrixkit());
-  Ref<SCElementOp> hcore_op
-      = new OneBodyIntOp(new SymmOneBodyIntIter(integral->hcore(), pl));
-
-  hcore_skel.assign(0.0);
-  hcore_skel.element_op(hcore_op);
-  hcore_op=0;
-
-  RefSymmSCMatrix hcore_SO(pl->SO_basisdim(), basis->so_matrixkit());
-  pl->symmetrize(hcore_skel,hcore_SO);
-
-  RefSymmSCMatrix density_AO(pl->AO_basisdim(), basis->so_matrixkit());
-  density_AO.assign(0.0);
-
-  RefSCMatrix vector_OSO(pl->SO_basisdim(), orthog->orthog_dim(),
-                         basis->so_matrixkit());
-  vector_OSO.assign(0.0);
-
-  RefDiagSCMatrix evals(orthog->orthog_dim(), basis->so_matrixkit());
-
-  RefSymmSCMatrix density_SO(pl->SO_basisdim(), basis->so_matrixkit());
-
-  Ref<SelfConsistentExtrapolation> extrap = new DIIS;
-  extrap->set_tolerance(1.0e-6);
-
-  // read the occupation numbers
-  RefSymmSCMatrix occ(orthog->orthog_dim(), basis->so_matrixkit());
-  occ.assign(0.0);
-  for (int i=0; i<orthog->orthog_dim(); i++) {
-      occ(i,i) = keyval->doublevalue("occ",i);
+#define TEST_R12_INFRASTRUCTURE 1
+#if TEST_R12_INFRASTRUCTURE
+  {
+    // create CLHF object
+    Ref<CLHF> clhf;
+    {
+      Ref<AssignedKeyVal> akv = new AssignedKeyVal;
+      akv->assign("molecule", molecule.pointer());
+      akv->assign("basis", basis.pointer());
+      Ref<KeyVal> kv = akv;
+      clhf = new CLHF(kv);
     }
 
-  // construct the initial guess to vector_OSO and density_AO
-  RefSymmSCMatrix hcore_OSO(orthog->orthog_dim(), basis->so_matrixkit());
-  hcore_OSO.assign(0.0);
-  hcore_OSO.accumulate_transform(orthog->basis_to_orthog_basis(),hcore_SO);
-  hcore_OSO.diagonalize(evals, vector_OSO);
+    // compute CLHF object
+    sc::ExEnv::out0() << "Energy = " << clhf->energy() << std::endl;
 
-  density_SO.assign(0.0);
-  RefSCMatrix vector_SO = orthog->basis_to_orthog_basis().t() * vector_OSO;
-  density_SO.accumulate_transform(vector_SO, occ);
-  density_AO = pl->to_AO_basis(density_SO);
-
-  while (!extrap->converged()) {
-      //while (!extrap->converged()) {
-      // construct the G matrix
-      RefSymmSCMatrix g_skel(basis->basisdim(), basis->matrixkit());
-      g_skel.assign(0.0);
-      Ref<FockContribution> g_contrib
-          = new CLHFContribution(basis,basis,basis,"replicated");
-      g_contrib->set_fmat(0, g_skel);
-      g_contrib->set_pmat(0, density_AO);
-      Ref<FockDistribution> fockdist = new FockDistribution;
-      Ref<FockBuild> fb = new FockBuild(fockdist, g_contrib, false,
-                                        basis, basis, basis);
-      fb->set_accuracy(1e-12);
-      fb->build();
-  
-      g_skel.scale(1.0/(double)pl->order());
-
-      RefSymmSCMatrix g_SO(pl->SO_basisdim(), basis->so_matrixkit());
-      pl->symmetrize(g_skel,g_SO);
-
-      // compute the exchange/correlation potential
-      RefSymmSCMatrix vxc_SO;
-      double exc = 0.0;
-
-      // construct the fock matrix
-      RefSymmSCMatrix f_SO = hcore_SO + g_SO;
-
-      // transform the fock matrix into the orthogonal basis
-      RefSymmSCMatrix f_OSO(orthog->orthog_dim(), basis->so_matrixkit());
-      f_OSO.assign(0.0);
-      f_OSO.accumulate_transform(orthog->basis_to_orthog_basis(),f_SO);
-
-      // Extrapolate the fock matrix
-      Ref<SCExtrapData> data = new SymmSCMatrixSCExtrapData(f_OSO);
-      RefSymmSCMatrix error_MO(orthog->orthog_dim(), basis->so_matrixkit());
-      error_MO.assign(0.0);
-      error_MO.accumulate_transform(vector_OSO.t(),f_OSO);
-      error_MO->scale_diagonal(0.0);
-  
-      RefSymmSCMatrix error_SO(pl->SO_basisdim(), basis->so_matrixkit());
-      error_SO.assign(0.0);
-      error_SO.accumulate_transform(orthog->basis_to_orthog_basis().t()
-                                    *vector_OSO, error_MO);
-      Ref<SCExtrapError> error = new SymmSCMatrixSCExtrapError(error_SO);
-      extrap->extrapolate(data,error);
-
-      // Diagonalize the fock matrix
-      f_OSO.diagonalize(evals, vector_OSO);
-
-      // Compute the density matrix
-      density_SO.assign(0.0);
-      vector_SO = orthog->basis_to_orthog_basis().t() * vector_OSO;
-      density_SO.accumulate_transform(vector_SO, occ);
-      density_AO = pl->to_AO_basis(density_SO);
-
-      double energy =exc + basis->molecule()->nuclear_repulsion_energy()
-                     +(density_SO * 0.5 * (hcore_SO + f_SO)).trace();
-
-      ExEnv::out0() << indent
-                    << scprintf("energy = %14.8f, error = %10.8f",
-                                energy, extrap->error()) << std::endl;
+    // grab orbitals and their symmetries
+    RefSCMatrix C_so = clhf->eigenvectors();
+    integral->set_basis(basis);
+    Ref<PetiteList> pl = integral->petite_list();
+    RefSCMatrix C_ao = pl->evecs_to_AO_basis(C_so);
+    std::vector<unsigned int> orbsym(C_ao.coldim().n(), 0);
+    {
+      Ref<SCBlockInfo> blocks = C_ao.coldim()->blocks();
+      if (blocks.nonnull()) {
+        const unsigned int nirreps = blocks->nblock();
+        for(unsigned int irrep=0; irrep<nirreps; ++irrep) {
+          const unsigned int first_orb_in_irrep = blocks->start(irrep);
+          const unsigned int norbs_in_irrep = blocks->size(irrep);
+          const unsigned int plast_orb_in_irrep = first_orb_in_irrep + norbs_in_irrep;
+          for(unsigned int o=first_orb_in_irrep; o<plast_orb_in_irrep; ++o) {
+            orbsym[o] = irrep;
+          }
+        }
+      }
     }
 
+    // grab densities
+    RefSymmSCMatrix P_so = clhf->density(); P_so.scale(0.5);
+    RefSymmSCMatrix P_mo = P_so.kit()->symmmatrix(C_ao.coldim());  P_mo.assign(0.0);
+    P_mo.accumulate_transform(clhf->mo_to_so(), P_so, SCMatrix::TransposeTransform);
+    P_mo.print("MO density");
+
+    // create World in which we will compute
+    // use defaults for all params
+    Ref<WavefunctionWorld> world = new WavefunctionWorld(Ref<KeyVal>(new AssignedKeyVal), clhf);
+
+    // use its orbitals to initialize Extern_RefWavefunction
+    Ref<RefWavefunction> ref_wfn = new Extern_RefWavefunction(world, basis, integral,
+                                                              C_ao, orbsym,
+                                                              P_mo, P_mo,
+                                                              clhf->nelectron()/2, 3, 1);
+  }
+#endif
+  
   return 0;
 }

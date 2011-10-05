266b2c85b8ce1de8c4b860ed3a7bed162f7fc991
cljanss <curtis.janssen@gmail.com>
2011-10-05 16:14:59 -0700

    Fixed a missing library dependency which could result in undefined symbols when linking MPQC on some OSes.

--- src/lib/chemistry/qc/scf/LIBS.h
+++ src/lib/chemistry/qc/scf/LIBS.h
@@ -3,6 +3,7 @@ libSCscf.LIBSUF
 #ifdef HAVE_SC_SRC_LIB_CHEMISTRY_QC_CINTS
 #  include <chemistry/qc/cints/LIBS.h>
 #endif
+#include <chemistry/qc/lcao/LIBS.h>
 #include <chemistry/qc/intv3/LIBS.h>
 #include <chemistry/qc/wfn/LIBS.h>
 #include <chemistry/molecule/LIBS.h>

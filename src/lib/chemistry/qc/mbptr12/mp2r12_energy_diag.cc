36e45d6841428abbf36d32ec07f5f17dc735ca3f
jinmeiz <jmzhang@vt.edu>
2012-01-20 20:15:31 -0500

    comment out compute_density_diag() since it is not finished yet

--- src/lib/chemistry/qc/mbptr12/mp2r12_energy_diag.cc
+++ src/lib/chemistry/qc/mbptr12/mp2r12_energy_diag.cc
@@ -513,7 +513,7 @@ void MP2R12Energy_Diag::contract_VT1(const Ref<DistArray4>& V,
 void MP2R12Energy_Diag::compute_ef12() {
 
   // calculate one electron density
-  compute_density_diag();
+//  compute_density_diag();
 
   // switch to new implementation that should work correctly for alpha-beta contributions in open-shell molecules
   return this->compute_ef12_10132011();

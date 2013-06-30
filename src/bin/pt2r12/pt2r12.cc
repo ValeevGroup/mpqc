2b95547297134f99c2e88d8eb2c49d4badd8583b
evaleev <eduard@valeyev.net>
2013-06-30 15:02:49 -0400

    1) MBPT2_R12 now runs ... at least for a couple of test cases 2) MADNESS+TA-based code also seems to work now.
    IN PROGRESS - issue MPQC-66: Port all features of autoconf+Makefile build to the new cmake-based build
    http://valeev.chem.vt.edu:8080/browse/MPQC-66

--- src/bin/pt2r12/pt2r12.cc
+++ src/bin/pt2r12/pt2r12.cc
@@ -37,11 +37,9 @@
 #endif
 #include <chemistry/qc/wfn/linkage.h>
 #include <chemistry/qc/scf/linkage.h>
-#ifdef HAVE_SC_SRC_LIB_CHEMISTRY_QC_MBPTR12
-#  include <chemistry/qc/mbptr12/linkage.h>
-#endif
-#ifdef HAVE_SC_SRC_LIB_CHEMISTRY_QC_LIBINT2
+#ifdef HAVE_LIBINT2
 #  include <chemistry/qc/libint2/linkage.h>
+#  include <chemistry/qc/mbptr12/linkage.h>
 #endif
 #include <util/state/linkage.h>
 
@@ -53,7 +51,7 @@
 #endif
 
 // MUST HAVE LIBINT2
-#if HAVE_INTEGRALLIBINT2
+#ifdef HAVE_LIBINT2
 #  include <chemistry/qc/libint2/libint2.h>
 #else
 #  error "this copy of MPQC does not include the Libint2 library (see libint.valeyev.net) -- cannot use F12 methods"
@@ -168,7 +166,7 @@ int try_main(int argc, char **argv)
   init.init_io(grp);
   Ref<ThreadGrp> thread = init.init_threadgrp();
   Ref<MemoryGrp> memory = init.init_memorygrp();
-#if HAVE_INTEGRALLIBINT2
+#ifdef HAVE_LIBINT2
   Integral::set_default_integral(new IntegralLibint2);
 #endif
   init.init_integrals();

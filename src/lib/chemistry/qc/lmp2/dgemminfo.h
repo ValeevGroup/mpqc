5f6e16fe411cac8f0b9aee41a5c2f7bd78e6c2e8
Andrey Asadchev <asadchev@gmail.com>
2013-07-30 12:13:40 -0400

    Fixed MPI,HDF5 builds Added mpqc.py run program Fixed sys includes in lmp2

--- src/lib/chemistry/qc/lmp2/dgemminfo.h
+++ src/lib/chemistry/qc/lmp2/dgemminfo.h
@@ -25,40 +25,43 @@
 #ifndef _chemistry_qc_lmp2_dgemminfo_h
 #define _chemistry_qc_lmp2_dgemminfo_h
 
+#ifndef USE_MICROTIME
+#include <sys/time.h>
+#include <time.h>
+#endif
+
 namespace sc {
 
 #ifdef USE_MICROTIME
-static inline void microtime(unsigned *lo, unsigned *hi)
-{
-  __asm __volatile (
-        ".byte 0x0f; .byte 0x31; movl    %%edx,%0; movl    %%eax,%1"
-                : "=g" (*hi), "=g" (*lo) :: "eax", "edx");
-}
-static inline double cpu_walltime(void)
-{
-  unsigned lo, hi;
-  microtime(&lo, &hi);
-  unsigned long long ticks = lo + (((unsigned long long)hi)<<32);
-  double secs = ticks*(1.0/(1496.612*1.0e6));
-  return secs;
-}
+
+  static inline void microtime(unsigned *lo, unsigned *hi) {
+    __asm __volatile (
+		      ".byte 0x0f; .byte 0x31; movl    %%edx,%0; movl    %%eax,%1"
+		      : "=g" (*hi), "=g" (*lo) :: "eax", "edx");
+  }
+
+  static inline double cpu_walltime(void) {
+    unsigned lo, hi;
+    microtime(&lo, &hi);
+    unsigned long long ticks = lo + (((unsigned long long)hi)<<32);
+    double secs = ticks*(1.0/(1496.612*1.0e6));
+    return secs;
+  }
+
 #else
 
-#include <sys/time.h>
-#include <time.h>
+  /// Returns the time in seconds.
+  static inline double cpu_walltime(void) {
+    struct timeval tod;
+    gettimeofday(&tod,0);
+    return tod.tv_sec + 0.000001 * tod.tv_usec;
+  }
 
-/// Returns the time in seconds.
-static inline double cpu_walltime(void)
-{
-  struct timeval tod;
-  gettimeofday(&tod,0);
-  return tod.tv_sec + 0.000001 * tod.tv_usec;
-}
 #endif
 
-/// Records information about the time take to perform a DGEMM operation.
-extern void count_dgemm(int n, int l, int m, double t);
-
+  /// Records information about the time take to perform a DGEMM operation.
+  extern void count_dgemm(int n, int l, int m, double t);
+  
 }
 
 #endif

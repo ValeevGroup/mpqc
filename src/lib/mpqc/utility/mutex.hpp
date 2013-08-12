40ded377ebf8667d1465e5d03fa77c3fa31132c5
evaleev <eduard@valeyev.net>
2013-08-12 12:07:38 -0400

    Added debugging code to global mutex.

--- src/lib/mpqc/utility/mutex.hpp
+++ src/lib/mpqc/utility/mutex.hpp
@@ -2,6 +2,7 @@
 #define MPQC_UTILITY_MUTEX_HPP
 
 #include <boost/thread/mutex.hpp>
+#include <util/misc/exenv.h>
 
 namespace mpqc {
 
@@ -9,18 +10,48 @@ namespace mpqc {
     /// @{
 
     /// Static mutex factory
-    template<typename>
+    template<typename T>
     struct static_mutex {
-        static void lock() { get().lock(); }
-        static void unlock() { get().unlock(); }
+        static const bool debug = false; // change to true
+        static void lock() {
+          if (debug) {
+            std::ostringstream oss;
+            oss << "entered mpqc::static_mutex<>::lock(): count = " << lock_count << std::endl;
+            sc::ExEnv::out0() << oss.str();
+          }
+          get().lock();
+          if (debug) {
+            ++lock_count;
+            std::ostringstream oss;
+            oss << "completed mpqc::static_mutex<>::lock(): count = " << lock_count << std::endl;
+            sc::ExEnv::out0() << oss.str();
+          }
+        }
+        static void unlock() {
+          if (debug) {
+            std::ostringstream oss;
+            oss << "entered mpqc::static_mutex<>::unlock(): count = " << lock_count << std::endl;
+            sc::ExEnv::out0() << oss.str();
+          }
+          get().unlock();
+          if (debug) {
+            std::ostringstream oss;
+            oss << "completed mpqc::static_mutex<>::unlock(): count = " << lock_count << std::endl;
+            sc::ExEnv::out0() << oss.str();
+          }
+        }
         static boost::mutex& get() {
             return mutex;
         }
-	static boost::mutex mutex;
+
+        static boost::mutex mutex;
+        static int64_t lock_count;
     };
 
     template<typename T>
     boost::mutex static_mutex<T>::mutex;
+    template<typename T>
+    int64_t static_mutex<T>::lock_count = int64_t(0);
     
     /// Static mutex instances.
     /// Example:
@@ -30,7 +61,7 @@ namespace mpqc {
     /// mutex::global::unlock()
     /// @endcode
     struct mutex {
-        struct global_mutex_tag;
+        struct global_mutex_tag {};
         typedef static_mutex<global_mutex_tag> global;
     };
 

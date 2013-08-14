90c8c0a87a7b2b77f15f96fa7300b504073ae71f
Andrey Asadchev <asadchev@gmail.com>
2013-08-13 20:25:19 -0400

    Added tensor to Eigen::Matrix cast

---
+++ src/lib/mpqc/tensor/expression.hpp
@@ -0,0 +1,12 @@
+#ifndef MPQC_TENSOR_EXPRESSION_HPP
+#define MPQC_TENSOR_EXPRESSION_HPP
+
+#include "mpqc/tensor/forward.hpp"
+
+namespace mpqc {
+
+
+} // mpqc
+
+
+#endif /* MPQC_TENSOR_EXPRESSION_HPP */

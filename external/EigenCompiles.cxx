54b9e4f39c6a9caba2be6b8562731fa571512074
Jinmei Zhang <gjmzhang@gmailc.om>
2013-08-25 12:13:26 -0400

    Eigen compile test code is now correct, so the test will actually pass.

--- external/EigenCompiles.cxx
+++ external/EigenCompiles.cxx
@@ -2,18 +2,11 @@
 #include <Eigen/Dense>
 #include <iostream>
 
-typedef Eigen::MatrixXd Matrix;
-
-template<class T>
-void test() {
-  Matrix m = Matrix::Random(5, 5);
-  Eigen::MatrixXd b = Matrix::Random(5, 5);
-  m.row(0) = b.col(0);
-  Eigen::SelfAdjointEigenSolver<Matrix> eig(m+m.transpose());
-  Matrix m_invsqrt = eig.operatorInverseSqrt();
+int main(int argc, char* argv[]){
+  Eigen::MatrixXd m = Eigen::MatrixXd::Random(5, 5);
+  m = m.transpose() + m;
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eig(m);
+  Eigen::MatrixXd m_invsqrt = eig.operatorInverseSqrt();
   std::cout << m_invsqrt << std::endl;
-}
-
-int main() {
-  test<void>();
+  return 0;
 }

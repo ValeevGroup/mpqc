4454e01b0703fb8a9db4fb849404521c79dec439
Andrey Asadchev <asadchev@gmail.com>
2013-09-11 20:14:06 -0400

    Resolved few after-merge issues, appears to be functioning correctly

--- src/lib/mpqc/ci/full/vector.hpp
+++ src/lib/mpqc/ci/full/vector.hpp
@@ -1,100 +1,110 @@
-#ifndef MPQC_CI_FULL_ARRAY_HPP
-#define MPQC_CI_FULL_ARRAY_HPP
+#ifndef MPQC_CI_FULL_VECTOR_HPP
+#define MPQC_CI_FULL_VECTOR_HPP
 
+#include "mpqc/ci/full/ci.hpp"
+#include "mpqc/ci/vector.hpp"
 #include "mpqc/array.hpp"
-#include "mpqc/ci/full.hpp"
-#include "mpqc/mpi.hpp"
 
 namespace mpqc {
 namespace ci {
 
+    template<class Index>
+    struct Vector< Full<Index> > {
 
-    template<>
-    struct Array<FullCI> {
-        Array(const std::string &name, size_t m, size_t n, MPI::Comm comm)
-            : data_(name, extents(m,n), comm),
-              alpha_(m), beta_(n)
-              
+        typedef mpqc::Array<double> Array;
+
+        Vector(std::string name, const CI< Full<Index> > &ci)
+            : array_(name, dims(ci), ci.comm),
+              alpha_(ci.alpha.size()),
+              beta_(ci.beta.size())
         {
-            //std::cout << "m = " << m << ", n = " << n << std::endl;
         }
 
-        struct Vector {
-            // not correct in parallel runs, fix it
-            Vector operator()(range r) {
-                MPQC_CHECK(r.size() < r_.size());
-                auto begin = r_.front();
-                return Vector(data_, alpha_, beta_,
-                              range(r.front() + begin, r.back()+1+begin));
-            }
+        struct Slice {
             size_t size() const {
-                return r_.size();
-            }
-            void read(mpqc::File::Dataspace<double> ds) {
-                mpqc::Vector v(this->size());
-                ds.read(v.data());
-                this->put(v);
-            }
-            void write(mpqc::File::Dataspace<double> ds) {
-                const mpqc::Vector &v = this->get();
-                ds.write(v.data());
+                return range1d_.size();
             }
             operator mpqc::Vector() const {
-                return this->get();
-            }
-            void put(const mpqc::Vector &v) {
-                MPQC_CHECK(v.size() == this->size());
-                data_(r2()).put(v.data());
-            }
-            mpqc::Vector get() const {
                 mpqc::Vector v(this->size());
-                data_(r2()).get(v.data());
+                array_.get(v.data());
                 return v;
             }
-        private:
-            friend class Array;
-            Vector(mpqc::Array<double> data, size_t alpha, size_t beta, range r)
-                : data_(data), alpha_(alpha), beta_(beta), r_(r)
-            {
-                //std::cout << "range r = " << r << std::endl;
+            void read(mpqc::File::Dataspace<double> ds) {
+                io<READ>(ds);
             }
-            std::vector<range> r2() const {
-                MPQC_CHECK(r_.front()%alpha_ == 0);
-                MPQC_CHECK(r_.size()%alpha_ == 0);
+
+            void write(mpqc::File::Dataspace<double> ds) {
+                io<WRITE>(ds);
+            }
+
+            void put(const mpqc::Vector &v) {
+                MPQC_CHECK(v.size() == this->size());
+                array_.put(v.data());
+            }
+
+        private:
+            enum { READ = -1, WRITE = 1 };
+            template<int OP>
+            void io(mpqc::File::Dataspace<double> ds) {
+                range alpha = this->range2d_[0];
+                range beta = this->range2d_[1];
+                // N.B. using buffer of 8MB, make adjustable
+                size_t B = std::max<size_t>((1<<20)/alpha.size(), 1);
+                double *buffer = new double[alpha.size()*B];
+                auto begin = this->range1d_.front();
+                foreach (auto b, beta.block(B)) {
+                    size_t size = alpha.size()*b.size();
+                    range r = range(begin, begin+size);
+                    if (OP == READ) {
+                        ds(r).read(buffer);
+                        array_(alpha, b).put(buffer);
+                    }
+                    if (OP == WRITE) {
+                        array_(alpha, b).get(buffer);
+                        ds(r).write(buffer);
+                    }
+                    begin += size;
+                }
+                delete [] buffer;
+            }
+
+        private:
+            friend class Vector;
+            range range1d_;
+            std::vector<range> range2d_;
+            Array array_;
+            Slice(Vector &v, range r)
+                : range1d_(r), range2d_(range2d(v, r)),
+                  array_(v.array_(range2d_)) {}
+            static std::vector<range> range2d(const Vector &v, range r) {
+                size_t alpha = v.alpha_;
+                size_t beta = v.beta_;
+                MPQC_CHECK(r.front()%alpha == 0);
+                MPQC_CHECK(r.size()%alpha == 0);
                 std::vector<range> r2;
-                r2.push_back(range(r_.front()%beta_, 1+r_.back()%beta_));
-                r2.push_back(range(r_.front()/beta_, 1+r_.back()/beta_));
+                r2.push_back(range(r.front()%beta, 1+r.back()%beta));
+                r2.push_back(range(r.front()/beta, 1+r.back()/beta));
                 return r2;
-                //std::cout << "r_[] = " << r_[0] << "," << r_[1] << std::endl;
             }
-        private:
-            mpqc::Array<double> data_;
-            size_t alpha_, beta_;
-            range r_;
         };
 
-        Vector vector(range r) {
-            return Vector(this->data_, alpha_, beta_, r);
+        Slice operator()(range r) {
+            return Slice(*this, r);
         }
 
-        mpqc::Array<double> array(range ri, range rj) {
-            //std::cout << "array " << ri << "," << rj << std::endl;
-            return data_(ri,rj);
-        }
-
-        mpqc::Array<double>& array() {
-            return data_;
+        operator Array&() {
+            return this->array_;
         }
 
         void sync() {
-            data_.sync();
+            array_.sync();
         }
 
     private:
+        Array array_;
         size_t alpha_, beta_;
-        mpqc::Array<double> data_;
-        static std::vector<size_t> extents(size_t m, size_t n) {
-            std::vector<size_t> v{m,n};
+        static std::vector<size_t> dims(const CI< Full<Index> > &ci) {
+            std::vector<size_t> v{ ci.alpha.size(), ci.beta.size() };
             //std::cout << "v=" << v.size() << std::endl;
             return v;
         }
@@ -103,4 +113,4 @@ namespace ci {
 }
 }
 
-#endif /* MPQC_CI_FULL_ARRAY_HPP */
+#endif /* MPQC_CI_FULL_VECTOR_HPP */

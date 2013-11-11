302be483f2fd75b9807bf7ea580c73bed44ccde9
Eduard Valeyev <eduard@valeyev.net>
2013-11-10 23:05:00 -0500

    Removed some uses of std::runtime_error in “new” mpqc

--- src/lib/mpqc/math/tensor/exception.hpp
+++ src/lib/mpqc/math/tensor/exception.hpp
@@ -1,25 +1,25 @@
 #ifndef MPQC_MATH_TENSOR_EXCEPTION_HPP
 #define MPQC_MATH_TENSOR_EXCEPTION_HPP
 
-#include <stdexcept>
 #include "mpqc/utility/string.hpp"
+#include <util/misc/scexception.h>
 
 namespace mpqc {
 
     /// @addtogroup MathTensor
     /// @{
 
-    struct TensorDimensionsException : std::runtime_error {
+    struct TensorDimensionsException : sc::SCException {
         template<typename Rank, typename Dim1, typename Dim2>
         TensorDimensionsException(Rank rank, Dim1 dim1, Dim2 dim2)
-            : std::runtime_error("rank<" + string_cast(rank) + "> " +
+            : sc::SCException("rank<" + string_cast(rank) + "> " +
                                  "dimensions mismatch (" +
                                  string_cast(dim1) + " != " + string_cast(dim2) +
                                  ")")
         {}
         template<typename Rank1, typename Rank2, typename Dim1, typename Dim2>
         TensorDimensionsException(Rank1 rank1, Rank2 rank2, Dim1 dim1, Dim2 dim2)
-            : std::runtime_error("rank<" + string_cast(rank1) + "> and" +
+            : sc::SCException("rank<" + string_cast(rank1) + "> and" +
                                  "rank<" + string_cast(rank2) + "> " +
                                  "dimensions mismatch (" +
                                  string_cast(dim1) + " != " + string_cast(dim2) +
@@ -27,10 +27,10 @@ namespace mpqc {
         {}
     };
 
-    struct TensorIndexException : std::runtime_error {
+    struct TensorIndexException : sc::SCException {
         template<typename Rank, typename Index, typename Begin, typename End>
         TensorIndexException(Rank rank, Index index, Begin begin, End end)
-            : std::runtime_error("rank<" + string_cast(rank) + "> " +
+            : sc::SCException("rank<" + string_cast(rank) + "> " +
                                  "index=" + string_cast(index) + " " +
                                  "outside the dimensions [" +
                                  string_cast(begin) + ":" + string_cast(end) + ")")
@@ -40,7 +40,7 @@ namespace mpqc {
     struct TensorRangeException : std::runtime_error {
         template<typename Rank, typename Range, typename Begin, typename End>
         TensorRangeException(Rank rank, Range range, Begin begin, End end)
-            : std::runtime_error("rank<" + string_cast(rank) + "> " +
+            : sc::SCException("rank<" + string_cast(rank) + "> " +
                                  "range=" + string_cast(range) + " " +
                                  "outside the dimensions [" +
                                  string_cast(begin) + ":" + string_cast(end) + ")")

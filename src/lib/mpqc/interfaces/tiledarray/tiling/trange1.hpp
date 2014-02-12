765e2c0bfa0d0e9010b91abdbe2e9addc067ab6e
Eduard Valeyev <eduard@valeyev.net>
2014-02-11 23:28:14 -0500

    Fixed code related to TiledBasisSet + filling TA::Array with integrals.

--- src/lib/mpqc/interfaces/tiledarray/tiling/trange1.hpp
+++ src/lib/mpqc/interfaces/tiledarray/tiling/trange1.hpp
@@ -33,74 +33,70 @@
 #include <vector>
 
 namespace mpqc {
-namespace tiling {
+  namespace tiling {
     /// @addtogroup ChemistryBasisIntegralTA
     /// @{
 
-  namespace TA = TiledArray;
+    namespace details {
 
-namespace details {
-
-    // Initialize the vector needed for TiledArray::TiledRange1 construction
-    std::vector<std::size_t> vec_init(std::size_t size_guess){
+      // Initialize the vector needed for TiledArray::TiledRange1 construction
+      std::vector<std::size_t> vec_init(std::size_t size_guess) {
         std::vector<std::size_t> vec_guess;
         vec_guess.reserve(size_guess); //  Reserve space
         vec_guess.push_back(0); // Tile 0 always starts at 0
         return vec_guess;
-    }
+      }
 
-    // Get the number of atoms in the system that are heavier than hydrogen.
-    std::size_t nheavy_atoms(const sc::Ref<sc::GaussianBasisSet> &basis){
+      // Get the number of atoms in the system that are heavier than hydrogen.
+      std::size_t nheavy_atoms(const sc::Ref<sc::GaussianBasisSet> &basis) {
         // copy Ref to work on it.
-      sc::Ref<sc::Molecule> mol = basis->molecule();
+        sc::Ref<sc::Molecule> mol = basis->molecule();
 
         // Return variable
         std::size_t nheavies = 0;
 
         // Loop over atoms and count how many are heavy.
-        for(auto i = 0; i < mol->natom(); ++i){
-            nheavies += (mol->Z(i) != 1) ? mol->Z(i) : 0;
+        for (auto i = 0; i < mol->natom(); ++i) {
+          nheavies += (mol->Z(i) != 1) ? mol->Z(i) : 0;
         }
         return nheavies;
-    }
-} // namespace details
+      }
+    } // namespace details
 
     /**
      * Returns TiledArray::TiledRange1 that corresponds to integral shells.
      * @param[in] basis Is a sc::GaussiangBasisSet
      */
-    TA::TiledRange1 tile_by_shell(const sc::Ref<sc::GaussianBasisSet> &basis){
+    ::TiledArray::TiledRange1 tile_by_shell(const sc::Ref<sc::GaussianBasisSet> &basis) {
 
-        // Shell and basis set information
-        std::size_t nshell = basis->nshell();
-        std::size_t nbasis = basis->nbasis();
+      // Shell and basis set information
+      std::size_t nshell = basis->nshell();
+      std::size_t nbasis = basis->nbasis();
 
-        std::vector<std::size_t> tilesizes = details::vec_init(nshell);
+      std::vector<std::size_t> tilesizes = details::vec_init(nshell);
 
+      // If we have some shells
+      if (nshell != 0) {
+        // Loop over shells
+        for (std::size_t i = 0; i < nshell; ++i) {
+          // Compute one past the last funciton in the shell.
+          // If i is the last shell then one past the last function
+          // on the shell is one past the last function in the basis.
+          std::size_t shell_end =
+              (i == nshell - 1) ? nbasis : basis->shell_to_function(i + 1);
 
-        // If we have some shells
-        if(nshell != 0){
-            // Loop over shells
-            for(std::size_t i = 0; i < nshell; ++i){
-                // Compute one past the last funciton in the shell.
-                // If i is the last shell then one past the last function
-                // on the shell is one past the last function in the basis.
-                std::size_t shell_end = (i == nshell - 1) ? nbasis :
-                                basis->shell_to_function(i + 1);
-
-                // Push back the last function on the shell.
-                // This way if shell one has 1 function and shell two has 3
-                // functions . The vector will look like vector[0] = |0,1) and
-                // vector[1] = |1,4)
-                tilesizes.push_back(shell_end);
-            }
+          // Push back the last function on the shell.
+          // This way if shell one has 1 function and shell two has 3
+          // functions . The vector will look like vector[0] = |0,1) and
+          // vector[1] = |1,4)
+          tilesizes.push_back(shell_end);
         }
-        else {
-            sc::ProgrammingError("The basis did not have any shells");
-        }
+      } else {
+        sc::ProgrammingError("The basis did not have any shells");
+      }
 
-        // construct TiledRange1
-        return TA::TiledRange1(tilesizes.begin(), tilesizes.end());
+      // construct TiledRange1
+      return ::TiledArray::TiledRange1(tilesizes.begin(), tilesizes.end());
     }
 
     /**
@@ -108,29 +104,26 @@ namespace details {
      * corresponds to all the shells on a single atom.
      * @param[in] basis Is a sc::GaussiangBasisSet
      */
-    TA::TiledRange1 tile_by_atom(const sc::Ref<sc::GaussianBasisSet> &basis){
+    ::TiledArray::TiledRange1 tile_by_atom(const sc::Ref<sc::GaussianBasisSet> &basis) {
 
-        // Basis set information
-        std::size_t ncenters = basis->ncenter();
-        std::vector<std::size_t> tilesizes = details::vec_init(ncenters);
+      // Basis set information
+      std::size_t ncenters = basis->ncenter();
+      std::vector<std::size_t> tilesizes = details::vec_init(ncenters);
 
-        // If we have atoms
-        if(ncenters != 0){
-            // Loop over atoms
-            for(std::size_t i = 0; i < ncenters; ++i){
-                // For each atom add the number of functions to the
-                // total number of functions so far.
-                tilesizes.push_back(tilesizes[i] +
-                                    basis->nbasis_on_center(i)
-                                    );
-            }
+      // If we have atoms
+      if (ncenters != 0) {
+        // Loop over atoms
+        for (std::size_t i = 0; i < ncenters; ++i) {
+          // For each atom add the number of functions to the
+          // total number of functions so far.
+          tilesizes.push_back(tilesizes[i] + basis->nbasis_on_center(i));
         }
-        else {
-            sc::ProgrammingError("The basis did not have any atoms");
-        }
+      } else {
+        sc::ProgrammingError("The basis did not have any atoms");
+      }
 
-        // construct TiledRange1
-        return TA::TiledRange1(tilesizes.begin(), tilesizes.end());
+      // construct TiledRange1
+      return ::TiledArray::TiledRange1(tilesizes.begin(), tilesizes.end());
     }
 
 #if 1
@@ -146,51 +139,46 @@ namespace details {
      * custum function or use mpqc::ShellOrdering and the TiledRange constructor
      * for integrals objects.
      */
-    TA::TiledRange1 by_grouped_hydrogens(const sc::Ref<sc::GaussianBasisSet> &basis){
+    ::TiledArray::TiledRange1 by_grouped_hydrogens(
+        const sc::Ref<sc::GaussianBasisSet> &basis) {
 
-        // Initialize the vector.
-        std::vector<std::size_t> tilesizes =
-                        details::vec_init(details::nheavy_atoms(basis));
+      // Initialize the vector.
+      std::vector<std::size_t> tilesizes = details::vec_init(
+          details::nheavy_atoms(basis));
 
-        std::size_t ncenters = basis->ncenter();
+      std::size_t ncenters = basis->ncenter();
 
-        // Check for centers
-        if(ncenters){
-            // Loop over centers
-            for(auto i = 0; i < ncenters; ++i){
-                // If atom is heavy add its basis functions as normal
-                if(basis->molecule()->Z(i) !=1){
-                    tilesizes.push_back(tilesizes[i] +
-                                        basis->nbasis_on_center(i)
-                                        );
-                }
-                // If atoms is H and not the last atom add its functions to
-                // atom i + 1
-                else if(i < (ncenters - 1)){
-                    tilesizes.push_back(tilesizes[i] +
-                                        basis->nbasis_on_center(i) +
-                                        basis->nbasis_on_center(++i)
-                                        );
-                }
-                // If atom is H and is the last atom it will be all by itself
-                else{
-                   tilesizes.push_back(tilesizes[i] +
-                                       basis->nbasis_on_center(i)
-                                      );
-                }
-            }
+      // Check for centers
+      if (ncenters) {
+        // Loop over centers
+        for (auto i = 0; i < ncenters; ++i) {
+          // If atom is heavy add its basis functions as normal
+          if (basis->molecule()->Z(i) != 1) {
+            tilesizes.push_back(tilesizes[i] + basis->nbasis_on_center(i));
+          }
+          // If atoms is H and not the last atom add its functions to
+          // atom i + 1
+          else if (i < (ncenters - 1)) {
+            tilesizes.push_back(
+                tilesizes[i] + basis->nbasis_on_center(i)
+                    + basis->nbasis_on_center(i+1));
+            ++i;
+          }
+          // If atom is H and is the last atom it will be all by itself
+          else {
+            tilesizes.push_back(tilesizes[i] + basis->nbasis_on_center(i));
+          }
         }
-        return TA::TiledRange1(tilesizes.begin(), tilesizes.end());
+      }
+      return ::TiledArray::TiledRange1(tilesizes.begin(), tilesizes.end());
     }
 #endif
 
-    /// @} // ChemistryBasisIntegralTA
-} // namespace tiling
+  /// @} // ChemistryBasisIntegralTA
+  }// namespace tiling
 
-    typedef TA::TiledRange1 (*TRange1Gen)(const sc::Ref<sc::GaussianBasisSet> &);
+  typedef ::TiledArray::TiledRange1 (*TRange1Gen)(const sc::Ref<sc::GaussianBasisSet> &);
 
 } // namespace mpqc
-
-
 
 #endif /* mpqc_interface_tiledarray_trange1_hpp */
--- src/lib/mpqc/math/tensor/exception.hpp
+++ src/lib/mpqc/math/tensor/exception.hpp
@@ -12,38 +12,38 @@ namespace mpqc {
     struct TensorDimensionsException : sc::SCException {
         template<typename Rank, typename Dim1, typename Dim2>
         TensorDimensionsException(Rank rank, Dim1 dim1, Dim2 dim2)
-            : sc::SCException("rank<" + string_cast(rank) + "> " +
+            : sc::SCException(("rank<" + string_cast(rank) + "> " +
                                  "dimensions mismatch (" +
                                  string_cast(dim1) + " != " + string_cast(dim2) +
-                                 ")")
+                                 ")").c_str())
         {}
         template<typename Rank1, typename Rank2, typename Dim1, typename Dim2>
         TensorDimensionsException(Rank1 rank1, Rank2 rank2, Dim1 dim1, Dim2 dim2)
-            : sc::SCException("rank<" + string_cast(rank1) + "> and" +
+            : sc::SCException(("rank<" + string_cast(rank1) + "> and" +
                                  "rank<" + string_cast(rank2) + "> " +
                                  "dimensions mismatch (" +
                                  string_cast(dim1) + " != " + string_cast(dim2) +
-                                 ")")
+                                 ")").c_str())
         {}
     };
 
     struct TensorIndexException : sc::SCException {
         template<typename Rank, typename Index, typename Begin, typename End>
         TensorIndexException(Rank rank, Index index, Begin begin, End end)
-            : sc::SCException("rank<" + string_cast(rank) + "> " +
+            : sc::SCException(("rank<" + string_cast(rank) + "> " +
                                  "index=" + string_cast(index) + " " +
                                  "outside the dimensions [" +
-                                 string_cast(begin) + ":" + string_cast(end) + ")")
+                                 string_cast(begin) + ":" + string_cast(end) + ")").c_str())
         {}
     };
 
-    struct TensorRangeException : std::runtime_error {
+    struct TensorRangeException : sc::SCException {
         template<typename Rank, typename Range, typename Begin, typename End>
         TensorRangeException(Rank rank, Range range, Begin begin, End end)
-            : sc::SCException("rank<" + string_cast(rank) + "> " +
+            : sc::SCException(("rank<" + string_cast(rank) + "> " +
                                  "range=" + string_cast(range) + " " +
                                  "outside the dimensions [" +
-                                 string_cast(begin) + ":" + string_cast(end) + ")")
+                                 string_cast(begin) + ":" + string_cast(end) + ")").c_str())
         {}
     };
 

ValeevGroup/mpqc

commit aaffc87a7d9be7627ebd516cb14e66e069b74080

Reworking the CMake macros used to build libraries to make them more flexible.

cmake/modules/AddLibrary.cmake
@@ -1,3 +1,17 @@
+# Macro for adding all subdirecties of a given folder.
+# result is a name you suply 
+# curdir is the current directory which often will be CMAKE_CURRENT_SOURCE_DIR
+MACRO(SUBDIRLIST result curdir)
+  FILE(GLOB children ${curdir} *)
+  SET(dirlist "")
+  FOREACH(child ${children})
+    IF(IS_DIRECTORY ${child})
+        SET(dirlist ${dirlist} ${child})
+    ENDIF()
+  ENDFOREACH()
+  SET(${result} ${dirlist})
+ENDMACRO()
+
 # Add MPQC library
 # add_mpqc_library(LIBRARY SUBDIRS)
 # LIBRARY - name of the library
@@ -9,7 +23,7 @@ macro(add_mpqc_library LIBRARY )
   #message("Library \"${LIBRARY}\": ${ARGN}")
   set(${LIBRARY}/OBJECTS)
   foreach(s ${ARGN})
-    add_subdirectory(${s})
+    #add_subdirectory(${s}) # THIS MAKES IT HARD TO ADD NEW FUNCTIONALITY TO OLD LIBS
     #message("${s}/OBJECTS: ${${s}/OBJECTS}")
     list(APPEND ${LIBRARY}/OBJECTS ${${s}/OBJECTS})
   endforeach()
@@ -38,7 +52,7 @@ macro(add_mpqc_objects_target TARGET )
   #message("Objects target \"${TARGET}\": ${ARGN}")
   add_custom_target(${TARGET})
   foreach(s ${ARGN})
-    add_subdirectory(${s})
+    #add_subdirectory(${s}) #THIS MAKES IT HARD TO USE WITH DISTANT OBJECT_DIRS
     add_dependencies(${TARGET} ${s})
     #message("${s}/OBJECTS: ${${s}/OBJECTS}")
     list(APPEND ${TARGET}/OBJECTS ${${s}/OBJECTS})

src/lib/CMakeLists.txt
@@ -1,7 +1,7 @@
+add_subdirectory(mpqc)
 add_subdirectory(chemistry)
 add_subdirectory(math)
 add_subdirectory(util)
-add_subdirectory(mpqc)
 add_subdirectory(extern)
 
 # library headers

src/lib/chemistry/CMakeLists.txt
@@ -1,15 +1,21 @@
 include(AddLibrary)
 
-set(subdirectories
+SUBDIRLIST(Subdirs CMAKE_CURRENT_SOURCE_DIR)
+
+FOREACH(subdir ${Subdirs})
+    ADD_SUBDIRECTORY(${subdir})
+ENDFOREACH()
+
+set(chemobjects
   molecule
   qc
   solvent
 )
 
-add_mpqc_library(chemistry ${subdirectories})
-install(TARGETS chemistry DESTINATION lib)
+add_mpqc_library(chemistry ${chemobjects})
 
 target_link_libraries(chemistry math util)
+
 
 if (HAVE_LIBINT2)
   target_link_libraries(chemistry ${LIBINT2_LIBRARY})

src/lib/chemistry/qc/CMakeLists.txt
@@ -1,6 +1,10 @@
 include(AddLibrary)
+SUBDIRLIST(Subdirs CMAKE_CURRENT_LIST_DIR)
+FOREACH(subdir ${Subdirs})
+    ADD_SUBDIRECTORY(${subdir})
+ENDFOREACH()
 
-set(subdirectories
+set(subobjects
   basis
   dft
   etrain
@@ -15,19 +19,19 @@ set(subdirectories
   )
 
 if (MPQC_CI)
-  list(APPEND subdirectories ci)
+  list(APPEND subobjects ci)
 endif()
 
 if (HAVE_LIBINT2)
-  list(APPEND subdirectories libint2)
+  list(APPEND subobjects libint2)
 endif()
 
 if (MPQC_R12)
-  list(APPEND subdirectories mbptr12 ccr12)
+  list(APPEND subobjects mbptr12 ccr12)
 endif()
 
 if (HAVE_PSI3)
-  list(APPEND subdirectories psi)
+  list(APPEND subobjects psi)
 endif()
 
-add_mpqc_objects_target(qc ${subdirectories})
+add_mpqc_objects_target(qc ${subobjects})

src/lib/chemistry/qc/basis/CMakeLists.txt
@@ -43,7 +43,7 @@ if (MPQC_UNITTEST)
     SRCDIR="${CMAKE_CURRENT_SOURCE_DIR}")
   add_executable(btest btest.cc $<TARGET_OBJECTS:basis>)
   add_test(btest btest)
-
+  
   target_link_libraries( btest
     chemistry
     ${LAPACK_LIBRARIES}

src/lib/chemistry/qc/ci/CMakeLists.txt
@@ -2,10 +2,12 @@ include(AddLibrary)
 
 # ci library
 
-add_mpqc_object_library(ci
-  ci.cc
-  compute.cc
-)
+IF(MPQC_CI)
+    add_mpqc_object_library(ci
+      ci.cc
+      compute.cc
+    )
+ENDIF()
 
 # tests
 if (MPQC_UNITTEST)

src/lib/chemistry/qc/libint2/CMakeLists.txt
@@ -2,6 +2,7 @@ include(AddLibrary)
 
 # libint2 library
 
+IF(HAVE_LIBINT2)
 add_mpqc_object_library(libint2
   bounds.cc
   #comp_eri.cc
@@ -36,6 +37,7 @@ add_mpqc_object_library(libint2
   tbintlibint2.cc
   tform.cc
 )
+ENDIF(HAVE_LIBINT2)
 
 # tests
 if (MPQC_UNITTEST)

src/lib/chemistry/qc/mbptr12/CMakeLists.txt
@@ -1,52 +1,52 @@
 include(AddLibrary)
 
 # mbptr12 library
-
-add_mpqc_object_library(mbptr12
-FxFgen.cc 
-Xgen.cc 
-approxApp.cc 
-approxB.cc 
-approxC.cc 
-approxC_GenRefansatz2.cc 
-approxC_ansatz1.cc 
-approxCp.cc 
-bc_contrib.cc 
-compute_1rdm_X_F12.cc 
-compute_VXB_a.cc 
-compute_VXB_a_vbsneqobs.cc 
-compute_VXB_c_ansatz1.cc 
-compute_VX_GenRefansatz2.cc 
-compute_amps.cc 
-compute_energy_a.cc 
-coulomb.cc 
-creator.cc 
-dkh.cc 
-ebc_contribs.cc 
-exchange.cc 
-fixedcoefficient.cc 
-fock.cc 
-gbc_contribs.cc 
-intermeds_g12.cc 
-mbptr12.cc 
-mp2_pair_energies.cc 
-mp2r12_energy.cc 
-mp2r12_energy_util.cc 
-pt2r12.cc 
-pt2r12_spinorbital.cc 
-r12_amps.cc 
-r12int_eval.cc 
-r12technology.cc 
-r12wfnworld.cc 
-ri_basis.cc 
-singles_emp2.cc 
-sr_r12intermediates_util.cc
-twobody_intermediates_ta.cc 
-twobody_intermeds.cc 
-twobodygrid.cc 
-twoparticlecontraction.cc
-)
-
+IF(MPQC_R12)
+    add_mpqc_object_library(mbptr12
+        FxFgen.cc 
+        Xgen.cc 
+        approxApp.cc 
+        approxB.cc 
+        approxC.cc 
+        approxC_GenRefansatz2.cc 
+        approxC_ansatz1.cc 
+        approxCp.cc 
+        bc_contrib.cc 
+        compute_1rdm_X_F12.cc 
+        compute_VXB_a.cc 
+        compute_VXB_a_vbsneqobs.cc 
+        compute_VXB_c_ansatz1.cc 
+        compute_VX_GenRefansatz2.cc 
+        compute_amps.cc 
+        compute_energy_a.cc 
+        coulomb.cc 
+        creator.cc 
+        dkh.cc 
+        ebc_contribs.cc 
+        exchange.cc 
+        fixedcoefficient.cc 
+        fock.cc 
+        gbc_contribs.cc 
+        intermeds_g12.cc 
+        mbptr12.cc 
+        mp2_pair_energies.cc 
+        mp2r12_energy.cc 
+        mp2r12_energy_util.cc 
+        pt2r12.cc 
+        pt2r12_spinorbital.cc 
+        r12_amps.cc 
+        r12int_eval.cc 
+        r12technology.cc 
+        r12wfnworld.cc 
+        ri_basis.cc 
+        singles_emp2.cc 
+        sr_r12intermediates_util.cc
+        twobody_intermediates_ta.cc 
+        twobody_intermeds.cc 
+        twobodygrid.cc 
+        twoparticlecontraction.cc
+    )
+ENDIF(MPQC_R12)
 # tests
 
 #set_property(SOURCE mbptr12test.cc PROPERTY COMPILE_DEFINITIONS

commit 20b5181fa57ad42b74f4276afef14001178e803b

Fixed bug introduced in aaffc87a7d9be7627ebd516cb14e66e069b74080 : in-source-tree compilation failed due to presence of cmake-produced directories

cmake/modules/AddLibrary.cmake
@@ -1,11 +1,11 @@
-# Macro for adding all subdirecties of a given folder.
+# Macro for adding all subdirectories of a given folder.
 # result is a name you suply 
 # curdir is the current directory which often will be CMAKE_CURRENT_SOURCE_DIR
 MACRO(SUBDIRLIST result curdir)
   FILE(GLOB children ${curdir} *)
   SET(dirlist "")
   FOREACH(child ${children})
-    IF(IS_DIRECTORY ${child})
+    IF(IS_DIRECTORY ${child} AND EXISTS ${child}/CMakeLists.txt)
         SET(dirlist ${dirlist} ${child})
     ENDIF()
   ENDFOREACH()
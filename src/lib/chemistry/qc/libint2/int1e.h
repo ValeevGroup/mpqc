ValeevGroup/mpqc

commit cb346a5e92cc90c112db34eac31f836ab9414b1f

Implemented IntegralLibint2::efield and efield_gradient

src/lib/chemistry/qc/libint2/int1e.h
@@ -87,10 +87,22 @@ class Int1eLibint2: public RefCount {
     // Such buffer contains all integrals including intermediates
     // These integrals are nonseparable, hence the first and second indices are composite
     double ***AI0_;
+    double ***AIX_;
+    double ***AIY_;
+    double ***AIZ_;
+    double ***AIXX_;
+    double ***AIXY_;
+    double ***AIXZ_;
+    double ***AIYY_;
+    double ***AIYZ_;
+    double ***AIZZ_;
     int indmax_;         // that's the range of the first 2 indices of AI0
     // Compute engines
-    void AI_OSrecurs_(double ***AI0, double PA[3], double PB[3],
-		      double PC[3], double gamma, int iang, int jang);
+    // Order = 0 => potential
+    // Order = 1 => efield
+    // Order = 2 => efield gradient
+    template <int Order = 0> void AI_OSrecurs_(double PA[3], double PB[3],
+                                               double PC[3], double gamma, int iang, int jang);
     void OI_OSrecurs_(double **OIX, double **OIY, double **OIZ, double PA[3], double PB[3],
 		      double gamma, int lmaxi, int lmaxj);
     ::libint2::FmEval_Chebyshev3* Fm_Eval_;
@@ -118,6 +130,8 @@ class Int1eLibint2: public RefCount {
     void hcore_sameam_general_();
     void edipole_full_general_();
     void equadrupole_full_general_();
+    void efield_full_general_();
+    void efield_grad_full_general_();
     void p4_full_general_();
 
     // Utility functions
@@ -137,7 +151,7 @@ class Int1eLibint2: public RefCount {
     void set_EdotV_origin(const Ref<EfieldDotVectorData>&);
     void set_Q_origin(const Ref<PointChargeData>&);
     Ref<IntParams> params();
-    Ref<IntParamsOrigin> multipole_origin();
+    Ref<IntParamsOrigin> origin();
     Ref<EfieldDotVectorData> EdotV_origin();
     Ref<PointChargeData> Q_origin();
 
@@ -152,8 +166,12 @@ class Int1eLibint2: public RefCount {
     void hcore(int ish, int jsh);
     void edipole(int ish, int jsh);
     void equadrupole(int ish, int jsh);
+    void efield(int ish, int jsh);
+    void efield_grad(int ish, int jsh);
     void p4(int ish, int jsh);
 };
+
+#include <chemistry/qc/libint2/obosrr.timpl.h>
 
 }
 

src/lib/chemistry/qc/libint2/CMakeLists.txt
@@ -10,6 +10,8 @@ add_mpqc_object_library(libint2
   comp_g12nc.cc
   #comp_grt.cc
   edipole.cc
+  efield.cc
+  efield_grad.cc
   equadrupole.cc
   eri.cc
   g12.cc

src/lib/chemistry/qc/libint2/edipole.cc
@@ -54,7 +54,7 @@ void Int1eLibint2::edipole(int sh1, int sh2)
 
 void Int1eLibint2::edipole_full_general_()
 {
-  Ref<IntParamsOrigin> origin = multipole_origin();
+  Ref<IntParamsOrigin> O = origin();
 
   int maxam1 = int_shell1_->max_am();
   int maxam2 = int_shell2_->max_am();
@@ -112,7 +112,7 @@ void Int1eLibint2::edipole_full_general_()
 	P[xyz] = (a1*doublet_info_.A[xyz] + a2*doublet_info_.B[xyz])*oog;
 	PA[xyz] = P[xyz] - doublet_info_.A[xyz];
 	PB[xyz] = P[xyz] - doublet_info_.B[xyz];
-	BO[xyz] = doublet_info_.B[xyz] - origin->r(xyz);
+	BO[xyz] = doublet_info_.B[xyz] - O->r(xyz);
       }
 
       OI_OSrecurs_(OIX_,OIY_,OIZ_,PA,PB,gamma,maxam1,maxam2+1);

src/lib/chemistry/qc/libint2/efield.cc
@@ -1,0 +1,170 @@
+//
+// efield.cc
+//
+// Copyright (C) 2001 Edward Valeev
+//
+// Author: Edward Valeev <evaleev@vt.edu>
+// Maintainer: EV
+//
+// This file is part of the SC Toolkit.
+//
+// The SC Toolkit is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// The SC Toolkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the SC Toolkit; see the file COPYING.LIB.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// The U.S. Government is granted a limited license as per AL 91-7.
+//
+
+#include <util/misc/math.h>
+
+#include <chemistry/qc/libint2/int1e.h>
+#include <chemistry/qc/libint2/macros.h>
+#include <chemistry/qc/libint2/tform.timpl.h>
+
+using namespace sc;
+
+void Int1eLibint2::efield(int sh1, int sh2) {
+  const int ntypes = 3;    // x, y, z
+  zero_buffers_vec_(ntypes);
+  compute_doublet_info_(sh1, sh2);
+
+  int maxam1 = int_shell1_->max_am();
+  int minam1 = int_shell1_->min_am();
+  int maxam2 = int_shell2_->max_am();
+  int minam2 = int_shell2_->min_am();
+
+  if (operset_params_.null()) {
+    operset_params_ = new IntParamsOrigin;
+  }
+
+  efield_full_general_();
+}
+
+void Int1eLibint2::efield_full_general_() {
+  Ref<IntParamsOrigin> o = origin();
+  const double* C = o->r();
+  const int ntypes = 3;      // x, y, z
+
+  const int maxam1 = int_shell1_->max_am();
+  const int maxam2 = int_shell2_->max_am();
+  const int z1weight = 1;
+  const int y1weight = maxam1 + 1;
+  const int x1weight = y1weight * y1weight;
+  const int z2weight = 1;
+  const int y2weight = maxam2 + 1;
+  const int x2weight = y2weight * y2weight;
+
+  /* See if need to transform to spherical harmonics */
+  bool need_cart2sph_transform = false;
+  if (int_shell1_->has_pure() || int_shell2_->has_pure())
+    need_cart2sph_transform = true;
+
+  /* See if contraction quartets need to be resorted into a shell quartet */
+  bool need_sort_to_shell_doublet = false;
+  int num_gen_shells = 0;
+  if (int_shell1_->ncontraction() > 1)
+    num_gen_shells++;
+  if (int_shell2_->ncontraction() > 1)
+    num_gen_shells++;
+  if (maxam1 + maxam2 && num_gen_shells >= 1)
+    need_sort_to_shell_doublet = true;
+
+  /* Determine where integrals need to go at each stage */
+  if (need_sort_to_shell_doublet) {
+    prim_ints_ = cart_ints_;
+    if (need_cart2sph_transform)
+      contr_doublets_ = sphharm_ints_;
+    else
+      contr_doublets_ = cart_ints_;
+    shell_doublet_ = target_ints_buffer_;
+  } else {
+    if (need_cart2sph_transform) {
+      prim_ints_ = cart_ints_;
+      contr_doublets_ = target_ints_buffer_;
+      shell_doublet_ = target_ints_buffer_;
+    } else {
+      prim_ints_ = target_ints_buffer_;
+      shell_doublet_ = target_ints_buffer_;
+    }
+  }
+
+  /* Begin loops over primitives. */
+  for (int p1 = 0; p1 < int_shell1_->nprimitive(); p1++) {
+    double a1 = int_shell1_->exponent(p1);
+    for (int p2 = 0; p2 < int_shell2_->nprimitive(); p2++) {
+      double a2 = int_shell2_->exponent(p2);
+
+      double gamma = a1 + a2;
+      double oog = 1.0 / gamma;
+      double over_pf = exp(-a1 * a2 * doublet_info_.AB2 * oog)
+          * sqrt(M_PI * oog) * M_PI * oog;
+
+      double P[3], PA[3], PB[3], PC[3];
+      for (int xyz = 0; xyz < 3; xyz++) {
+        P[xyz] = (a1 * doublet_info_.A[xyz] + a2 * doublet_info_.B[xyz]) * oog;
+        PA[xyz] = P[xyz] - doublet_info_.A[xyz];
+        PB[xyz] = P[xyz] - doublet_info_.B[xyz];
+      }
+
+      {
+        PC[0] = P[0] - C[0];
+        PC[1] = P[1] - C[1];
+        PC[2] = P[2] - C[2];
+        AI_OSrecurs_<1>(PA, PB, PC, gamma, maxam1, maxam2);
+
+        /*--- contract each buffer into appropriate location ---*/
+        double *ints_buf = prim_ints_;
+        for (int gc1 = 0; gc1 < int_shell1_->ncontraction(); gc1++) {
+          double norm1 = int_shell1_->coefficient_unnorm(gc1, p1);
+          int am1 = int_shell1_->am(gc1);
+          for (int gc2 = 0; gc2 < int_shell2_->ncontraction(); gc2++) {
+            double norm2 = int_shell2_->coefficient_unnorm(gc2, p2);
+            int am2 = int_shell2_->am(gc2);
+            double total_pf = over_pf * norm1 * norm2;
+
+            int k1, l1, m1, k2, l2, m2;
+            FOR_CART(k1,l1,m1,am1)
+                int ind1 = k1 * x1weight + l1 * y1weight + m1 * z1weight;
+                FOR_CART(k2,l2,m2,am2)
+                    int ind2 = k2 * x2weight + l2 * y2weight + m2 * z2weight;
+                    *(ints_buf++) += AIX_[ind1][ind2][0] * total_pf;
+                    *(ints_buf++) += AIY_[ind1][ind2][0] * total_pf;
+                    *(ints_buf++) += AIZ_[ind1][ind2][0] * total_pf;
+                END_FOR_CART
+            END_FOR_CART
+
+          }
+        }
+      }
+    }
+  }
+
+  if (need_cart2sph_transform)
+    transform_contrquartets_vec_(ntypes, prim_ints_, contr_doublets_);
+
+  // If not CCA-compliant normalization -- re-normalize all integrals
+#if INTEGRALLIBINT2_NORMCONV != INTEGRALLIBINT2_NORMCONV_CCA
+  norm_contrcart_<6u>(need_cart2sph_transform ? contr_doublets_ : prim_ints_);
+#endif
+
+  if (need_sort_to_shell_doublet)
+    sort_contrdoublets_to_shelldoublet_vec_(ntypes, contr_doublets_,
+                                            shell_doublet_);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+// Local Variables:
+// mode: c++
+// c-file-style: "CLJ"
+// End:

src/lib/chemistry/qc/libint2/efield_grad.cc
@@ -1,0 +1,174 @@
+//
+// efield.cc
+//
+// Copyright (C) 2001 Edward Valeev
+//
+// Author: Edward Valeev <evaleev@vt.edu>
+// Maintainer: EV
+//
+// This file is part of the SC Toolkit.
+//
+// The SC Toolkit is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// The SC Toolkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the SC Toolkit; see the file COPYING.LIB.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// The U.S. Government is granted a limited license as per AL 91-7.
+//
+
+#include <util/misc/math.h>
+
+#include <chemistry/qc/libint2/int1e.h>
+#include <chemistry/qc/libint2/macros.h>
+#include <chemistry/qc/libint2/tform.timpl.h>
+
+using namespace sc;
+
+void Int1eLibint2::efield_grad(int sh1, int sh2) {
+  const int ntypes = 6;    // xx, xy, xz, yy, yz, zz
+  zero_buffers_vec_(ntypes);
+  compute_doublet_info_(sh1, sh2);
+
+  int maxam1 = int_shell1_->max_am();
+  int minam1 = int_shell1_->min_am();
+  int maxam2 = int_shell2_->max_am();
+  int minam2 = int_shell2_->min_am();
+
+  if (operset_params_.null()) {
+    operset_params_ = new IntParamsOrigin;
+  }
+
+  efield_grad_full_general_();
+}
+
+void Int1eLibint2::efield_grad_full_general_() {
+  Ref<IntParamsOrigin> o = origin();
+  const double* C = o->r();
+  const int ntypes = 6;      // xx, xy, xz, yy, yz, zz
+
+  const int maxam1 = int_shell1_->max_am();
+  const int maxam2 = int_shell2_->max_am();
+  const int z1weight = 1;
+  const int y1weight = maxam1 + 1;
+  const int x1weight = y1weight * y1weight;
+  const int z2weight = 1;
+  const int y2weight = maxam2 + 1;
+  const int x2weight = y2weight * y2weight;
+
+
+  /* See if need to transform to spherical harmonics */
+  bool need_cart2sph_transform = false;
+  if (int_shell1_->has_pure() || int_shell2_->has_pure())
+    need_cart2sph_transform = true;
+
+  /* See if contraction quartets need to be resorted into a shell quartet */
+  bool need_sort_to_shell_doublet = false;
+  int num_gen_shells = 0;
+  if (int_shell1_->ncontraction() > 1)
+    num_gen_shells++;
+  if (int_shell2_->ncontraction() > 1)
+    num_gen_shells++;
+  if (maxam1 + maxam2 && num_gen_shells >= 1)
+    need_sort_to_shell_doublet = true;
+
+  /* Determine where integrals need to go at each stage */
+  if (need_sort_to_shell_doublet) {
+    prim_ints_ = cart_ints_;
+    if (need_cart2sph_transform)
+      contr_doublets_ = sphharm_ints_;
+    else
+      contr_doublets_ = cart_ints_;
+    shell_doublet_ = target_ints_buffer_;
+  } else {
+    if (need_cart2sph_transform) {
+      prim_ints_ = cart_ints_;
+      contr_doublets_ = target_ints_buffer_;
+      shell_doublet_ = target_ints_buffer_;
+    } else {
+      prim_ints_ = target_ints_buffer_;
+      shell_doublet_ = target_ints_buffer_;
+    }
+  }
+
+  /* Begin loops over primitives. */
+  for (int p1 = 0; p1 < int_shell1_->nprimitive(); p1++) {
+    double a1 = int_shell1_->exponent(p1);
+    for (int p2 = 0; p2 < int_shell2_->nprimitive(); p2++) {
+      double a2 = int_shell2_->exponent(p2);
+
+      double gamma = a1 + a2;
+      double oog = 1.0 / gamma;
+      double over_pf = exp(-a1 * a2 * doublet_info_.AB2 * oog)
+          * sqrt(M_PI * oog) * M_PI * oog;
+
+      double P[3], PA[3], PB[3], PC[3];
+      for (int xyz = 0; xyz < 3; xyz++) {
+        P[xyz] = (a1 * doublet_info_.A[xyz] + a2 * doublet_info_.B[xyz]) * oog;
+        PA[xyz] = P[xyz] - doublet_info_.A[xyz];
+        PB[xyz] = P[xyz] - doublet_info_.B[xyz];
+      }
+
+      {
+        PC[0] = P[0] - C[0];
+        PC[1] = P[1] - C[1];
+        PC[2] = P[2] - C[2];
+        AI_OSrecurs_<2>(PA, PB, PC, gamma, maxam1, maxam2);
+
+        /*--- contract each buffer into appropriate location ---*/
+        double *ints_buf = prim_ints_;
+        for (int gc1 = 0; gc1 < int_shell1_->ncontraction(); gc1++) {
+          double norm1 = int_shell1_->coefficient_unnorm(gc1, p1);
+          int am1 = int_shell1_->am(gc1);
+          for (int gc2 = 0; gc2 < int_shell2_->ncontraction(); gc2++) {
+            double norm2 = int_shell2_->coefficient_unnorm(gc2, p2);
+            int am2 = int_shell2_->am(gc2);
+            double total_pf = over_pf * norm1 * norm2;
+
+            int k1, l1, m1, k2, l2, m2;
+            FOR_CART(k1,l1,m1,am1)
+                int ind1 = k1 * x1weight + l1 * y1weight + m1 * z1weight;
+                FOR_CART(k2,l2,m2,am2)
+                    int ind2 = k2 * x2weight + l2 * y2weight + m2 * z2weight;
+                    *(ints_buf++) += AIXX_[ind1][ind2][0] * total_pf;
+                    *(ints_buf++) += AIXY_[ind1][ind2][0] * total_pf;
+                    *(ints_buf++) += AIXZ_[ind1][ind2][0] * total_pf;
+                    *(ints_buf++) += AIYY_[ind1][ind2][0] * total_pf;
+                    *(ints_buf++) += AIYZ_[ind1][ind2][0] * total_pf;
+                    *(ints_buf++) += AIZZ_[ind1][ind2][0] * total_pf;
+                END_FOR_CART
+            END_FOR_CART
+
+          }
+        }
+      }
+    }
+  }
+
+  if (need_cart2sph_transform)
+    transform_contrquartets_vec_(ntypes, prim_ints_, contr_doublets_);
+
+  // If not CCA-compliant normalization -- re-normalize all integrals
+#if INTEGRALLIBINT2_NORMCONV != INTEGRALLIBINT2_NORMCONV_CCA
+  norm_contrcart_<6u>(need_cart2sph_transform ? contr_doublets_ : prim_ints_);
+#endif
+
+  if (need_sort_to_shell_doublet)
+    sort_contrdoublets_to_shelldoublet_vec_(ntypes, contr_doublets_,
+                                            shell_doublet_);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+// Local Variables:
+// mode: c++
+// c-file-style: "CLJ"
+// End:

src/lib/chemistry/qc/libint2/equadrupole.cc
@@ -54,7 +54,7 @@ void Int1eLibint2::equadrupole(int sh1, int sh2)
 
 void Int1eLibint2::equadrupole_full_general_()
 {
-  Ref<IntParamsOrigin> origin = multipole_origin();
+  Ref<IntParamsOrigin> O = origin();
   int maxam1 = int_shell1_->max_am();
   int maxam2 = int_shell2_->max_am();
   const int ntypes = 6;      // integrals for xx, xy, xz, yy, yz, zz
@@ -111,7 +111,7 @@ void Int1eLibint2::equadrupole_full_general_()
 	P[xyz] = (a1*doublet_info_.A[xyz] + a2*doublet_info_.B[xyz])*oog;
 	PA[xyz] = P[xyz] - doublet_info_.A[xyz];
 	PB[xyz] = P[xyz] - doublet_info_.B[xyz];
-	BO[xyz] = doublet_info_.B[xyz] - origin->r(xyz);
+	BO[xyz] = doublet_info_.B[xyz] - O->r(xyz);
       }
 
       OI_OSrecurs_(OIX_,OIY_,OIZ_,PA,PB,gamma,maxam1,maxam2+2);

src/lib/chemistry/qc/libint2/hcore.cc
@@ -164,7 +164,7 @@ void Int1eLibint2::hcore_full_general_()
 	PC[0] = P[0] - bs1_->r(atom,0);
 	PC[1] = P[1] - bs1_->r(atom,1);
 	PC[2] = P[2] - bs1_->r(atom,2);
-	AI_OSrecurs_(AI0_,PA,PB,PC,gamma,maxam1,maxam2);
+	AI_OSrecurs_<0>(PA,PB,PC,gamma,maxam1,maxam2);
 
 	/*--- contract each buffer into appropriate location ---*/
 	double *ints_buf = prim_ints_;
@@ -279,7 +279,7 @@ void Int1eLibint2::hcore_sameam_general_()
 	PC[0] = P[0] - bs1_->r(atom,0);
 	PC[1] = P[1] - bs1_->r(atom,1);
 	PC[2] = P[2] - bs1_->r(atom,2);
-	AI_OSrecurs_(AI0_,PA,PB,PC,gamma,tam1,tam2);
+	AI_OSrecurs_<0>(PA,PB,PC,gamma,tam1,tam2);
 
 	/*--- contract each buffer into appropriate location ---*/
 	double *ints_buf = cart_ints_;

src/lib/chemistry/qc/libint2/int1e.cc
@@ -93,12 +93,28 @@ Int1eLibint2::Int1eLibint2(Integral *integral,
   }
 
   if (need_coulomb_) {
-    const int mmax = bs1_->max_angular_momentum() + bs2_->max_angular_momentum() + order;
+    int efield_order = 0;
+    if (ntypes_ > 1) efield_order = 1;
+    if (ntypes_ > 3) efield_order = 2;
+    const int mmax = bs1_->max_angular_momentum() + bs2_->max_angular_momentum() + order + efield_order;
     Fm_Eval_ = new ::libint2::FmEval_Chebyshev3(mmax);
     Fm_table_ = new double[mmax+1];
     indmax_ = (max_am+order)*(max_am+1+order)*(max_am+1+order)+1;
     // Allocate AI0
-    AI0_ = init_box_(indmax_,indmax_,2*(max_am+order)+1);
+    AI0_ = init_box_(indmax_,indmax_,2*(max_am+order)+3);
+    if (efield_order) {
+      AIX_ = init_box_(indmax_,indmax_,2*(max_am+order)+2);
+      AIY_ = init_box_(indmax_,indmax_,2*(max_am+order)+2);
+      AIZ_ = init_box_(indmax_,indmax_,2*(max_am+order)+2);
+    }
+    if (efield_order) {
+      AIXX_ = init_box_(indmax_,indmax_,2*(max_am+order)+1);
+      AIXY_ = init_box_(indmax_,indmax_,2*(max_am+order)+1);
+      AIXZ_ = init_box_(indmax_,indmax_,2*(max_am+order)+1);
+      AIYY_ = init_box_(indmax_,indmax_,2*(max_am+order)+1);
+      AIYZ_ = init_box_(indmax_,indmax_,2*(max_am+order)+1);
+      AIZZ_ = init_box_(indmax_,indmax_,2*(max_am+order)+1);
+    }
   }
 }
 
@@ -115,6 +131,19 @@ Int1eLibint2::~Int1eLibint2()
   }
   if (need_coulomb_) {
     free_box_(AI0_);
+    if (ntypes_ > 1) {
+      free_box_(AIX_);
+      free_box_(AIY_);
+      free_box_(AIZ_);
+    }
+    if (ntypes_ > 3) {
+      free_box_(AIXX_);
+      free_box_(AIXY_);
+      free_box_(AIXZ_);
+      free_box_(AIYY_);
+      free_box_(AIYZ_);
+      free_box_(AIZZ_);
+    }
     delete Fm_Eval_;
     delete[] Fm_table_;
   }
@@ -148,7 +177,7 @@ Int1eLibint2::params()
 }
 
 Ref<IntParamsOrigin>
-Int1eLibint2::multipole_origin()
+Int1eLibint2::origin()
 {
   return require_dynamic_cast<IntParamsOrigin*>(operset_params_.pointer(), "need multipole origin, but not in multipole evaluator");
 }

commit d702674b2ee302db969d08ac983deeb3bdeb0600

Fixed bug introduced in cb346a5e92cc90c112db34eac31f836ab9414b1f (did not compile without C++11).

src/lib/chemistry/qc/libint2/int1e.h
@@ -101,8 +101,8 @@ class Int1eLibint2: public RefCount {
     // Order = 0 => potential
     // Order = 1 => efield
     // Order = 2 => efield gradient
-    template <int Order = 0> void AI_OSrecurs_(double PA[3], double PB[3],
-                                               double PC[3], double gamma, int iang, int jang);
+    template <int Order> void AI_OSrecurs_(double PA[3], double PB[3],
+                                           double PC[3], double gamma, int iang, int jang);
     void OI_OSrecurs_(double **OIX, double **OIY, double **OIZ, double PA[3], double PB[3],
 		      double gamma, int lmaxi, int lmaxj);
     ::libint2::FmEval_Chebyshev3* Fm_Eval_;
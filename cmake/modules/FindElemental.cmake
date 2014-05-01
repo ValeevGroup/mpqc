ec940375ba708a9d92c946fa0ed9552a388efb29
calewis <drew90@vt.edu>
2014-05-01 09:48:01 -0400

    Added FindXXX.cmake files to play nicely with tools that recognize Cmake

---
+++ cmake/modules/FindElemental.cmake
@@ -0,0 +1,21 @@
+# - Try to find Elemental
+#
+# Writen by Drew Lewis
+# 
+
+if (ELEMENTAL_INCLUDE_DIRS)
+    set(ELEMENTAL_FOUND TRUE)
+
+else()
+    find_path(ELEMENTAL_INCLUDE_DIR NAMES elemental.hpp
+        PATHS ${ELEMENTAL_DIR}/include)
+    
+    mark_as_advanced(ELEMENTAL_INCLUDE_DIR)
+    set(ELEMENTAL_INCLUDE_DIRS ${ELEMENTAL_INCLUDE_DIR}
+        CACHE PATH "The TiledArray include path.")
+    
+    if(ELEMENTAL_INCLUDE_DIRS)
+        set(ELEMENTAL_FOUND TRUE)
+    endif()
+  
+endif(ELEMENTAL_INCLUDE_DIRS)

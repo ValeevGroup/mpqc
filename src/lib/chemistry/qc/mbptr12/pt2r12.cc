603650b89df10e46ca7985d0d09447a9d10ad76c
Chong Peng <pchong90@vt.edu>
2014-05-20 18:44:53 -0400

    change cabs_single to TA expression, give correct results

--- src/lib/chemistry/qc/mbptr12/pt2r12.cc
+++ src/lib/chemistry/qc/mbptr12/pt2r12.cc
@@ -1692,7 +1692,7 @@ template<typename T>
          * @param[out] BC
          */
         void operator()(const Array2& C, Array2& BC) {
-            BC("m1,B'") = Bmatrix("B',A',n1,m1") * C("n1,A'");
+            BC("x,B'") = Bmatrix("x,B',y,A'") * C("y,A'");
         }
     };
 }
@@ -1736,40 +1736,32 @@ double PT2R12::cabs_singles_Dyall()
 
     bootup_mpqc3();
 
-    // make all the matrices
-    tim.enter("compute integrals");
     // density matrices
     TArray2 gamma1; gamma1("m,n") = _2("<m|gamma|n>"); // rdm1 occ
     TArray4 gamma2; gamma2("m,n,m1,n1") = _4("<m n|gamma|m1 n1>"); //rdm2 occ
 
-    // g
-    TArray4 g_ijkl; g_ijkl("n1,m1,m,n") = _4("<n1 m1|g|m n>"); // occ
-    TArray4 g_iajb; g_iajb("m,f,n,e") = _4("<m f|g|n e>"); // occ vir occ vir
-    TArray4 g_iabj; g_iabj("m,e,f,n") = _4("<m e|g|f n>"); // occ vir vir occ
+    TArray2 I_aA; I_aA("e,B'") = _2("<e|I|B'>"); // allvir vir
 
     // h_core
     TArray2 h_ij; h_ij("m,n") = _2("<m|h|n>");  //occ occ
-    TArray2 h_ab; h_ab("e,f") = _2("<e|h|f>");   //vir vir
 
     // fock
-    TArray2 F_ij; F_ij("m,n") = _2("<m|F|n>");  //occ occ
-    TArray2 F_ci; F_ci("c',n") = _2("<c'|F|n>");  //cabs occ
     TArray2 F_AB; F_AB("A',B'") = _2("<A'|F|B'>"); //allvir allvir
-    TArray2 F_ab; F_ab("e,f") = _2("<e|F|f>");  //vir vir
-    TArray2 F_iA; F_iA("m,A'") = _2("<m|F|A'>");  // occ allvir
-
-    // delta
-    TArray2 I_AB; I_AB("B',A'") = _2("<B'|I|A'>"); //allvir allvir
-    TArray2 I_Aa; I_Aa("B',e") = _2("<B'|I|e>"); // allvir vir
-    TArray2 I_Ac; I_Ac("B',c'") = _2("<B'|I|c'>"); // allvir cabs
-    tim.exit();
 
     // make B matrix
     tim.enter("compute B matrix"); // time B matrix
+
+    TArray4 B;
+
     tim.enter("term1");
     // term1
-    TArray4 term1;
-    term1("A',B',x,y") = F_AB("A',B'") * gamma1("x,y");
+    {
+
+      TArray4 term1;
+      term1("x,B',y,A'") = F_AB("A',B'") * gamma1("x,y");
+      B("x,B',y,A'") = term1("x,B',y,A'");
+    }
+    madness::World::get_default().gop.fence();
     tim.exit();
     std::cout << "term1 of B complete \n" << std::endl;
 #if DEBUGG
@@ -1778,33 +1770,51 @@ double PT2R12::cabs_singles_Dyall()
     std::cout.precision(10);
     // std::cout << "term1 of B: \n" << term1 << std::endl;
 #endif
-    tim.enter("term2");
+
     // term2
-    TArray4 term2;
-    term2("A',B',x,y") = - I_AB("A',B'")*(h_ij("i,y") * gamma1("x,i") + g_ijkl("i,j,k,y") * gamma2("k,x,i,j"));
+    tim.enter("term2");
+    {
+      TArray2 I_AB; I_AB("B',A'") = _2("<B'|I|A'>"); //allvir allvir
+      TArray4 g_ijkl; g_ijkl("n1,m1,m,n") = _4("<n1 m1|g|m n>"); // occ
+      TArray4 term2;
+      term2("x,B',y,A'") = - ( gamma1("x,i") * h_ij("i,y") + gamma2("x,j,k,i") * g_ijkl("j,k,i,y") )*I_AB("A',B'");
+      B("x,B',y,A'") = B("x,B',y,A'") + term2("x,B',y,A'");
+    }
+    madness::World::get_default().gop.fence();
     tim.exit();
     std::cout << "term2 of B complete \n" << std::endl;
 #if DEBUGG
     // std::cout << "term2 of B: \n" << term2 << std::endl;
 #endif
-    tim.enter("term3");
+
     // term3
-    TArray4 term3;
-    term3("A',B',x,y") = I_Aa("B',b")*I_Aa("A',a")*(h_ab("a,b") - F_ab("a,b"))*gamma1("x,y");
+    tim.enter("term3");
+    {
+      TArray2 F_ab; F_ab("e,f") = _2("<e|F|f>");  //vir vir
+      TArray2 h_ab; h_ab("e,f") = _2("<e|h|f>");   //vir vir
+      TArray4 term3;
+      term3("x,B',y,A'") = (h_ab("a,b") - F_ab("a,b"))*I_aA("b,B'")*I_aA("a,A'")*gamma1("x,y");
+      B("x,B',y,A'") = B("x,B',y,A'") + term3("x,B',y,A'");
+    }
+    madness::World::get_default().gop.fence();
     tim.exit();
     std::cout << "term3 of B complete \n" << std::endl;
-    tim.enter("term4");
-    // term4
-    TArray4 term4;
-    term4("A',B',x,y") = I_Aa("B',b")* I_Aa("A',a")*(g_iabj("i,a,j,b")*gamma2("j,x,i,y") + g_iajb("j,a,b,i")*gamma2("i,x,y,j"));
 
+    // term4
+    tim.enter("term4");
+    {
+      TArray4 g_abij; g_abij("f,e,m,n") = _4("<m f|g|n e>"); // occ vir occ vir
+      TArray4 g_iajb; g_iajb("f,e,m,n") = _4("<m e|g|f n>"); // occ vir vir occ
+      TArray4 term4;
+      term4("x,B',y,A'") = (g_abij("a,b,j,i")*gamma2("i,x,j,y") + g_iajb("a,b,j,i")*gamma2("i,x,y,j"))*I_aA("b,B'")*I_aA("a,A'");
+      B("x,B',y,A'") = B("x,B',y,A'") + term4("x,B',y,A'");
+    }
+    madness::World::get_default().gop.fence();
+    tim.exit();
+    std::cout << "term4 of B complete \n" << std::endl;
 #if DEBUGG
     std::cout << "term4 of B: \n" << term4 << std::endl;
 #endif
-    tim.exit();
-    std::cout << "term4 of B complete \n" << std::endl;
-    TArray4 B;
-    B("B',A',y,x") = term1("B',A',y,x") + term2("B',A',y,x") + term3("B',A',y,x") + term4("B',A',y,x");
 
     tim.exit();
     std::cout << "B complete \n" << std::endl;
@@ -1824,23 +1834,30 @@ double PT2R12::cabs_singles_Dyall()
 
     //compute b matrix in a(x) = b
     TArray2 b;
-    b("x,B'") = gamma1("j,x") * F_ci("c,j") * I_Ac("B',c");
-    // x we trying to solve, C
     TArray2 x;
-    x("x,B'") = b("x,B'");
-    if (cabs_singles_h0_ == string("dyall_1")) {
-      // compute b based on Equation(15)
-      b("x,B'") = -b("x,B'");
-    }
+    {
+      TArray2 I_cA; I_cA("c',B'") = _2("<c'|I|B'>"); // allvir cabs
+      TArray2 F_ci; F_ci("n,c'") = _2("<n|F|c'>");  //cabs occ
 
-    if (cabs_singles_h0_ == string("dyall_2")) {
-      // - gamma(j,k) * h(k, beta) - gamma(jm,kl)*g(beta m, kl)
-      TArray4 g_Bmkl;
-      g_Bmkl("B',m,m1,n1") = _4("<B' m|g|m1 n1>");  //g(allvir, occ, occ, occ)
-      TArray2 h_iA;
-      h_iA("m,A'") = srr12intrmds._2("<m|h|A'>");  //h(occ,allvir)
-      b("j,B'") = - gamma1("j,k") * h_iA("k,B'") - gamma2("j,m,k,l") * g_Bmkl("B',m,k,l");
+      b("x,B'") = gamma1("x,j") * F_ci("j,c") * I_cA("c,B'");
+      // x we trying to solve, C
+
+      x("x,B'") = b("x,B'");
+      if (cabs_singles_h0_ == string("dyall_1")) {
+        // compute b based on Equation(15)
+        b("x,B'") = -b("x,B'");
+      }
+
+      if (cabs_singles_h0_ == string("dyall_2")) {
+        // - gamma(j,k) * h(k, beta) - gamma(jm,kl)*g(beta m, kl)
+        TArray4 g_mklB;
+        g_mklB("m,m1,n1,B'") = _4("<B' m|g|m1 n1>");//g(allvir, occ, occ, occ)
+        TArray2 h_iA;
+        h_iA("m,A'") = _2("<m|h|A'>");//h(occ,allvir)
+        b("j,B'") = - gamma1("j,k") * h_iA("k,B'") - gamma2("j,m,k,l") * g_mklB("m,k,l,B'");
+      }
     }
+    madness::World::get_default().gop.fence();
 
 #if DEBUGG
     std::cout << "b matrix: \n" << b << std::endl;
@@ -1851,26 +1868,30 @@ double PT2R12::cabs_singles_Dyall()
 #endif
 
     // make preconditioner: inverse of diagonal elements <A'|F|A'> - <m|h|m>
-    typedef detail::diag_precond2<double> pceval_type;//!< evaluator of preconditioner
-    typedef TA::Array<double, 2, LazyTensor<double, 2, pceval_type> > TArray2d;
-    TArray2d Delta_iA(b.get_world(), b.trange());
-
-    pceval_type Delta_iA_gen(TA::array_to_eigen(h_ij), TA::array_to_eigen(F_AB));
-    // construct local tiles
-    for (auto t = Delta_iA.trange().tiles().begin();
-        t != Delta_iA.trange().tiles().end(); ++t) {
-      if (Delta_iA.is_local(*t)) {
-        std::array<std::size_t, 2> index;
-        std::copy(t->begin(), t->end(), index.begin());
-        madness::Future<typename TArray2d::value_type> tile(
-            (LazyTensor<double, 2, pceval_type>(&Delta_iA, index, &Delta_iA_gen)));
-
-        // Insert the tile into the array
-        Delta_iA.set(*t, tile);
-      }
-    }
     TArray2 preconditioner;
-    preconditioner("i,A'") = Delta_iA("i,A'");
+    {
+
+      typedef detail::diag_precond2<double> pceval_type;//!< evaluator of preconditioner
+      typedef TA::Array<double, 2, LazyTensor<double, 2, pceval_type> > TArray2d;
+      TArray2d Delta_iA(b.get_world(), b.trange());
+
+      pceval_type Delta_iA_gen(TA::array_to_eigen(h_ij), TA::array_to_eigen(F_AB));
+      // construct local tiles
+      for (auto t = Delta_iA.trange().tiles().begin();
+          t != Delta_iA.trange().tiles().end(); ++t) {
+        if (Delta_iA.is_local(*t)) {
+          std::array<std::size_t, 2> index;
+          std::copy(t->begin(), t->end(), index.begin());
+          madness::Future<typename TArray2d::value_type> tile(
+              (LazyTensor<double, 2, pceval_type>(&Delta_iA, index, &Delta_iA_gen)));
+
+          // Insert the tile into the array
+          Delta_iA.set(*t, tile);
+        }
+      }
+      preconditioner("y,A'") = Delta_iA("y,A'");
+    }
+    madness::World::get_default().gop.fence();
 
     tim.enter("conjugate solver"); // time conjugate solver
 
@@ -1888,6 +1909,7 @@ double PT2R12::cabs_singles_Dyall()
 
     //calculate the second order energy based on Equation (16)
     double E = -1.0*dot(x("j,A'"), b("j,A'"));
+    shutdown_mpqc3();
     return E;
   }
   else{
@@ -1924,41 +1946,51 @@ double PT2R12::cabs_singles_Fock() {
 
     bootup_mpqc3();
 
-    // make all the matrixes
-    tim.enter("compute integrals");
     // go to file sr_r12intermediates.h for notation
+
     // density matrices
     TArray2 gamma1; gamma1("m,n") = _2("<m|gamma|n>"); // occ
     TArray4 gamma2; gamma2("n1,m,m1,n") = _4("<n1 m|gamma|m1 n>"); // occ
+
 #if DEBUGG
     std::cout << "gamma1: \n" << gamma1 << std::endl;
 #endif
     // fock matrices
     TArray2 F_ij; F_ij("m,n") = _2("<m|F|n>"); // occ occ
     TArray2 F_iA; F_iA("m,A'") = _2("<m|F|A'>"); // occ allvir
-    TArray2 F_ci; F_ci("c',n") = _2("<c'|F|n>");  //cabs occ
+
     TArray2 F_AB; F_AB("A',B'") = _2("<A'|F|B'>"); // allvir allvir
 
     //delta
     TArray2 I_AB; I_AB("B',A'") = _2("<B'|I|A'>"); // allvir allvir
-    TArray2 I_Ac; I_Ac("B',c'") = _2("<B'|I|c'>"); // allvir cabs
 
-    tim.exit();
 
     tim.enter("compute B matrix");
     // make B matrix in Equation (18)
-    // term1
-    TArray4 term1;
-    term1("A',B',x,y") = F_AB("A',B'") * gamma1("x,y");
-    // term2
-    TArray4 term2;
-    term2("A',B',x,y") = I_AB("A',B'") * F_ij("i,j") * gamma2("i,x,j,y");
-    // term3
-    TArray4 term3;
-    term3("A',B',x,y") = - I_AB("A',B'") * F_ij("i,j") * gamma1("i,j") * gamma1("x,y");
-    // B
     TArray4 B;
-    B("A',B',x,y") = term1("A',B',x,y") + term2("A',B',x,y") + term3("A',B',x,y");
+
+    // term1
+    {
+      TArray4 term1;
+      term1("x,B',y,A'") = F_AB("A',B'") * gamma1("x,y");
+      B("x,B',y,A'") =  term1("x,B',y,A'");
+    }
+    madness::World::get_default().gop.fence();
+    // term2
+    {
+      TArray4 term2;
+      term2("x,B',y,A'") =  F_ij("i,j") * gamma2("i,x,j,y") * I_AB("A',B'");
+      B("x,B',y,A'") = B("x,B',y,A'") + term2("x,B',y,A'");
+    }
+    madness::World::get_default().gop.fence();
+    // term3
+    {
+      TArray4 term3;
+      term3("x,B',y,A'") = - F_ij("i,j") * gamma1("x,y") * gamma1("i,j")  * I_AB("A',B'");
+      B("x,B',y,A'") = B("x,B',y,A'") + term3("x,B',y,A'");
+    }
+    madness::World::get_default().gop.fence();
+
 #if DEBUGG
     std::cout << "B matrix: \n" << B << std::endl;
 #endif
@@ -1967,35 +1999,48 @@ double PT2R12::cabs_singles_Fock() {
     //  solve the linear algebra problem a(x)=b in Equation (15)
     //
     // x we trying to solve, C
-    TArray2 x; x("x,B'") = gamma1("x,j") * F_ci("c',j") * I_Ac("B',c'");
-    // b in a(x) = b
-    TArray2 b; b("x,B'") = -x("x,B'");
+
+    TArray2 x;
+    TArray2 b;
+    {
+      TArray2 I_Ac; I_Ac("B',c'") = _2("<B'|I|c'>"); // allvir cabs
+      TArray2 F_ci; F_ci("c',n") = _2("<c'|F|n>");  //cabs occ
+      x("x,B'") =  I_Ac("B',c'") * F_ci("c',j") * gamma1("j,x");
+      // b in a(x) = b
+      b("x,B'") = -x("x,B'");
+    }
+    madness::World::get_default().gop.fence();
+
 #if DEBUGG
     std::cout << "b matrix: \n" << b << std::endl;
 #endif
     std::cout << " range of b matrix" << b.trange().elements() << std::endl;
 
     // make preconditioner: inverse of diagonal elements <A'|F|A'> - <m|F|m>
-    typedef diag_precond2<double> pceval_type;//!< evaluator of preconditioner
-    typedef TA::Array<double, 2, LazyTensor<double, 2, pceval_type> > TArray2d;
-    TArray2d Delta_iA(b.get_world(), b.trange());
-
-    pceval_type Delta_iA_gen(TA::array_to_eigen(F_ij), TA::array_to_eigen(F_AB));
-    // construct local tiles
-    for (auto t = Delta_iA.trange().tiles().begin();
-        t != Delta_iA.trange().tiles().end(); ++t) {
-      if (Delta_iA.is_local(*t)) {
-        std::array<std::size_t, 2> index;
-        std::copy(t->begin(), t->end(), index.begin());
-        madness::Future<typename TArray2d::value_type> tile(
-            (LazyTensor<double, 2, pceval_type>(&Delta_iA, index, &Delta_iA_gen)));
-
-        // Insert the tile into the array
-        Delta_iA.set(*t, tile);
-      }
-    }
     TArray2 preconditioner;
-    preconditioner("i,A") = Delta_iA("i,A'");
+    {
+      typedef diag_precond2<double> pceval_type; //!< evaluator of preconditioner
+      typedef TA::Array<double, 2, LazyTensor<double, 2, pceval_type> > TArray2d;
+      TArray2d Delta_iA(b.get_world(), b.trange());
+
+      pceval_type Delta_iA_gen(TA::array_to_eigen(F_ij), TA::array_to_eigen(F_AB));
+      // construct local tiles
+      for (auto t = Delta_iA.trange().tiles().begin();
+          t != Delta_iA.trange().tiles().end(); ++t) {
+        if (Delta_iA.is_local(*t)) {
+          std::array<std::size_t, 2> index;
+          std::copy(t->begin(), t->end(), index.begin());
+          madness::Future<typename TArray2d::value_type> tile(
+              (LazyTensor<double, 2, pceval_type>(&Delta_iA, index, &Delta_iA_gen)));
+
+          // Insert the tile into the array
+          Delta_iA.set(*t, tile);
+        }
+      }
+      preconditioner("y,A'") = Delta_iA("y,A'");
+    }
+
+
 #if DEBUGG
     std::cout << "preconditioner: \n" << preconditioner << std::endl;
 #endif
@@ -2012,7 +2057,8 @@ double PT2R12::cabs_singles_Fock() {
 #endif
 
     //calculate the second order energy based on Equation (16)
-    double E = -1.0* dot(x("i,A'"),b("i,A'"));
+    double E = -1.0* dot(x("y,A'"),b("y,A'"));
+    shutdown_mpqc3();
     return E;
   }
   else{

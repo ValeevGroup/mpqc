817bd0d7898efaacc4c8a2073e1afb27855dabf2
justusc <justusc@vt.edu>
2014-08-11 16:42:59 -0400

    Updated MADNESS includes to reflect changes in the directory structure of MADNESS.

--- src/lib/util/madness/world.h
+++ src/lib/util/madness/world.h
@@ -32,7 +32,7 @@
 #ifndef _mpqc_src_lib_util_madness_world_h
 #define _mpqc_src_lib_util_madness_world_h
 
-#include <world/worldfwd.h>
+#include <madness/world/worldfwd.h>
 #include <util/class/class.h>
 #include <util/keyval/keyval.h>
 

16da9337437c22dbb612e719cb94cdcb1666eafb
drewlewis <drew90@vt.edu>
2014-08-11 14:14:14 -0500

    New files to eventually hold basis and shell. Also adding MPQC libs to project.

---
+++ Basis/basis.h
@@ -0,0 +1,6 @@
+#ifndef BASIS_H
+#define BASIS_H
+
+//#include <chemistry/qc/basis/gausbasis.h>
+
+#endif // BASIS_H
---
+++ cmake/modules/FindMPQC.cmake
@@ -0,0 +1,47 @@
+if (MPQC_INCLUDE_DIRS)
+
+  set(MPQC_FOUND TRUE)
+
+else ()
+
+  set(MPQC_INCLUDE_DIR "")
+  message(STATUS "MPQC_DIR = ${MPQC_PATH}")
+  find_path(MPQC_INCLUDE_DIR NAMES mpqc_config.h
+      PATHS
+      ${MPQC_PATH}/include/mpqc/
+    )
+  message(STATUS "MPQC_INCLUDE_DIR = ${MPQC_INCLUDE_DIR}")
+
+  if(MPQC_INCLUDE_DIR)
+    set(MPQC_LIB_DIR ${MPQC_PATH}/lib)
+    set(MPQC_DEFAULT_COMPONENT_LIST chemistry)
+
+    foreach(_component ${MPQC_DEFAULT_COMPONENT_LIST})
+      list(FIND MPQC_DEFAULT_COMPONENT_LIST ${MPQC_LIB_DIR}/${_component} _comp_found)
+
+      #if(_comp_found EQUAL -1)
+      #  message(FATAL_ERROR "Invalid DMHM component: ${_component}")
+      #endif()
+
+      if(_comp_found)
+        set(MPQC_${_component}_FOUND TRUE)
+      endif()
+    endforeach()
+
+    set(MPQC_LIBRARIES "")
+    foreach(_component ${MPQC_DEFAULT_COMPONENT_LIST})
+      if(MPQC_${_component}_FOUND)
+        list(APPEND MPQC_LIBRARIES ${_component})
+      endif()
+    endforeach()
+
+    set(MPQC_FOUND TRUE)    
+    
+  endif(MPQC_INCLUDE_DIR)
+
+  mark_as_advanced(MPQC_INCLUDE_DIRS MPQC_LIBRARIES)
+  SET(MPQC_INCLUDE_DIRS ${MPQC_INCLUDE_DIR} CACHE PATH "The mpqc include path.")
+  SET(MPQC_LIBRARIES ${MPQC_LIBRARIES} CACHE PATH "The mpqc library path.")
+
+endif()
+

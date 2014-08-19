0eb1f58dc50ae69b42e10853955147698ff7916d
drewlewis <drew90@vt.edu>
2014-08-18 23:17:40 -0400

    Added Framework for low rank tile implementation.

---
+++ Matrix/lr_tile.h
@@ -0,0 +1,4 @@
+#ifndef LR_TILE_H
+#define LR_TILE_H
+
+#endif // LR_TILE_H
---
+++ include/tiledarray.h
@@ -0,0 +1,11 @@
+
+#ifndef HEIRCHEM_TILEDARRAY_H
+#define HEIRCHEM_TILEDARRAY_H
+
+#pragma GCC diagnostic push
+#pragma GCC system_header
+#include <tiledarray.h>
+#pragma GCC diagnostic pop
+
+
+#endif // HEIRCHEM_TILEDARRAY_H

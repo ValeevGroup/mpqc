f0518226a4ebe2242479f2e0cbe9282d8250ce1b
drewlewis <drew90@vt.edu>
2014-08-21 14:11:53 -0400

    Can now add LRTiles with TiledArray :).

--- Matrix/Matrix_test.cpp
+++ Matrix/Matrix_test.cpp
@@ -3,20 +3,39 @@
 #include <vector>
 #include "../include/tiledarray.h"
 
-int main(int argc, char** argv) {
-  madness::World& world = madness::initialize(argc, argv);
+int main(int argc, char **argv) {
+  madness::World &world = madness::initialize(argc, argv);
 
   // Test that multiplication works
   Eigen::MatrixXd Z = Eigen::MatrixXd::Random(1000, 1000);
   Eigen::MatrixXd Q = Eigen::MatrixXd::Random(1000, 1000);
 
+  Z = Z.transpose() * Z;
+  Q = Q.transpose() * Q;
+
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es(Z);
+  Eigen::MatrixXd C = es.eigenvectors();
+  Eigen::VectorXd V = es.eigenvalues();
+
+  std::for_each(V.data(), V.data() + 990, [](double &x) { x = 0.0; });
+  Z = C * V.asDiagonal() * C.transpose();
+
+  es.compute(Q);
+  C = es.eigenvectors();
+  V = es.eigenvalues();
+
+  std::for_each(V.data(), V.data() + 990, [](double &x) { x = 0.0; });
+  Q = C * V.asDiagonal() * C.transpose();
+
   LRTile<double> B(Z);
-  LRTile<double> C(Q);
-  LRTile<double> D = B.mult(C);
+  std::cout << "Rank of B = " << B.rank() << std::endl;
+  LRTile<double> F(Q);
+  std::cout << "Rank of F = " << F.rank() << std::endl;
+  LRTile<double> D = B.mult(F);
   std::cout << "Does multiply work (1:yes,0:no)? "
             << D.matrixLR().isApprox(Z * Q) << "\n";
 
-  LRTile<double> E = B.add(C);
+  LRTile<double> E = B.add(F);
   std::cout << "Does add work (1:yes,0:no)? " << E.matrixLR().isApprox(Z + Q)
             << "\n";
 
@@ -27,8 +46,14 @@ int main(int argc, char** argv) {
   std::cout << "Size of added LRTile after compression = " << E.size()
             << " size of full matrix = " << E.matrixLR().size() << "\n";
 
+  std::cout << "Does compressed add work (1:yes,0:no)? "
+            << E.matrixLR().isApprox(Z + Q) << "\n";
+
+  Eigen::ColPivHouseholderQR<Eigen::MatrixXd> qr(E.matrixLR());
+  std::cout << "The QRP rank of E is " << qr.rank() << std::endl;
+
   std::vector<unsigned int> blocking;
-  for (auto i : {0, 2, 4}) {
+  for (auto i : {0, 5, 10}) {
     blocking.push_back(i);
   }
 
@@ -38,6 +63,34 @@ int main(int argc, char** argv) {
   TiledArray::TiledRange trange(blocking2.begin(), blocking2.end());
 
   TiledArray::Array<double, 2, LRTile<double>> A(world, trange);
+
+  auto it = A.begin();
+  auto end = A.end();
+
+  for (; it != end; ++it) {
+    Eigen::MatrixXd Q = Eigen::MatrixXd::Random(5, 5);
+    Q = Q.transpose() * Q;
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es(Q);
+    Eigen::MatrixXd C = es.eigenvectors();
+    Eigen::VectorXd V = es.eigenvalues();
+
+    std::for_each(V.data(), V.data() + 4, [](double &x) { x = 0.0; });
+    Q = C * V.asDiagonal() * C.transpose();
+
+    TiledArray::Array<double, 2, LRTile<double>>::value_type tile(Q);
+
+    *it = tile;
+  }
+  std::cout << "A = \n" << A << std::endl;
+
+  TiledArray::Array<double, 2, LRTile<double>> TB;
+  TB("i,j") = A("i,j") + A("i,j");
+  world.gop.fence();
+  std::cout << "TB = \n" << TB << std::endl;
+
+  TB("i,j") = A("i,k") * A("k,j");
+  world.gop.fence();
+  std::cout << "TB = \n" << TB << std::endl;
 
   madness::finalize();
   return 0;
--- Matrix/lr_tile.h
+++ Matrix/lr_tile.h
@@ -2,7 +2,9 @@
 #define LR_TILE_H
 
 #include "../include/eigen.h"
+#include "../include/tiledarray.h"
 #include <utility>
+#include <iostream>
 
 namespace detail {
 
@@ -14,7 +16,7 @@ template <typename T> using LR_pair = std::pair<EigMat<T>, EigMat<T>>;
 /**
 * qr_decomp returns the low rank Q and R matrices as a pair.
 */
-template <typename T> LR_pair<T> qr_decomp(const EigMat<T>& input, double cut) {
+template <typename T> LR_pair<T> qr_decomp(const EigMat<T> &input, double cut) {
   Eigen::ColPivHouseholderQR<EigMat<T>> qr(input);
   qr.setThreshold(cut);
 
@@ -33,13 +35,18 @@ template <typename T> LR_pair<T> qr_decomp(const EigMat<T>& input, double cut) {
  */
 template <typename T> class LRTile {
 public:
-  typedef T value_type; /// The data type of the tile, typically double.
+  typedef LRTile eval_type; /// The data type of the tile, typically double.
+  typedef T value_type;     /// The data type of the tile, typically double.
+  typedef TiledArray::Range range_type; /// Tensor range type
+  typedef double numeric_type;
+  typedef std::size_t size_type; /// The data type of the tile, typically
+                                 /// double.
 
   template <typename U> using EigMat = detail::EigMat<U>;
 
   LRTile() = default;
   ~LRTile() = default;
-  LRTile(const LRTile& rhs) = default;
+  LRTile(const LRTile &rhs) = default;
   // LRTile(LRTile &&rhs) = default; Must write by hand :P.
 
   /**
@@ -47,7 +54,7 @@ public:
    * given an input matrix.
    * @param input is an eigen matrix which with matching data type to the class.
    */
-  explicit LRTile(const EigMat<T>& input, double cut = 1e-08) : L_(), R_() {
+  explicit LRTile(const EigMat<T> &input, double cut = 1e-08) : L_(), R_() {
     auto QR_pair = detail::qr_decomp(input, cut);
     L_ = std::get<0>(QR_pair);
     R_ = std::get<1>(QR_pair);
@@ -59,7 +66,7 @@ public:
    * @param left_mat the left hand matrix
    * @param right_mat the right hand matrix
    */
-  explicit LRTile(const EigMat<T>& left_mat, const EigMat<T>& right_mat)
+  explicit LRTile(const EigMat<T> &left_mat, const EigMat<T> &right_mat)
       : L_(left_mat), R_(right_mat), rank_(right_mat.cols()) {}
 
   /**
@@ -67,7 +74,7 @@ public:
    * @param a Low Rank tile to multiple the current tile with.
    * @return a new Low Rank tile.
    */
-  LRTile mult(const LRTile<T> right) const {
+  LRTile mult(const LRTile<T> &right) const {
     // Check which rank is smaller
     bool use_left_rank = (rank() < right.rank());
     EigMat<T> L;
@@ -100,8 +107,42 @@ public:
     R.topRows(rank()) = matrixR();
     R.bottomRows(right.rank()) = right.matrixR();
 
-    return LRTile(L, R);
+    LRTile out(L, R);
+
+    // TODO need heuristic to decide when to compress.
+    out.compress();
+
+    return out;
   }
+
+  bool empty() const { return false; }
+
+  LRTile add(const LRTile &right, const TiledArray::Permutation &perm) const {}
+  LRTile add(const LRTile &right,
+             const numeric_type factor,
+             const TiledArray::Permutation &perm) const {}
+  LRTile
+  add(const value_type &value, const TiledArray::Permutation &perm) const {}
+
+
+  LRTile clone() const {}
+  LRTile permute(const TiledArray::Permutation &perm) const {}
+  LRTile scale(const numeric_type factor) const {}
+  LRTile
+  scale(const numeric_type factor, const TiledArray::Permutation &perm) const {}
+  LRTile &scale_to(const numeric_type factor) const {}
+  LRTile &add_to(const LRTile &right) const {}
+  LRTile &add_to(const LRTile &right, const numeric_type factor) const {}
+  LRTile &add_to(const value_type &value) const {}
+
+  LRTile mult(const LRTile &right, const numeric_type factor) const {}
+  LRTile mult(const LRTile &right, const TiledArray::Permutation &perm) const {}
+  LRTile mult(const LRTile &right,
+              const numeric_type factor,
+              const TiledArray::Permutation &perm) const {}
+
+  LRTile &mult_to(const LRTile &right) {}
+  LRTile &mult_to(const LRTile &right, const numeric_type factor) {}
 
   /**
    * @brief compress attempts to reduce the rank of the tile
@@ -123,18 +164,38 @@ public:
     }
   }
 
+  LRTile gemm(const LRTile &arg1,
+              const LRTile::numeric_type factor,
+              const TiledArray::math::GemmHelper &gemm_config) {}
+
+  // GEMM operation with fused indices as defined by gemm_config; multiply arg1
+  // by arg2, return the result
+  LRTile gemm(const LRTile &arg1,
+              const LRTile &arg2,
+              const LRTile::numeric_type factor,
+              const TiledArray::math::GemmHelper &gemm_config) {}
+
+  // GEMM operation with fused indices as defined by gemm_config; multiply left
+  // by right, store to result
+  void gemm(LRTile &result,
+            const LRTile &arg1,
+            const LRTile &arg2,
+            const LRTile::numeric_type factor,
+            const TiledArray::math::GemmHelper &gemm_config) {}
+
 
   /**
    * @brief matrixL
    * @return the left hand side low rank matrix.
    */
-  inline const EigMat<T>& matrixL() const { return L_; }
+  inline const EigMat<T> &matrixL() const { return L_; }
 
   /**
    * @brief matrixR
    * @return the right hand side low rank matrix.
    */
-  inline const EigMat<T>& matrixR() const { return R_; }
+  inline const EigMat<T> &matrixR() const { return R_; }
+
 
   /**
    * @brief matrixLR
@@ -154,10 +215,21 @@ public:
    */
   inline std::size_t size() const { return L_.size() + R_.size(); }
 
+  template <typename Archive> void serialize(Archive &ar) {}
+
 private:
   EigMat<T> L_;
   EigMat<T> R_;
   std::size_t rank_ = 0;
 };
+
+template <typename T>
+inline std::ostream &operator<<(std::ostream &os, const LRTile<T> &t) {
+  os << "{\n";
+  os << "L \n" << t.matrixL();
+  os << "\n\nR \n" << t.matrixR();
+  os << "\n}";
+  return os;
+}
 
 #endif // LR_TILE_H

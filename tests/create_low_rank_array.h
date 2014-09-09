bebaa3840ab49c4e0dc7e2d04aecb5b37a6043d7
drewlewis <drew90@vt.edu>
2014-09-08 21:13:03 -0400

    Added unit tests for tile_algebra.h

---
+++ tests/create_low_rank_array.h
@@ -0,0 +1,29 @@
+#ifndef CREATE_LOW_RANK_ARRAY_H
+#define CREATE_LOW_RANK_ARRAY_H
+
+#include "../include/eigen.h"
+
+namespace HeirChemTest {
+template <typename T>
+using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+
+template <typename T>
+Matrix<T> low_rank_matrix(int rows, int cols, int rank) {
+    auto full_rank = std::min(rows, cols);
+    assert(rank <= std::min(rows, cols));
+
+    Eigen::JacobiSVD
+        <Matrix<T>> svd(Matrix<T>::Random(rows, cols), Eigen::ComputeThinU
+                        | Eigen::ComputeThinV);
+    auto svals = svd.singularValues();
+
+    for (auto i = 0ul; i < (full_rank - rank) && i < svals.size(); ++i) {
+        svals[(svals.size() - 1) - i] = 0;
+    }
+
+    return svd.matrixU() * svals.asDiagonal() * svd.matrixV().transpose();
+}
+
+} // namespace HeirChemTest
+
+#endif // CREATE_LOW_RANK_ARRAY_H

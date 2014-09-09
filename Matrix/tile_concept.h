7a432b26cc5d238c37eb8a0a9560f2e30e7943c9
drewlewis <drew90@vt.edu>
2014-09-09 09:00:48 -0400

    Trying to figure out how to bring in TileConcept.

--- Matrix/tile_concept.h
+++ Matrix/tile_concept.h
@@ -1,19 +1,24 @@
 #ifndef TILE_CONCEPT_H
 #define TILE_CONCEPT_H
 
-#include <include/tiledarray.h>
+#include "../include/tiledarray.h"
 #include <memory>
 
 class TileConcept {
   public:
     virtual ~TileConcept() = default;
-    virtual TileConcept clone() const = 0;
+    virtual TileConcept *clone() const = 0;
+
+    virtual void const *data() const = 0;
+    virtual void *data() = 0;
+    virtual std::type_info const &type() const noexcept = 0;
+
 };
 
 template <typename T>
 class TileModel : public TileConcept {
   public:
-    TileModel(T t) : tile_type_(std::move(t)) {}
+    TileModel(T t) : tile_(std::move(t)) {}
     TileModel(const TileModel &t) = default;
     TileModel(TileModel &&t) = default;
     TileModel &operator=(TileModel &&t) = default;
@@ -22,8 +27,15 @@ class TileModel : public TileConcept {
         return *this;
     }
 
-    virtual TileConcept clone() const override final {
+    virtual TileConcept *clone() const override final {
         return new TileModel(*this);
+    }
+
+    virtual void const *data() const override final { return &tile_; }
+    virtual void *data() override final { return &tile_; }
+
+    virtual std::type_info const &type() const noexcept override final {
+        return typeid(T);
     }
 
   private:
@@ -36,26 +48,45 @@ class TileAble {
 
     template <typename T>
     TileAble(TiledArray::Range range, T t)
-        : range_(range_),
-          tile_ptr(std::make_shared<TileModel<T>>(std::move(t))) {
-    }
+        : range_(std::move(range)),
+          tile_ptr_(std::make_shared<TileModel<T>>(std::move(t))) {}
+
+    template <typename T>
+    TileAble(T t)
+        : range_(TiledArray::Range::Range()),
+          tile_ptr_(std::make_shared<TileModel<T>>(std::move(t))) {}
 
     TileAble(const TileAble &t)
-        : range_(range), tile_ptr(std::move(t.tile_ptr->clone())) {}
+        : range_(t.range_), tile_ptr_(std::move(t.tile_ptr_->clone())) {}
     TileAble(TileAble &&t) = default;
     TileAble &operator=(TileAble &&t) = default;
     TileAble &operator=(TileAble t) {
-        *this = std::move(t);
+        range_ = std::move(t.range_);
+        tile_ptr_ = std::move(t.tile_ptr_);
         return *this;
     }
 
     inline TileAble clone() const {
-        return TileAble(range_, std::move(tile_ptr->clone()));
+        return TileAble(range_, std::move(tile_ptr_->clone()));
+    }
+
+    inline std::type_info const &type() const { return tile_ptr_->type(); }
+
+    template <typename T>
+    T *cast_data() {
+        assert(tile_ptr_->type() == typeid(T));
+        return static_cast<T *>(tile_ptr_->data());
+    }
+
+    template <typename T>
+    T const *cast_data() const {
+        assert(tile_ptr_->type() == typeid(T));
+        return static_cast<T const *>(tile_ptr_->data());
     }
 
   private:
     TiledArray::Range range_;
-    std::shared_ptr<TileConcept> tile_ptr;
+    std::shared_ptr<TileConcept> tile_ptr_;
 };
 
 #endif // TILE_CONCEPT_H
---
+++ tests/tileable_test.cpp
@@ -0,0 +1,12 @@
+#include "../matrix/tile_concept.h"
+#include <gtest/gtest.h>
+
+TEST(TileConceptTest, DefaultConstructor){
+  EXPECT_NO_THROW(TileAble(3.0));
+  TileAble tile(3.0);
+  EXPECT_TRUE((tile.type() == typeid(double)));
+  EXPECT_EQ(3.0, *(tile.cast_data<double>()));
+}
+
+
+

64d831ae100343d39079e63bda6b80e8d63ad43b
drewlewis <drew90@vt.edu>
2014-09-14 10:21:59 -0400

    Added an example for tile algebra, used as example and for generating timings.

--- Matrix/examples/tile_algebra.cpp
+++ Matrix/examples/tile_algebra.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[]) {
     int start = 50;
-    int end = 501;
+    int end = 2001;
 
     Eigen::MatrixXd A;
     Eigen::MatrixXd B;
@@ -15,17 +15,17 @@ int main(int argc, char *argv[]) {
             std::string file_name = "Square_gemm_no_add_" + std::to_string(i)
                                     + "_" + std::to_string(j) + ".dat";
             std::ofstream out_file(file_name);
-            for (auto k = start; k <= end; k += 50) {
-                out_file << i << "," << i << "," << k << ",";
+            for (auto k = start; k < end; k += 50) {
+                out_file << i << "," << j << "," << k << ",";
                 double time = madness::wall_time();
                 A = Eigen::MatrixXd::Random(i, k);
-                B = Eigen::MatrixXd::Random(k, i);
+                B = Eigen::MatrixXd::Random(k, j);
                 C = algebra::cblas_gemm(A, B, 1.0);
                 time = madness::wall_time() - time;
                 out_file << time << std::endl;
             }
             out_file.close();
-            std::cout << "Done with " << i << " " << i << std::endl;
+            std::cout << "Done with Gemm no add " << i << " " << j << std::endl;
         }
     }
 
@@ -36,17 +36,17 @@ int main(int argc, char *argv[]) {
                                     + "_" + std::to_string(j) + ".dat";
             std::ofstream out_file(file_name);
             C = Eigen::MatrixXd::Random(i, j);
-            for (auto k = start; k <= end; k += 50) {
-                out_file << i << "," << i << "," << k << ",";
+            for (auto k = start; k < end; k += 50) {
+                out_file << i << "," << j << "," << k << ",";
                 double time = madness::wall_time();
                 A = Eigen::MatrixXd::Random(i, k);
-                B = Eigen::MatrixXd::Random(k, i);
+                B = Eigen::MatrixXd::Random(k, j);
                 algebra::cblas_gemm_inplace(A, B, C, 1.0, 1.0);
                 time = madness::wall_time() - time;
                 out_file << time << std::endl;
             }
             out_file.close();
-            std::cout << "Done with " << i << " " << i << std::endl;
+            std::cout << "Done with Gemm " << i << " " << j << std::endl;
         }
     }
 
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
             std::string file_name = "QR_Init_" + std::to_string(i) + "_"
                                     + std::to_string(j) + ".dat";
             std::ofstream out_file(file_name);
-            for (auto k = 10; k <= std::min(i, j); k += 10) {
+            for (auto k = 10; k < std::min(i, j); k += 10) {
                 C = HeirChemTest::low_rank_matrix<double>(i, j, k);
                 out_file << i << "," << j << "," << k << ",";
                 double time = madness::wall_time();
@@ -64,7 +64,7 @@ int main(int argc, char *argv[]) {
                 out_file << time << std::endl;
             }
             out_file.close();
-            std::cout << "Done with " << i << " " << j << std::endl;
+            std::cout << "Done with Qr " << i << " " << j << std::endl;
         }
     }
 

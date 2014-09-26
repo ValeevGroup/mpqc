64b6bcac3caadd4898da216423ef23eb3c20b32c
drewlewis <drew90@vt.edu>
2014-09-26 08:43:35 -0400

    Removed rank from the full tile gemm profile since it no longer was needed.

--- Matrix/profiling/full_rank_gemm.cpp
+++ Matrix/profiling/full_rank_gemm.cpp
@@ -1,6 +1,7 @@
 #include "../full_rank_tile.h"
 #include "../tile_ops.h"
 #include "../tile_mutations.h"
+#include "../include/tiledarray.h"
 #include <iostream>
 #include <string>
 
@@ -8,7 +9,6 @@ using matrix = Eigen::MatrixXd;
 
 int main(int argc, char *argv[]) {
     auto dim = 0;
-    auto rank = 0;
     if (argc >= 2) {
         dim = std::stoi(argv[1]);
     } else {
@@ -16,28 +16,39 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    matrix A(dim,dim);
+    matrix A(dim, dim);
+    TiledArray::Tensor<double> TA_A(TiledArray::Range(dim, dim), 1.0);
 
-    auto size = dim * rank;
+    TiledArray::math::GemmHelper gemm_helper(
+        madness::cblas::CBLAS_TRANSPOSE::NoTrans,
+        madness::cblas::CBLAS_TRANSPOSE::NoTrans, 2, 2, 2);
+
+
+    auto size = dim * dim;
     auto Ap = A.data();
-    for(auto i = 0; i < size; ++i){
-      *(Ap + i) = 1.0;
+    for (auto i = 0; i < size; ++i) {
+        *(Ap + i) = 1.0;
     }
 
-    FullRankTile<double> fA{A};
+    FullRankTile<double> fA{A.eval()};
     FullRankTile<double> fB{std::move(A)};
 
     double time = madness::wall_time();
     auto C = binary_ops::gemm(fA, fB, 1.0);
     time = madness::wall_time() - time;
 
+    double ta_time = madness::wall_time();
+    TA_A = TA_A.gemm(TA_A, 1.0, gemm_helper);
+    ta_time = madness::wall_time() - ta_time;
+
     double time2 = madness::wall_time();
-    ternary_mutations::gemm(C,fA,fB,1.0,1.0);
+    ternary_mutations::gemm(C, fA, fB, 1.0, 1.0);
     time2 = madness::wall_time() - time2;
 
-    std::cout << "Time for " << dim << "x" << dim
-              << " gemm no add was " << time << " s\n"
-              << "Time for gemm with add = " << time2 << " s\n";
+    std::cout << "Time for " << dim << "x" << dim << " gemm no add was " << time
+              << " s\n"
+              << "Time for gemm with add = " << time2 << " s\n"
+              << "Time for TA gemm no add = " << ta_time << " s\n";
 
     return 0;
 }

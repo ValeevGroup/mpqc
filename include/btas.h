e758f02ccfcaa8c0aede86c050eb113691fd3eff
drewlewis <drew90@vt.edu>
2014-11-17 11:14:25 -0500

    Added Module for finding BTAS, added include header for BTAS, added a very simple test for BTAS, and Finally updated the TODO list.

---
+++ include/btas.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef TCC_INCLUDE_BTAS_H
+#define TCC_INCLUDE_BTAS_H
+
+#pragma GCC diagnostic push
+#pragma GCC system_header
+#include <btas/btas.h>
+#include <btas/tensor.h>
+#include <btas/tensorview.h>
+#include <btas/tensor_func.h>
+#pragma GCC diagnostic pop
+
+#endif // TCC_INCLUDE_BTAS_H

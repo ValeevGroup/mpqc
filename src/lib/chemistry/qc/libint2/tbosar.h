eb894981a802308f28354639093ca980afe59156
Eduard Valeyev <eduard@valeyev.net>
2015-01-08 15:14:59 -0500

    fixed 6abe3729bbbdcced342644ebb2b07289204c5f7e

--- src/lib/chemistry/qc/libint2/tbosar.h
+++ src/lib/chemistry/qc/libint2/tbosar.h
@@ -64,7 +64,8 @@ namespace sc {
           Fm_Eval_(CoreIntsEngine<_FmEvalType>::instance(mmax)) {
         }
 
-        const double* eval(double* Fm_table, unsigned int mmax, double T, double rho = 0.0) const {
+        const double* eval(double* Fm_table, unsigned int mmax, double T, double rho = 0.0,
+                           void* /* thread_local_scratch */ = 0) const {
           static double oo2np1[] = {1.0,  1.0/3.0,  1.0/5.0,  1.0/7.0,  1.0/9.0,
             1.0/11.0, 1.0/13.0, 1.0/15.0, 1.0/17.0, 1.0/19.0,
             1.0/21.0, 1.0/23.0, 1.0/25.0, 1.0/27.0, 1.0/29.0,

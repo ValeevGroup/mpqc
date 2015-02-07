fec2a920232725ad4db4fa15c62e23f00b4d2815
Eduard Valeyev <eduard@valeyev.net>
2015-02-07 12:20:30 -0500

    travis-ci: amend 32104972e8a84dd7777ad23a84078964f75620a1

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -6,5 +6,5 @@ sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo add-apt-repository ppa:kalakris/cmake -y
 sudo apt-get update
-sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev
+sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev cmake-2.8.11.2
 cmake --version

da32dc506bfa8a815092a72e9430e115ae9026a2
Eduard Valeyev <eduard@valeyev.net>
2015-02-07 15:44:56 -0500

    travis-ci: set FC env var

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -6,6 +6,7 @@ if [ "$CXX" = "g++" ]; then
     export CC=/usr/bin/gcc-$GCC_VERSION
     export CXX=/usr/bin/g++-$GCC_VERSION
 fi
+export FC=/usr/bin/gfortran-$GCC_VERSION
 export CXXFLAGS="-std=c++11"
 export LDFLAGS=$OPENMPFLAGS
 

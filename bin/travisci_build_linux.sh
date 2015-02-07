aa18d10207ae40cf231586d6829b6e117d673a92
Eduard Valeyev <eduard@valeyev.net>
2015-02-07 10:59:24 -0500

    travis-ci: first attempt

---
+++ bin/travisci_build_linux.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+if [ "$CXX" = "g++" ]; then
+    export CC=/usr/bin/gcc-$GCC_VERSION
+    export CXX=/usr/bin/g++-$GCC_VERSION
+fi
+export CXXFLAGS="-std=c++11"
+export LDFLAGS=$OPENMPFLAGS
+
+./configure
+make -j2
+make check0

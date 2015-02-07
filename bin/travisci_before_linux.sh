44687695525b5ed6abd8ada02e92c55cd5635162
Eduard Valeyev <eduard@valeyev.net>
2015-02-07 15:17:35 -0500

    travis-ci: checking gfortran presence

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -8,3 +8,4 @@ sudo add-apt-repository ppa:kalakris/cmake -y
 sudo apt-get update
 sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev cmake
 cmake --version
+gfortran-$GCC_VERSION --version

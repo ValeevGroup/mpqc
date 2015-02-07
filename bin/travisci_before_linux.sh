d50faf9a310df43aee47acb335bd2b2cd0d0490d
Eduard Valeyev <eduard@valeyev.net>
2015-02-07 15:30:23 -0500

    travis-ci: looking for gfortran

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -6,6 +6,6 @@ sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo add-apt-repository ppa:kalakris/cmake -y
 sudo apt-get update
-sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev cmake
+sudo apt-get install -y g++-$GCC_VERSION gfortran-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev cmake
 cmake --version
 gfortran-$GCC_VERSION --version

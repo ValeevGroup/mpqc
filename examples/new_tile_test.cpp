df8385bdf5a3b83cdcd0fbc25aa3f6805c93e2a2
Drew Lewis <drew90@vt.edu>
2015-03-31 20:15:20 -0400

    Changed clang format slightly, and have essentually finished add for for the One True Tile.  It is only missing scaling by a constant factor. add_to is not yet working, but I don't think it will take to long.

--- examples/new_tile_test.cpp
+++ examples/new_tile_test.cpp
@@ -7,16 +7,40 @@
 
 #include <iostream>
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     auto &world = madness::initialize(argc, argv);
 
-    auto tr1 = TA::TiledRange1{2,2,2};
+    auto tr1 = TA::TiledRange1{0, 2, 4};
     auto tr = TA::TiledRange{tr1, tr1};
 
-    TA::Array<double, 2, Tile<TA::Tensor<double>>> A(world, tr);
+    TA::Array<double, 2, Tile<TA::Tensor<double>>, TA::DensePolicy> A(world,
+                                                                      tr);
+
     A.set_all_local(1.0);
 
     std::cout << "Array A\n" << A << std::endl;
+
+    for (auto fut : A) {
+        std::cout << "Tile is empty? " << fut.get().empty() << "\n";
+        std::cout << "Norm of tile = " << fut.get().norm() << "\n";
+    }
+
+    TA::Array<double, 2, Tile<TA::Tensor<double>>, TA::DensePolicy> B(world,
+                                                                      tr);
+    B.set_all_local(2.0);
+
+    decltype(A) C;
+    C("i,j") = A("i,j") + B("i,j");
+
+    std::cout << "C = \n" << C << std::endl;
+
+    C("i,j") = C("i,j") + A("i,j");
+
+    std::cout << "C = \n" << C << std::endl;
+
+    C("i,j") = C("i,j") + C("j,i");
+
+    std::cout << "C = \n" << C << std::endl;
 
     madness::finalize();
     return 0;
--- tensor/tcc_tile.h
+++ tensor/tcc_tile.h
@@ -10,14 +10,73 @@
 namespace tcc {
 namespace tensor {
 
+namespace detail {
+
+// Helper class to facilitate ADL
 template <typename T>
-class Tile {
-    TA::Range range_;
-    std::shared_ptr<T> tile_;
+class TileModel {
+    T tile_;
 
   public:
     using eval_type = T;
-    using value_type = Tile<T>;
+    using value_type = T;
+    using numeric_type = typename T::numeric_type;
+    using size_type = std::size_t;
+
+    TileModel() = default;
+    ~TileModel() = default;
+    TileModel(TileModel const &) = default;
+    TileModel(TileModel &&) = default;
+    TileModel &operator=(TileModel &&) = default;
+    TileModel &operator=(TileModel const &) = default;
+
+    TileModel(T &&t) : tile_{std::move(t)} {}
+    TileModel(T const &t) : tile_{t} {}
+
+    T &tile() { return tile_; }
+    T const &tile() const { return tile_; }
+
+    bool empty_() const { return empty(tile_); }
+    auto norm_() const -> decltype(norm(tile_)) { return norm(tile_); }
+
+    T clone_() const { return clone(tile_); }
+
+    template <typename Right>
+    auto add_(Right const &r) const -> decltype(add(tile_, r)) {
+        return add(tile_, r);
+    }
+
+    template <typename Right>
+    auto add_(Right const &r,
+              TA::Permutation const &p) const -> decltype(add(tile_, r, p)) {
+        return add(tile_, r, p);
+    }
+
+    template <typename Right>
+    T &add_to_(Right const &r) {
+        add_to(tile_, r);
+        return tile_;
+    }
+
+    template <typename Right>
+    T &add_to_(Right const &r, TA::Permutation const &p) {
+        add_to(tile_, r, p);
+        return tile_;
+    }
+
+    T permute_(TA::Permutation const &p) const { return permute(tile_, p); }
+};
+
+} // namespace detail
+
+
+template <typename T>
+class Tile {
+    TA::Range range_;
+    std::shared_ptr<detail::TileModel<T>> tile_;
+
+  public:
+    using value_type = T;
     using range_type = TA::Range;
     using numeric_type = typename T::numeric_type;
     using size_type = std::size_t;
@@ -31,22 +90,50 @@ class Tile {
 
     Tile(TA::Range r) : range_{std::move(r)} {}
     Tile(TA::Range r, T t)
-        : range_{std::move(r)}, tile_{std::make_shared<T>(std::move(t))} {}
-    Tile(TA::Range r, std::shared_ptr<T> st)
-        : range_{std::move(r)}, tile_{std::move(st)} {}
+            : range_{std::move(r)},
+              tile_{std::make_shared<detail::TileModel<T>>(
+                    detail::TileModel<T>{std::move(t)})} {}
 
-    template<typename Value>
+    template <typename Value>
     Tile(TA::Range r, Value v)
-        : range_{std::move(r)}, tile_{std::make_shared<T>(T{r,v})} {}
+            : range_{std::move(r)},
+              tile_{std::make_shared<detail::TileModel<T>>(
+                    detail::TileModel<T>{detail::TileModel<T>{T{r, v}}})} {}
 
-    std::shared_ptr<T> tile_ptr() { return tile_; }
-    const std::shared_ptr<T> tile_ptr() const { return tile_; }
-    T &tile() { return *tile_; }
-    T const &tile() const { return *tile_; }
-    Tile clone() const { return Tile{range_, *tile_}; }
-    TA::Range const &range() const { return range; }
-    bool empty() const { return !tile_; }
-    double norm() const { return tile_->norm(); }
+    T &tile() { return tile_->tile(); }
+    T const &tile() const { return tile_->tile(); }
+
+    Tile clone() const { return Tile{range_, tile_->clone_()}; }
+
+    TA::Range const &range() const { return range_; }
+
+    bool empty() const { return (!tile_ || tile_->empty_()); }
+
+    auto norm() const -> decltype(tile_ -> norm_()) { return tile_->norm_(); }
+
+    template <typename U>
+    auto add(
+          Tile<U> const &u) const -> Tile<decltype(tile_ -> add_(u.tile()))> {
+        auto out = tile_->add_(u.tile());
+        return Tile<decltype(out)>{range_, std::move(out)};
+    }
+
+    template <typename U>
+    auto add(Tile<U> const &u, TA::Permutation const &p) const
+          -> Tile<decltype(tile_ -> add_(u.tile(), p))> {
+        auto out = tile_->add_(u.tile(), p);
+        return Tile<decltype(out)>{range_, std::move(out)};
+    }
+
+    template <typename U>
+    Tile &add_to(Tile<U> const &u) {
+        tile_->add_to_(u.tile());
+        return *this;
+    }
+
+    Tile permute(TA::Permutation const &p) const {
+        return Tile{range_, tile_->permute_(p)};
+    }
 
     template <typename Archive>
     void serialize(Archive &ar) {
@@ -54,8 +141,8 @@ class Tile {
     }
 };
 
-template<typename T>
-inline std::ostream& operator<<(std::ostream& os, Tile<T> const & tile){
+template <typename T>
+inline std::ostream &operator<<(std::ostream &os, Tile<T> const &tile) {
     os << tile.range() << ": " << tile.tile() << "\n";
     return os;
 }

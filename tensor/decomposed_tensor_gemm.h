c634ea010f98737ea33449b89ab132605338d218
drewlewis <drew90@vt.edu>
2015-04-22 08:46:25 -0400

    Writing new gemm paths to DecomposedTensor

--- tensor/decomposed_tensor_gemm.h
+++ tensor/decomposed_tensor_gemm.h
@@ -6,6 +6,7 @@
 #include "decomposed_tensor.h"
 #include "decomposed_tensor_algebra.h"
 #include "decomposed_tensor_addition.h"
+#include "decomposed_tensor_gemm_helper.h"
 
 namespace tcc {
 namespace tensor {
@@ -13,19 +14,15 @@ namespace tensor {
 template <typename T>
 DecomposedTensor<T>
 gemm(DecomposedTensor<T> const &a, DecomposedTensor<T> const &b, const T factor,
-     TA::math::GemmHelper const &gemm_helper) {
-    // Assume that b is never decomposed.
-    if (gemm_helper.left_rank() == 3) {
-        if (gemm_helper.right_rank() == 2
-            && gemm_helper.result_rank() == 3) { // Eri3 * D
-            if (a.ndecomp() == 1) {              // Reg gemm
-                return DecomposedTensor<T>{a.cut(),
-                                           a.tensor(0).gemm(b.tensor(0), factor,
-                                                            gemm_helper)};
-            } else if (a.ndecomp() == 2) { // LR gemm
-                auto Rp = a.tensor(1).gemm(b.tensor(0), factor, gemm_helper);
-                return DecomposedTensor<T>{a.cut(), a.tensor(0).clone(),
-                                           std::move(Rp)};
+     TA::math::GemmHelper const &gh) {
+    if (gh.result_rank() == 3) {
+        if (gh.left_rank() == 3) {
+            if (gh.right_rank() == 2) {
+                return detail::low_rank_gemm<3, 3, 2>{}(a, b, factor, gh);
+            }
+        } else if (gh.left_rank() == 2) {
+            if (gh.right_rank() == 3) {
+                return detail::low_rank_gemm<3, 2, 3>{}(a, b, factor, gh);
             }
         }
     }
@@ -36,73 +33,20 @@ gemm(DecomposedTensor<T> const &a, DecomposedTensor<T> const &b, const T factor,
 template <typename T>
 DecomposedTensor<T> &gemm(DecomposedTensor<T> &c, DecomposedTensor<T> const &a,
                           DecomposedTensor<T> const &b, const T factor,
-                          TA::math::GemmHelper const &gemm_helper) {
-    // Only density contraction for now.
-    assert(gemm_helper.result_rank() == 3 && gemm_helper.left_rank() == 3
-           && gemm_helper.right_rank() == 2);
-
-    // assume b is never decomposed.
-    if (c.ndecomp() == 1) {
-        if (a.ndecomp() == 1) {
-            c.tensor(0).gemm(a.tensor(0), b.tensor(0), factor, gemm_helper);
-        } else {
-
-            auto Rp = a.tensor(1).gemm(b.tensor(0), factor, gemm_helper);
-            auto NoT = gemm_helper.left_op();
-            auto gh = TA::math::GemmHelper(NoT, NoT, c.tensor(0).range().dim(),
-                                           a.tensor(0).range().dim(),
-                                           Rp.range().dim());
-            c.tensor(0).gemm(a.tensor(0), Rp, factor, gh);
+                          TA::math::GemmHelper const &gh) {
+    if (gh.result_rank() == 3) {
+        if (gh.left_rank() == 3) {
+            if (gh.right_rank() == 2) { // Eri3 * D
+                return detail::low_rank_gemm<3, 3, 2>{}(c, a, b, factor, gh);
+            }
+        } else if(gh.left_rank() == 2){
+            if(gh.right_rank() == 3){
+                return detail::low_rank_gemm<3,2,3>{}(c,a, b, factor, gh);
+            }
         }
-        // Doesn't seem to be necessary
-        /* auto decomp_c = algebra::two_way_decomposition(c); */
-        /* if (!decomp_c.empty()) { */
-        /*     c = std::move(decomp_c); */
-        /* } */
-        return c;
-    } else {
-        if (a.ndecomp() == 1) {
-            auto ab_tensor = a.tensor(0).gemm(b.tensor(0), 1.0, gemm_helper);
-            const auto NoT = gemm_helper.left_op();
-            auto gh = TA::math::GemmHelper(NoT, NoT, 3, 2, 3);
-            ab_tensor.gemm(c.tensor(0), c.tensor(1), 1.0, gh);
-            c = DecomposedTensor<double>(c.cut(), std::move(ab_tensor));
-            /* auto decomp_test = algebra::two_way_decomposition(c); */
-
-            /* if (!decomp_test.empty()) { */
-            /*     c = std::move(decomp_test); */
-            /* } */
-
-            return c;
-        }
-
-        auto ab = gemm(a, b, factor, gemm_helper);
-        c = add(c, ab);
-        auto const &c_left_extent = c.tensor(0).range().size();
-        auto const &c_right_extent = c.tensor(1).range().size();
-        const auto long_dim = c_right_extent[1] * c_right_extent[2];
-        auto out_dim = c.rank();
-        const auto full_rank = std::min(c_left_extent[0], long_dim);
-
-        if (out_dim >= full_rank / 6) {
-            algebra::recompress(c);
-            out_dim = c.rank();
-        }
-
-        if (out_dim > full_rank/2) {
-            c = DecomposedTensor<T>(c.cut(), algebra::combine(c));
-        }
-
-        /* auto c_test = algebra::two_way_decomposition( */
-        /*       DecomposedTensor<double>(c.cut(), algebra::combine(c))); */
-        /* if(!c_test.empty()){ */
-        /*     c = std::move(c_test); */
-        /* } */
-
-        return c;
     }
-
     assert(false);
+    return c;
 }
 
 } // namespace tensor
---
+++ tensor/decomposed_tensor_gemm_helper.h
@@ -0,0 +1,239 @@
+#pragma once
+
+#ifndef TCC_TENSOR_DECOMPOSEDTENSORGEMMHELPER_H
+#define TCC_TENSOR_DECOMPOSEDTENSORGEMMHELPER_H
+
+#include "decomposed_tensor.h"
+#include "decomposed_tensor_algebra.h"
+#include "decomposed_tensor_addition.h"
+
+namespace tcc {
+namespace tensor {
+namespace detail {
+
+template <typename U>
+using Dtensor = DecomposedTensor<U>;
+
+using GHelper = TA::math::GemmHelper;
+
+static constexpr auto NoT = madness::cblas::CBLAS_TRANSPOSE::NoTrans;
+
+template <unsigned long... Dims>
+struct low_rank_gemm {
+
+    template <typename T>
+    Dtensor<T> operator()(Dtensor<T> const &a, Dtensor<T> const &b,
+                          const T factor, GHelper const &gh) {
+        std::cout << "No overload for gemm with dimensions " << gh.result_rank()
+                  << " = " << gh.left_rank() << " * " << gh.right_rank()
+                  << std::endl;
+        assert(false);
+        return Dtensor<T>{};
+    }
+};
+
+// Eri3("X,a,b") * D("b,k") = C("X, a, k")
+template <>
+struct low_rank_gemm<3ul, 3ul, 2ul> {
+    template <typename T>
+    Dtensor<T> operator()(Dtensor<T> const &a, Dtensor<T> const &b, const T f,
+                          GHelper const &gh) {
+        // for now assume that b is always full rank.
+        if (a.ndecomp() == 1) { // Reg gemm
+            return Dtensor<T>{a.cut(), a.tensor(0).gemm(b.tensor(0), f, gh)};
+        } else if (a.ndecomp() == 2) { // LR gemm
+            auto Rp = a.tensor(1).gemm(b.tensor(0), f, gh);
+            return Dtensor<T>{a.cut(), a.tensor(0).clone(), std::move(Rp)};
+        }
+    }
+
+    template <typename T>
+    Dtensor<T> &operator()(Dtensor<T> &c, Dtensor<T> const &a,
+                           Dtensor<T> const &b, const T f, GHelper const &gh) {
+        // assume b is never decomposed.
+        if (c.ndecomp() == 1) {
+            if (a.ndecomp() == 1) {
+                c.tensor(0).gemm(a.tensor(0), b.tensor(0), f, gh);
+            } else {
+
+                auto Rp = a.tensor(1).gemm(b.tensor(0), f, gh);
+                auto NoT = gh.left_op();
+                auto gh = TA::math::GemmHelper(NoT, NoT,
+                                               c.tensor(0).range().dim(),
+                                               a.tensor(0).range().dim(),
+                                               Rp.range().dim());
+                c.tensor(0).gemm(a.tensor(0), Rp, f, gh);
+            }
+            // Doesn't seem to be necessary
+            /* auto decomp_c = algebra::two_way_decomposition(c); */
+            /* if (!decomp_c.empty()) { */
+            /*     c = std::move(decomp_c); */
+            /* } */
+            return c;
+        } else {
+            if (a.ndecomp() == 1) {
+                auto ab_tensor = a.tensor(0).gemm(b.tensor(0), 1.0, gh);
+                const auto NoT = gh.left_op();
+                auto gh = TA::math::GemmHelper(NoT, NoT, 3, 2, 3);
+                ab_tensor.gemm(c.tensor(0), c.tensor(1), 1.0, gh);
+                c = DecomposedTensor<double>(c.cut(), std::move(ab_tensor));
+                /* auto decomp_test = algebra::two_way_decomposition(c); */
+
+                /* if (!decomp_test.empty()) { */
+                /*     c = std::move(decomp_test); */
+                /* } */
+
+                return c;
+            }
+
+            auto ab = this->operator()(a, b, f, gh);
+            c = add(c, ab);
+            auto const &c_left_extent = c.tensor(0).range().size();
+            auto const &c_right_extent = c.tensor(1).range().size();
+            const auto long_dim = c_right_extent[1] * c_right_extent[2];
+            auto out_dim = c.rank();
+            const auto full_rank = std::min(c_left_extent[0], long_dim);
+
+            if (out_dim >= full_rank / 6) {
+                algebra::recompress(c);
+                out_dim = c.rank();
+            }
+
+            if (out_dim > full_rank / 2) {
+                c = DecomposedTensor<T>(c.cut(), algebra::combine(c));
+            }
+
+            return c;
+        }
+
+        assert(false);
+    }
+};
+
+template <>
+struct low_rank_gemm<3ul, 2ul, 3ul> {
+    template <typename T>
+    Dtensor<T> operator()(Dtensor<T> const &a, Dtensor<T> const &b, const T f,
+                          GHelper const &gh) {
+        if (a.ndecomp() == 1) { // Full * 
+            if (b.ndecomp() == 1) { // Full Full
+                return Dtensor<T>{a.cut(),
+                                  a.tensor(0).gemm(b.tensor(0), f, gh)};
+            } else { // Full Low
+                const auto gh_fl = GHelper(NoT, NoT, 2, 2, 2);
+                auto sw = a.tensor(0).gemm(b.tensor(0), f, gh_fl);
+                return Dtensor<T>{a.cut(), std::move(sw), b.tensor(1).clone()};
+            }
+        } else { // Low *
+            if (b.ndecomp() == 1) { // Low Full
+                auto Tw = a.tensor(1).gemm(b.tensor(0), f, gh);
+                return Dtensor<T>{a.cut(), a.tensor(0).clone(), std::move(Tw)};
+            } else { // Low Low
+                const auto gh_m = GHelper(NoT, NoT, 2, 2, 2);
+                auto M = a.tensor(1).gemm(b.tensor(0), 1.0, gh_m);
+
+                // Want to contract over largest dim so output is small.
+                const auto go_left = a.rank() >= b.rank();
+                if (go_left) {
+                    const auto gh_left = GHelper(NoT, NoT, 2, 2, 2);
+                    return Dtensor<T>{a.cut(), a.tensor(0).gemm(M, f, gh_left),
+                                      b.tensor(1).clone()};
+                } else {
+                    const auto gh_right = GHelper(NoT, NoT, 2, 3, 3);
+                    return Dtensor<T>{a.cut(), a.tensor(0).clone(),
+                                      M.gemm(b.tensor(1), f, gh_right)};
+                }
+            }
+        }
+    }
+
+    template <typename T>
+    Dtensor<T> &operator()(Dtensor<T> &c, Dtensor<T> const &a,
+                           Dtensor<T> const &b, const T f, GHelper const &gh) {
+        // assume b is never decomposed.
+        if (c.ndecomp() == 1) { // Full * * 
+            if (a.ndecomp() == 1) { // Full Full *
+                if (b.ndecomp() == 1) { //  Full Full Full
+                    c.tensor(0).gemm(a.tensor(0), b.tensor(0), f, gh);
+                } else { // Full Full Low
+                    const auto gh_left = GHelper(NoT, NoT, 2, 2, 2);
+                    c.tensor(0)
+                          .gemm(a.tensor(0).gemm(b.tensor(0), 1.0, gh_left),
+                                b.tensor(1), f, gh);
+                }
+            } else { // Full Low *
+                if (b.ndecomp() == 1) { //  Full Low Full
+                    const auto gh_right = GHelper(NoT, NoT, 3, 2, 3);
+                    c.tensor(0).gemm(
+                          a.tensor(0),
+                          a.tensor(1).gemm(b.tensor(0), 1.0, gh_right), f, gh);
+                } else { //  Full Low Low
+                    const auto gh_m = GHelper(NoT, NoT, 2, 2, 2);
+                    auto mid = a.tensor(1).gemm(b.tensor(0), 1.0, gh_m);
+
+                    auto const &sa_sizes = a.tensor(0).range().size();
+                    auto const &tb_sizes = b.tensor(1).range().size();
+                    auto const &m_sizes = mid.range().size();
+                    const auto r1 = m_sizes[0];
+                    const auto r2 = m_sizes[1];
+                    const auto X = sa_sizes[0];
+                    const auto ab = tb_sizes[1] * tb_sizes[2];
+
+                    auto left_cost = X * r2 * (r1 + ab);
+                    auto right_cost = ab * r1 * (r2 + X);
+
+                    if (left_cost <= right_cost) {
+                        auto gh_left = GHelper(NoT, NoT, 2, 2, 2);
+                        auto left_tensor = a.tensor(0).gemm(mid, 1.0, gh_left);
+                        c.tensor(0).gemm(left_tensor, b.tensor(1), f, gh);
+                    } else {
+                        auto gh_right = GHelper(NoT, NoT, 3, 2, 3);
+                        auto right_tensor
+                              = mid.gemm(b.tensor(1), 1.0, gh_right);
+                        c.tensor(0).gemm(a.tensor(0), right_tensor, f, gh);
+                    }
+                }
+                // For V into Eri always recompress
+                auto decomp_c = algebra::two_way_decomposition(c);
+                if (!decomp_c.empty()) {
+                    c = std::move(decomp_c);
+                }
+                return c;
+            }
+        } else { // Low * * 
+            if (a.ndecomp() == 1) { // Low Full *
+                if (b.ndecomp() == 1) { // Low Full Full
+                    auto temp = a.tensor(0).gemm(b.tensor(0), f, gh);
+                    temp.gemm(c.tensor(0), c.tensor(1), 1.0, gh);
+
+                    c = Dtensor<T>{c.cut(), std::move(temp)};
+                    auto decomp_c = algebra::two_way_decomposition(c);
+                    if (!decomp_c.empty()) {
+                        c = std::move(decomp_c);
+                    }
+                }
+            } else { // Other cases can be handled by the following LFL LLF and LLL
+                auto ab = this->operator()(a, b, f, gh);
+                c = add(c, ab);
+                algebra::recompress(c);
+                auto out_dim = c.rank();
+                auto left_dim = c.tensor(0).range().size()[0];
+                auto const &right_extent = c.tensor(1).range().size();
+                auto right_dim = right_extent[1] * right_extent[2];
+                if (out_dim >= std::min(left_dim, right_dim) / 2) {
+                    c = Dtensor<T>(c.cut(), algebra::combine(c));
+                }
+            }
+
+            return c;
+        }
+    }
+};
+
+
+} // namespace detail
+} // namespace tensor
+} // namespace tcc
+
+
+#endif // TCC_TENSOR_DECOMPOSEDTENSORGEMMHELPER_H

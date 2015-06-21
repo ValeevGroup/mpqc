4ff3065b0f666f8340f2b9d9d6c4052e85602d6c
Drew Lewis <drew90@vt.edu>
2015-06-21 13:27:18 -0400

    Added some code to use Doxygen and removed the old gtest unit testing since it wasn't being used and I didn't like gtest that much

--- tests/gtest.h
+++
@@ -1,9 +0,0 @@
-#ifndef TCC_TESTS_GTEST_H
-#define TCC_TESTS_GTEST_H
-
-#pragma GCC diagnostic push
-#pragma GCC system_header
-#include <gtest/gtest.h>
-#pragma GCC diagnostic pop
-
-#endif // TCC_TESTS_GTEST_H

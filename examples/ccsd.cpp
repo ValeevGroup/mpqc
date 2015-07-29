e59a8abac942b253110768f46e4df843bc98b556
Chong <pchong90@vt.edu>
2015-07-28 22:49:41 -0400

    clean up ccsd main function, added input for frozen core

--- examples/ccsd.cpp
+++ examples/ccsd.cpp
@@ -151,7 +151,7 @@ int try_main(int argc, char *argv[], madness::World& world) {
                             : false;
 
         // get other info
-        auto frozen_core = true;
+        bool frozen_core = in.HasMember("frozen core") ? in["frozen core"].GetBool() : false;
 
       volatile int debug
               = in.HasMember("debug break") ? in["debug break"].GetInt() : 0;
@@ -537,7 +537,6 @@ int try_main(int argc, char *argv[], madness::World& world) {
             std::cout << "Frozen Core: " << n_frozen_core << "  electrons" << std::endl;
             n_frozen_core = n_frozen_core/2;
         }
-        std::cout << "Frozen Core: " << n_frozen_core << "  electrons" << std::endl;
 
       S_TA = TA::to_new_tile_type(S, to_ta);
       F_TA = TA::to_new_tile_type(F, to_ta);
@@ -549,13 +548,10 @@ int try_main(int argc, char *argv[], madness::World& world) {
       Eig::GeneralizedSelfAdjointEigenSolver<decltype(S_eig)> es(F_eig,
                                                                  S_eig);
       ens = es.eigenvalues().bottomRows(S_eig.rows() - n_frozen_core);
-        std::cout << ens << std::endl;
       auto C_all = es.eigenvectors();
       decltype(S_eig) C_occ = C_all.block(0, n_frozen_core, S_eig.rows(), occupation/2-n_frozen_core);
       decltype(S_eig) C_vir = C_all.rightCols(S_eig.rows() - occupation / 2);
 
-        std::cout << C_all << std::endl;
-        std::cout << C_occ << std::endl;
       std::size_t all = S.trange().elements().extent()[0];
       tre = std::make_shared<TRange1Engine>(occupation/2 - n_frozen_core, all-n_frozen_core, blocksize);
 
@@ -604,7 +600,6 @@ int try_main(int argc, char *argv[], madness::World& world) {
     decltype(F_TA) fock_ai_aparse;
     fock_ai_aparse("a,i") = F_TA("mu,nu")*Cv("mu,a")*Ci("nu,i");
     fock_ai = TA::to_dense(fock_ai_aparse);
-        std::cout << fock_ai << std::endl;
     }
 
     world.gop.fence();

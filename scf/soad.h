da6070ad3feb4aa7a00ed99d8b5c5603a063b3b5
drewlewis <drew90@vt.edu>
2015-08-31 15:03:13 -0400

    Removed soad size printing

--- scf/soad.h
+++ scf/soad.h
@@ -209,8 +209,6 @@ ArrayType fock_from_minimal_v_oh(
     {
         auto EriJ = BlockSparseIntegrals(
               world, eng_pool, utility::make_array(df_bs, min_bs, min_bs), op);
-        utility::print_par(EriJ.get_world(), "\n");
-        utility::print_size_info(EriJ, "SOAD EriJ");
         TA::Array<double, 1, typename decltype(EriJ)::value_type,
                   TA::SparsePolicy> trans;
         trans("P") = EriJ("P,a,b") * D_min("a,b");
@@ -221,7 +219,6 @@ ArrayType fock_from_minimal_v_oh(
     auto EriK
           = BlockSparseIntegrals(world, eng_pool,
                                  utility::make_array(df_bs, obs, min_bs), op);
-    utility::print_size_info(EriK, "SOAD EriK");
     // Reuse EriK so we don't have a temp Array laying around.
     EriK("X,i,a") = V_inv_oh("X,P") * (EriK("P,a,b") * L_d("b,i"));
     K("i,j") = EriK("X,k,i") * EriK("X,k,j");

820956a7c3a9845eff041391cf8e20e7e88f12fc
Eduard Valeyev <eduard@valeyev.net>
2015-12-16 10:03:24 -0500

    travis-ci: fixed cmake 3 install issues

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -3,10 +3,14 @@
 set -e
 
 sudo add-apt-repository ppa:boost-latest/ppa -y
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
+# Add PPA for a newer version GCC
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 # PPA for newer cmake (3.2.3)
 sudo add-apt-repository ppa:george-edison55/precise-backports -y
-sudo apt-get update
-sudo apt-get install -y g++-$GCC_VERSION gfortran-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev cmake
+
+sudo apt-get update -qq
+sudo apt-get -y -qq --no-install-suggests --no-install-recommends --force-yes install cmake cmake-data
+sudo apt-get install -y g++-$GCC_VERSION gfortran-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev
+
 cmake --version
 gfortran-$GCC_VERSION --version

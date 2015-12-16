e42f482fe9e0d70c6b95c72f8c8c9ac768823bbd
Eduard Valeyev <eduard@valeyev.net>
2015-12-16 08:25:37 -0500

    travis-ci: upgraded to cmake 3.2.3

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -4,7 +4,8 @@ set -e
 
 sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
-sudo add-apt-repository ppa:kalakris/cmake -y
+# PPA for newer cmake (3.2.3)
+sudo add-apt-repository ppa:george-edison55/precise-backports -y
 sudo apt-get update
 sudo apt-get install -y g++-$GCC_VERSION gfortran-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev cmake
 cmake --version

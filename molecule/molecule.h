1c0d970f24a6c264a3dcc6abab1dc6fe03b617d6
drewlewis <drew90@vt.edu>
2016-04-25 15:48:54 -0400

    Added documentation to Molecule.

--- molecule/molecule.h
+++ molecule/molecule.h
@@ -19,17 +19,12 @@ namespace molecule {
  * \brief The molecule module contains information about how to make and cluster
  *molecules
  *
- *
- *  The molecule module contains all of the classes which are needed for
- *  clustering.  Ultimately, everything in the molecule module should support
- *  the AtomClusterable interface.
- *
  * @{
  */
 
 /*! \brief Molecule is a class which contains a vector of AtomBasedClusterables
  *
- * At its core molecule is a collection of things which can be collapsed
+ * At its core molecule is a collection of clusters that can be collapsed
  * to atoms.  Its main job is allow for clustering of its clusterables.
  *
  */
@@ -37,9 +32,9 @@ class Molecule : public DescribedClass {
   private:
     std::vector<AtomBasedClusterable> elements_;
 
-    Vec3D com_ = {0, 0, 0};
+    Vec3D com_ = {0, 0, 0}; /// Center of Mass
     double mass_ = 0.0;
-    int64_t charge_ = 0;
+    int64_t charge_ = 0; /// Net charge (# protons - # electrons)
 
     void init(std::istream& file, bool sort_input);
 
@@ -47,33 +42,63 @@ class Molecule : public DescribedClass {
 
     Molecule() = default;
 
+    /*! \brief KeyVal constructor for Molecule
+     * 
+     *  The KeyVal constructor expects a field called file_name and an optional
+     *  input sort_input, which are the name of a file that contains molecular
+     *  coordinates and a boolean telling the constructor whether the atoms
+     *  should be sorted based on their distance from the center of mass. By
+     *  default sort_input is set to true.
+     *  
+     *  The clustering of the KeyVal constructor is currently one cluster per
+     *  atom, but this may change as KeyVal input is finalized.
+     */
     Molecule(const KeyVal& kv);
-
+    
+    /*! \brief Constructor to build Molecule from stream.
+     * 
+     * This constructor has same parameters as the KeyVal constructor. 
+     */
     Molecule(std::istream &file_stream, bool sort_input);
 
+    /*! \brief Constructor to build Molecule from a vector of clusterables
+     * 
+     * This constructor takes a vector of AtomBasedClusterables and uses it to
+     * initialize the Molecule's Clusterables. If sort_input is true the
+     * Clusterables are sorted based on distance from the center of mass. 
+     */
     Molecule(std::vector<AtomBasedClusterable> c, bool sort_input = true);
 
+    /// Function to set the mass of the Molecule
     void set_mass(double mass) {
         Molecule::mass_ = mass;
     }
 
+    /// Function to set the charge of the Molecule
     void set_charge(int64_t charge) {
         Molecule::charge_ = charge;
     }
 
+    /// Charge of the Molecule: charge = (# protons - # electrons)
     int64_t charge() const { return charge_; }
+
+    /// Mass of the Molecule
     double mass() const { return mass_; }
 
+    /// Iterator to the first clusterable in the Molecule
     std::vector<AtomBasedClusterable>::const_iterator begin() const {
         return elements_.begin();
     }
 
+    /// Iterator to one past the final clusterable in the Molecule
     std::vector<AtomBasedClusterable>::const_iterator end() const {
         return elements_.end();
     }
 
+    /// Number of clusters in the Molecule
     int64_t nclusters() const { return elements_.size(); }
 
+    /// Vector containing the Clusterable that make up the Molecule
     std::vector<AtomBasedClusterable> const &clusterables() const {
         return elements_;
     }
@@ -82,15 +107,31 @@ class Molecule : public DescribedClass {
         return charge_ - total_charge;
     }
 
+    /// Computes the number of core electrons in the Molecule.
     int64_t core_electrons() const;
 
+    /// Returns the nuclear repulsion energy of the Molecule.
     double nuclear_repulsion() const;
 
+    /*! \brief A vector of all atoms in the Molecule
+     * 
+     * This function will loop over the clusterables and extract their atoms in
+     * a recursive fashion. 
+     *
+     * \note Returns copies of the atoms, not a reference to the atoms stored
+     * in the Clusterables of the Molecule.
+     */
     std::vector<Atom> atoms() const;
 
+    /*! \brief Center of mass of the Molecule. 
+     * 
+     * Necessary to satisfy the AtomBasedClusterAble interface so Molecules are
+     * also clusterable.
+    */
     Vec3D const &com() const { return com_; }
 };
 
+/// Make Molecules printable
 std::ostream &operator<<(std::ostream &, Molecule const &);
 
 /*! @} */

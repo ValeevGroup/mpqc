e551f558cb0c4dc36b7de2c72def4fb1aef5b10e
drewlewis <drew90@vt.edu>
2016-04-25 15:49:34 -0400

    Text formatting for previous commit.

--- molecule/molecule.h
+++ molecule/molecule.h
@@ -36,48 +36,43 @@ class Molecule : public DescribedClass {
     double mass_ = 0.0;
     int64_t charge_ = 0; /// Net charge (# protons - # electrons)
 
-    void init(std::istream& file, bool sort_input);
+    void init(std::istream &file, bool sort_input);
 
   public:
-
     Molecule() = default;
 
     /*! \brief KeyVal constructor for Molecule
-     * 
+     *
      *  The KeyVal constructor expects a field called file_name and an optional
      *  input sort_input, which are the name of a file that contains molecular
      *  coordinates and a boolean telling the constructor whether the atoms
      *  should be sorted based on their distance from the center of mass. By
      *  default sort_input is set to true.
-     *  
+     *
      *  The clustering of the KeyVal constructor is currently one cluster per
      *  atom, but this may change as KeyVal input is finalized.
      */
-    Molecule(const KeyVal& kv);
-    
+    Molecule(const KeyVal &kv);
+
     /*! \brief Constructor to build Molecule from stream.
-     * 
-     * This constructor has same parameters as the KeyVal constructor. 
+     *
+     * This constructor has same parameters as the KeyVal constructor.
      */
     Molecule(std::istream &file_stream, bool sort_input);
 
     /*! \brief Constructor to build Molecule from a vector of clusterables
-     * 
+     *
      * This constructor takes a vector of AtomBasedClusterables and uses it to
      * initialize the Molecule's Clusterables. If sort_input is true the
-     * Clusterables are sorted based on distance from the center of mass. 
+     * Clusterables are sorted based on distance from the center of mass.
      */
     Molecule(std::vector<AtomBasedClusterable> c, bool sort_input = true);
 
     /// Function to set the mass of the Molecule
-    void set_mass(double mass) {
-        Molecule::mass_ = mass;
-    }
+    void set_mass(double mass) { Molecule::mass_ = mass; }
 
     /// Function to set the charge of the Molecule
-    void set_charge(int64_t charge) {
-        Molecule::charge_ = charge;
-    }
+    void set_charge(int64_t charge) { Molecule::charge_ = charge; }
 
     /// Charge of the Molecule: charge = (# protons - # electrons)
     int64_t charge() const { return charge_; }
@@ -114,18 +109,18 @@ class Molecule : public DescribedClass {
     double nuclear_repulsion() const;
 
     /*! \brief A vector of all atoms in the Molecule
-     * 
+     *
      * This function will loop over the clusterables and extract their atoms in
-     * a recursive fashion. 
+     * a recursive fashion.
      *
      * \note Returns copies of the atoms, not a reference to the atoms stored
      * in the Clusterables of the Molecule.
      */
     std::vector<Atom> atoms() const;
 
-    /*! \brief Center of mass of the Molecule. 
-     * 
-     * Necessary to satisfy the AtomBasedClusterAble interface so Molecules are
+    /*! \brief Center of mass of the Molecule.
+     *
+     * Necessary to satisfy the AtomBasedClusterable interface so Molecules are
      * also clusterable.
     */
     Vec3D const &com() const { return com_; }

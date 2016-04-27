1e9155d55f04266d68c0f93435a726ec7278d90f
drewlewis <drew90@vt.edu>
2016-04-27 10:08:22 -0400

    Changed clang-format to google's default and formated type_traits and keyval.cpp

--- src/mpqc/util/keyval/keyval.cpp
+++ src/mpqc/util/keyval/keyval.cpp
@@ -1,27 +1,27 @@
-
-#include <mpqc/util/keyval/keyval.hpp>
+#include "mpqc/util/keyval/keyval.hpp"
 
 namespace mpqc {
 
-  DescribedClass::keyval_ctor_wrapper_type
-  DescribedClass::type_to_keyval_ctor(const std::string& type_name) {
-    auto& registry = keyval_ctor_registry();
-    if (registry.find(type_name) == registry.end())
-      throw std::runtime_error("DescribedClass::type_to_keyval_ctor -- type not registered");
-    return registry[type_name];
-  }
+DescribedClass::keyval_ctor_wrapper_type DescribedClass::type_to_keyval_ctor(
+    const std::string& type_name) {
+  auto& registry = keyval_ctor_registry();
+  if (registry.find(type_name) == registry.end())
+    throw std::runtime_error(
+        "DescribedClass::type_to_keyval_ctor -- type not registered");
+  return registry[type_name];
+}
 
-  ///////////////////////////////////////////////
+///////////////////////////////////////////////
 
-  KeyVal::KeyVal() : top_tree_(std::make_shared<ptree>()),
+KeyVal::KeyVal()
+    : top_tree_(std::make_shared<ptree>()),
       class_registry_(std::make_shared<class_registry_type>()),
       path_("") {}
 
-  std::shared_ptr<KeyVal::ptree>
-  KeyVal::tree() const {
-    std::shared_ptr<ptree> result(top_tree_,
-                                  &top_tree_->get_child(ptree::path_type{path_, separator}));
-    return result;
-  }
+std::shared_ptr<KeyVal::ptree> KeyVal::tree() const {
+  std::shared_ptr<ptree> result(
+      top_tree_, &top_tree_->get_child(ptree::path_type{path_, separator}));
+  return result;
+}
 
-} // namespace mpqc
+}  // namespace mpqc
--- src/mpqc/util/misc/type_traits.h
+++ src/mpqc/util/misc/type_traits.h
@@ -11,14 +11,15 @@
 #include <type_traits>
 
 namespace mpqc {
-    /*! \brief enable_if_t typedef 
-     *
-     * This is a standard C++ 14 feature, but we don't want to require C++ 14
-     * compilers yet
-     */
-    template< bool B, class T = void >
-    using enable_if_t = typename std::enable_if<B,T>::type;
+
+/*! \brief enable_if_t typedef
+ *
+ * This is a standard C++ 14 feature, but we don't want to require C++ 14
+ * compilers yet
+ */
+template <bool B, class T = void>
+using enable_if_t = typename std::enable_if<B, T>::type;
 
 }
 
-#endif // MPQC_UTIL_MISC_TYPETRAITS_H_
+#endif  // MPQC_UTIL_MISC_TYPETRAITS_H_

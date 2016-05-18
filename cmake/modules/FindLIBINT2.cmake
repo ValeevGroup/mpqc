bc33ea6a57b003382c2ce70f98cc826ce3df40b8
Eduard Valeyev <eduard@valeyev.net>
2016-05-18 15:23:26 -0400

    LIBINT_ -> LIBINT2_ in cmake variable names

--- cmake/modules/FindLIBINT2.cmake
+++ cmake/modules/FindLIBINT2.cmake
@@ -6,57 +6,57 @@ if (NOT ${EIGEN3_FOUND})
   find(Eigen3)
 endif()
 
-if (LIBINT_INCLUDE_DIRS)
-  set (LIBINT_FOUND TRUE)
+if (LIBINT2_INCLUDE_DIRS)
+  set (LIBINT2_FOUND TRUE)
 
-else (LIBINT_INCLUDE_DIRS)
-  set (LIBINT_FOUND FALSE)
+else (LIBINT2_INCLUDE_DIRS)
+  set (LIBINT2_FOUND FALSE)
 
-  if(LIBINT_INSTALL_DIR)
-    set(_LIBINT_INSTALL_DIR ${LIBINT_INSTALL_DIR})
-  endif(LIBINT_INSTALL_DIR)
+  if(LIBINT2_INSTALL_DIR)
+    set(_LIBINT2_INSTALL_DIR ${LIBINT2_INSTALL_DIR})
+  endif(LIBINT2_INSTALL_DIR)
 
-  set(LIBINT_INC_SEARCH_DIR ${_LIBINT_INSTALL_DIR}/include)
+  set(LIBINT2_INC_SEARCH_DIR ${_LIBINT2_INSTALL_DIR}/include)
 
-  find_path(LIBINT_INCLUDE_DIR NAMES libint2.hpp
+  find_path(LIBINT2_INCLUDE_DIR NAMES libint2.hpp
     HINTS
-    ${LIBINT_INC_SEARCH_DIR})
+    ${LIBINT2_INC_SEARCH_DIR})
 
-  find_path(LIBINT_INCLUDE_LIBINT2_DIR NAMES cxxapi.h
+  find_path(LIBINT2_INCLUDE_LIBINT2_DIR NAMES cxxapi.h
     HINTS
-    ${LIBINT_INC_SEARCH_DIR}/libint2)
+    ${LIBINT2_INC_SEARCH_DIR}/libint2)
 
-  set(LIBINT_INCLUDE_DIR ${LIBINT_INCLUDE_DIR} ${LIBINT_INCLUDE_LIBINT2_DIR})
+  set(LIBINT2_INCLUDE_DIR ${LIBINT2_INCLUDE_DIR} ${LIBINT2_INCLUDE_LIBINT2_DIR})
 
-  mark_as_advanced(LIBINT_INCLUDE_DIR)
+  mark_as_advanced(LIBINT2_INCLUDE_DIR)
 
-  set(_LIBINT_LIB_NAMES
+  set(_LIBINT2_LIB_NAMES
     "int2"
     )
 
-  set(_LIBINT_LIBRARY_DIR ${_LIBINT_INSTALL_DIR}/lib)
-  set(LIBINT_LIBRARY "")
+  set(_LIBINT2_LIBRARY_DIR ${_LIBINT2_INSTALL_DIR}/lib)
+  set(LIBINT2_LIBRARY "")
 
-  foreach(_lib ${_LIBINT_LIB_NAMES})
+  foreach(_lib ${_LIBINT2_LIB_NAMES})
     set(current_lib NOTFOUND)
-    find_library(current_lib ${_lib} HINTS ${_LIBINT_LIBRARY_DIR})
+    find_library(current_lib ${_lib} HINTS ${_LIBINT2_LIBRARY_DIR})
     if(NOT current_lib)
-      message(FATAL_ERROR "MPQC could not find Libint lib: ${_lib}")
+      message(FATAL_ERROR "MPQC could not find Libint2 lib: ${_lib}")
     endif(NOT current_lib)
-    LIST(APPEND LIBINT_LIBRARY ${current_lib})
+    LIST(APPEND LIBINT2_LIBRARY ${current_lib})
   endforeach()
 
-  set(LIBINT_LIBRARY_DIR ${_LIBINT_LIBRARY_DIR})
-  mark_as_advanced(LIBINT_LIBRARY)
+  set(LIBINT2_LIBRARY_DIR ${_LIBINT2_LIBRARY_DIR})
+  mark_as_advanced(LIBINT2_LIBRARY)
 
   # CODA
-  if(LIBINT_INCLUDE_DIR AND LIBINT_LIBRARY)
+  if(LIBINT2_INCLUDE_DIR AND LIBINT2_LIBRARY)
   
     # validate version, etc. by compiling tests
     cmake_push_check_state()
   
-    list(APPEND CMAKE_REQUIRED_INCLUDES ${EIGEN3_INCLUDE_DIR} ${LIBINT_INCLUDE_DIR})
-    list(APPEND CMAKE_REQUIRED_LIBRARIES ${LIBINT_LIBRARY})
+    list(APPEND CMAKE_REQUIRED_INCLUDES ${EIGEN3_INCLUDE_DIR} ${LIBINT2_INCLUDE_DIR})
+    list(APPEND CMAKE_REQUIRED_LIBRARIES ${LIBINT2_LIBRARY})
     
     # sanity check
     CHECK_CXX_SOURCE_COMPILES(
@@ -67,9 +67,9 @@ else (LIBINT_INCLUDE_DIRS)
         libint2::finalize();
         return 0;
       }
-      "  LIBINT_COMPILES)
-    if (NOT LIBINT_COMPILES)
-      message(FATAL_ERROR "Could not compile LIBINT test program.\nSee CMakeFiles/CMakeError.log for details")
+      "  LIBINT2_COMPILES)
+    if (NOT LIBINT2_COMPILES)
+      message(FATAL_ERROR "Could not compile LIBINT2 test program.\nSee CMakeFiles/CMakeError.log for details")
     endif()
     
     # make sure libint2 version is up to date
@@ -86,20 +86,20 @@ else (LIBINT_INCLUDE_DIRS)
             libint2::TwoBodyEngine<libint2::cGTG_times_Coulomb> e1;
             return 0;
           }
-      "  LIBINT_IS_UP_TO_DATE)    
-    if (NOT LIBINT_IS_UP_TO_DATE)
-      message(FATAL_ERROR "Libint library is too old: 2.2.0 (alpha or later) is required")
+      "  LIBINT2_IS_UP_TO_DATE)    
+    if (NOT LIBINT2_IS_UP_TO_DATE)
+      message(FATAL_ERROR "Libint2 library is too old: 2.2.0 (alpha or later) is required")
     endif()
     
     cmake_pop_check_state()
   
-    set(LIBINT_FOUND TRUE)
-    set(LIBINT_LIBRARIES ${LIBINT_LIBRARY})
-    set(LIBINT_INCLUDE_DIRS ${LIBINT_INCLUDE_DIR})
-    set(LIBINT_LIBRARY_DIRS ${LIBINT_LIBRARY_DIR})
-    mark_as_advanced(LIBINT_INCLUDE_DIRS LIBINT_LIBRARY_DIRS LIBINT_LIBRARIES)
-    message(STATUS "Found Libint libraries: ${LIBINT_LIBRARIES}")
-  endif(LIBINT_INCLUDE_DIR AND LIBINT_LIBRARY)
+    set(LIBINT2_FOUND TRUE)
+    set(LIBINT2_LIBRARIES ${LIBINT2_LIBRARY})
+    set(LIBINT2_INCLUDE_DIRS ${LIBINT2_INCLUDE_DIR})
+    set(LIBINT2_LIBRARY_DIRS ${LIBINT2_LIBRARY_DIR})
+    mark_as_advanced(LIBINT2_INCLUDE_DIRS LIBINT2_LIBRARY_DIRS LIBINT2_LIBRARIES)
+    message(STATUS "Found Libint libraries: ${LIBINT2_LIBRARIES}")
+  endif(LIBINT2_INCLUDE_DIR AND LIBINT2_LIBRARY)
 
 
-endif(LIBINT_INCLUDE_DIRS)
+endif(LIBINT2_INCLUDE_DIRS)

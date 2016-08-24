81367127faf4e74b3fa3d4d398e25207acc6ac3f
drewlewis <drew90@vt.edu>
2016-08-24 09:52:23 -0400

    Should have been in last commit.

--- examples/mpqc_new.cpp
+++ examples/mpqc_new.cpp
@@ -1,15 +1,17 @@
 /// New MPQC Main file with KeyVal
 
-// #include "../include/tiledarray.h"
+#include "../include/tiledarray.h"
 
 #include "../utility/parallel_file.h"
 #include "../utility/parallel_print.h"
 
 #include <mpqc/chemistry/molecule/molecule.h>
+#include <mpqc/chemistry/molecule/clustering_functions.h>
+#include <mpqc/chemistry/molecule/make_clusters.h>
 #include <mpqc/util/keyval/keyval.hpp>
 #include <mpqc/chemistry/qc/wfn/ao_wfn.h>
 #include <mpqc/chemistry/qc/properties/energy.h>
-// #include <mpqc/chemistry/qc/integrals/atomic_integral.h>
+#include <mpqc/chemistry/qc/integrals/atomic_integral.h>
 
 #include <sstream>
 
@@ -36,8 +38,7 @@ int try_main(int argc, char *argv[], madness::World &world) {
   //
   // construct molecule
   //
-  molecule::Molecule mol =
-      std::make_shared<molecule::Molecule>(kv.keyval("molecule"));
+  auto mol = molecule::Molecule(kv.keyval("molecule"));
   auto nclusters = 1;  // Hard Coded for now
   auto clustered_mol = std::make_shared<molecule::Molecule>(
       molecule::attach_hydrogens_and_kmeans(mol.clusterables(), nclusters));
@@ -46,8 +47,8 @@ int try_main(int argc, char *argv[], madness::World &world) {
   //
   // construct basis registry
   //
-  auto bs = kv.value<basis::Basis>("obs");
-  auto dfbs = kv.value<basis::Basis>("dfbs");
+  auto bs = basis::Basis(kv.keyval("obs"));
+  auto dfbs = basis::Basis(kv.keyval("dfbs"));
 
   auto bs_registry = std::make_shared<OrbitalBasisRegistry>();
   bs_registry->add(OrbitalIndex(L"κ"), bs);
@@ -60,6 +61,27 @@ int try_main(int argc, char *argv[], madness::World &world) {
   integrals::AtomicIntegral<TA::TensorD, TA::SparsePolicy> ao_int(
       world, ta_pass_through, kv.class_ptr<molecule::Molecule>("molecule"),
       bs_registry);
+
+  kv.assign("ao_integrals", &ao_int);
+
+  auto wfn_world = qc::WfnWorld(kv);
+  kv.assign("wfn_world", &wfn_world);
+
+  qc::Wfn* wfn = new qc::AOWfn(kv);
+
+  auto energy_prop = qc::Energy(kv);
+  auto energy_prop_ptr = &energy_prop;
+
+  wfn->visit(energy_prop_ptr);
+  std::cout << "Wfn energy is: " << *energy_prop.result() << std::endl;
+
+  {  // Test ints from Wfn
+    auto S = wfn->wfn_world()->ao_integrals().compute(L"<κ|λ>");
+    // TA::DistArray<TA::TensorD, TA::SparsePolicy> S = ao_int.compute(L"<κ|λ>");
+    assert(S.is_initialized());
+  }
+
+  delete wfn;
 
   return 0;
 }

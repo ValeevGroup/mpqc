c3244a6459976ee1b852610ecffb99cdcac6e95d
Eduard Valeyev <eduard.valeyev@gmail.com>
2016-09-20 07:55:29 -0400

    mpqc main will need the linkage forcing to make a valid executable.

---
+++ src/mpqc/chemistry/molecule/linkage.h
@@ -0,0 +1,15 @@
+#ifndef SRC_MPQC_CHEMISTRY_MOLECULE_LINKAGE_H_
+#define SRC_MPQC_CHEMISTRY_MOLECULE_LINKAGE_H_
+
+#include <mpqc/chemistry/molecule/molecule.h>
+#include <mpqc/util/keyval/forcelink.h>
+
+namespace mpqc {
+namespace molecule {
+
+mpqc::detail::ForceLink<mpqc::molecule::Molecule> fl1;
+
+}
+}
+
+#endif // MPQC_MOLECULE_CLUSTER_H

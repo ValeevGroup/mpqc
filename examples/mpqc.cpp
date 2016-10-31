d7d85616f7933bb9c31bad3a424e68f0aa17a3da
Eduard Valeyev <eduard.valeyev@gmail.com>
2016-10-31 19:13:34 -0400

    amended 5a6b66cb67d55a5f17d04871b7c74806de3ed8a9

--- examples/mpqc.cpp
+++ examples/mpqc.cpp
@@ -338,7 +338,7 @@ int try_main(int argc, char *argv[], madness::World &world) {
   auto ao_in = json::get_nested(in, "AOIntegral");
 
   integrals::AtomicIntegral<TA::TensorD, TA::SparsePolicy> ao_int(
-      world, ta_routines::TensorDPassThrough(),
+      world, TA::Noop<TA::TensorD,true>(),
       std::make_shared<molecule::Molecule>(clustered_mol), bs_registry, param,
       ao_in);
 

6675f3eb806d376c0e709c167f0ac0872ea4c659
drewlewis <drew90@vt.edu>
2016-11-01 11:31:16 -0400

    Fixing Bug in Libint install script for TCI

--- bin/build-libint-linux.sh
+++ bin/build-libint-linux.sh
@@ -33,12 +33,7 @@ cd _build
 # Unpack libint tarball
 tar -xvzf ../external/libint-2.2.0-beta1.tgz
 
-cd libint-2.2.0-beta1
-./autogen.sh
-
-cd ..
 mkdir -p libint
-
 cd libint
 
 export CXXFLAGS="-O0 -std=c++11"
@@ -51,20 +46,6 @@ export CXXFLAGS="-O0 -std=c++11"
 make -j2
 make install
 
-# Install MPICH
-# if [ ! -d "${HOME}/mpich" ]; then
-#     wget --no-check-certificate -q http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
-#     tar -xzf mpich-3.2.tar.gz
-#     cd mpich-3.2
-#     ./configure CC=$CC CXX=$CXX --disable-fortran --disable-romio --prefix=${HOME}/mpich
-#     make -j2
-#     make install
-#     ${HOME}/mpich/bin/mpichversion
-#     ${HOME}/mpich/bin/mpicc -show
-#     ${HOME}/mpich/bin/mpicxx -show
-# else
-#     echo "MPICH installed..."
-#     find ${HOME}/mpich -name mpiexec
-#     find ${HOME}/mpich -name mpicc
-#     find ${HOME}/mpich -name mpicxx
-# fi
+cd $HOME
+ls 
+ls _install

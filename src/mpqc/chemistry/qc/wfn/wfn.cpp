108053914e4f67c47528f4f5485b699f4b580379
Eduard Valeyev <eduard.valeyev@gmail.com>
2016-11-02 13:19:40 -0400

    ++dox

--- src/mpqc/chemistry/qc/wfn/wfn.cpp
+++ src/mpqc/chemistry/qc/wfn/wfn.cpp
@@ -15,21 +15,17 @@ Wavefunction::Wavefunction(const KeyVal &kv) {
   else if(kv.exists("..:wfn_world")){
     wfn_world_ = kv.keyval("..:wfn_world").class_ptr<qc::WavefunctionWorld>();
   }
-  // use global provide wfn_world
+  // use global (i.e. top-level) wfn_world
   else if(kv.exists("$:wfn_world")){
     wfn_world_ = kv.keyval("$:wfn_world").class_ptr<qc::WavefunctionWorld>();
   }
   else{
-    throw std::runtime_error("Wfn could not find wfn_world keyval! \n");
+    throw std::runtime_error("Wavefunction could not find wfn_world keyval! \n");
   }
 
 }
 
 Wavefunction::~Wavefunction() = default;
-
-//////void Wfn::compute(PropertyBase* pb){
-////  throw std::runtime_error("Wfn::compute is abstract mehtod! \n");
-//}
 
 }
 }

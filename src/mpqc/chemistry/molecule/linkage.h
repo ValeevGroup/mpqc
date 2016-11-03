68af4209af6cccc2f1125535bcff3246098ec313
drewlewis <drew90@vt.edu>
2016-11-03 13:06:30 -0400

    Making one more attempt to appease g++ 5.4 in TCI
    There is a namespace error with the forcelink header we are trying to
    figure out.

--- src/mpqc/chemistry/molecule/linkage.h
+++ src/mpqc/chemistry/molecule/linkage.h
@@ -5,10 +5,13 @@
 #include "mpqc/util/keyval/forcelink.h"
 
 namespace mpqc {
+
 class Molecule;
+
+mpqc::detail::ForceLink<Molecule> fl1;
+
 } // namespace mpqc
 
-mpqc::detail::ForceLink<mpqc::Molecule> fl1;
 
 
 #endif  // SRC_MPQC_CHEMISTRY_MOLECULE_LINKAGE_H_

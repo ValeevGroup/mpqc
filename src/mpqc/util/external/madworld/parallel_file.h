0a906524bca265f5786a0b08b908a4a4888a60b7
Eduard Valeyev <eduard.valeyev@gmail.com>
2016-11-05 10:45:14 -0400

    1. added missing library dependencies 2. moved parallel_file.h -> parallel_file.cpp (nontemplated code should not go into the header, except when needs to be inlined)

--- src/mpqc/util/external/madworld/parallel_file.h
+++ src/mpqc/util/external/madworld/parallel_file.h
@@ -2,8 +2,8 @@
 // Created by Chong Peng on 3/24/16.
 //
 
-#ifndef MPQC_PARALLEL_FILE_H
-#define MPQC_PARALLEL_FILE_H
+#ifndef SRC_MPQC_UTIL_EXTERNAL_MADWORLD_PARALLEL_FILE_H_
+#define SRC_MPQC_UTIL_EXTERNAL_MADWORLD_PARALLEL_FILE_H_
 
 #include <madness/world/world.h>
 #include <madness/world/worldgop.h>
@@ -11,46 +11,13 @@
 namespace mpqc {
 namespace utility {
 
-inline void parallel_read_file(madness::World &world,
-                               const std::string &filename, char *&buffer) {
-  int size;
-  std::string contents;
-  if (world.rank() == 0) {
-    std::ifstream input_file(filename, std::ifstream::in);
+void parallel_read_file(madness::World &world, const std::string &filename,
+                        char *&buffer);
 
-    if (input_file.fail()) {
-      std::ostringstream oss;
-      oss << "could not open file \"" << filename << "\"";
-      throw std::invalid_argument(oss.str().c_str());
-    }
-
-    contents = std::string((std::istreambuf_iterator<char>(input_file)),
-                           std::istreambuf_iterator<char>());
-
-    input_file.close();
-    size = contents.size() + 1;
-  }
-
-  world.gop.broadcast(size, 0);
-  buffer = new char[size];
-
-  if (world.rank() == 0) {
-    strcpy(buffer, contents.c_str());
-  }
-
-  world.gop.broadcast(buffer, size, 0);
-}
-
-inline void parallel_read_file(madness::World &world,
-                               const std::string &filename,
-                               std::stringstream &output) {
-  char *buffer;
-  parallel_read_file(world, filename, buffer);
-  output << buffer;
-  delete[] buffer;
-}
+void parallel_read_file(madness::World &world, const std::string &filename,
+                        std::stringstream &output);
 
 }  // end of namespace utility
 }  // end of namespace mpqc
 
-#endif  // MPQC_PARALLEL_FILE_H
+#endif  // SRC_MPQC_UTIL_EXTERNAL_MADWORLD_PARALLEL_FILE_H_

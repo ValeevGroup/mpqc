b7875d0322174823932abe1027a0aa6dae65f7c6
Eduard Valeyev <eduard.valeyev@gmail.com>
2016-11-10 11:31:38 -0500

    introduced primitive validation test suite (all of 1 test so far)

---
+++ tests/validation/test.cmake
@@ -0,0 +1,38 @@
+
+macro(runtest srcDir baseName)
+
+set(OUTPUT_FILE_NAME "${CMAKE_BINARY_DIR}/${baseName}.out")
+
+set(CHECK_CMD "${pythonExec}")
+set(CHECK_ARGS "${srcDir}/check.py"
+"${OUTPUT_FILE_NAME}"
+"${srcDir}/reference/outputs/${baseName}.out")
+
+if (NOT EXISTS "${OUTPUT_FILE_NAME}")
+
+  set(MPQC_CMD "${CMAKE_BINARY_DIR}/../../examples/run.sh")
+  set(MPQC_ARGS "${CMAKE_BINARY_DIR}/../../src/bin/mpqc/mpqc"
+      "-p ${srcDir}/reference/inputs"
+      "${srcDir}/reference/inputs/${baseName}.json")
+
+  execute_process(COMMAND
+                  ${MPQC_CMD} ${MPQC_ARGS}
+                  OUTPUT_FILE "${OUTPUT_FILE_NAME}"
+                  RESULT_VARIABLE MPQC_RESULT)
+
+  if(MPQC_RESULT)
+    message(STATUS "output: ${OUTPUT_VARIABLE}")
+    message(FATAL_ERROR "Error running ${MPQC_CMD}")
+  endif()
+endif()
+
+execute_process(COMMAND
+                ${CHECK_CMD} ${CHECK_ARGS}
+                RESULT_VARIABLE CHECK_RESULT)
+if(CHECK_RESULT)
+    message(FATAL_ERROR "Error running ${CHECK_CMD}")
+endif(CHECK_RESULT)
+
+endmacro(runtest)
+
+runtest(${srcDir} ${testName} ${pythonExec})

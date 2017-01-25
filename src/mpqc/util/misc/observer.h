00cbbaaeaa642ec9718dfd9cbb81b9460ab9e22b
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-01-24 21:52:03 -0500

    No clean way to copy observers, deleted copy ops

--- src/mpqc/util/misc/observer.h
+++ src/mpqc/util/misc/observer.h
@@ -9,10 +9,13 @@ namespace utility {
 
 /// @brief helps to set up messaging between objects via the Observer pattern
 
-/// Use as a base class for any class that wants to send messages to other recipients.
+/// Use as a base class for any class that wants to send messages to other
+/// recipients.
 /// Recipients register callbacks by calling Observable::register_message .
-/// @note Observable holds weak (non-owning) pointers to the observers. Message lifetime
-///       is managed automatically as long as the observers derive from the Observer class.
+/// @note Observable holds weak (non-owning) pointers to the observers. Message
+/// lifetime
+///       is managed automatically as long as the observers derive from the
+///       Observer class.
 /// @sa Observer
 class Observable {
  public:
@@ -21,8 +24,8 @@ class Observable {
   /// adds a message to the list, returns a receipt that the observer
   /// needs to destroy at the end of its life (i.e., in the dtor).
   template <typename Observer>
-  std::shared_ptr<void * const> register_message(Observer *observer,
-                                                 std::function<void()> message) {
+  std::shared_ptr<void *const> register_message(Observer *observer,
+                                                std::function<void()> message) {
     void *observer_void_ptr = static_cast<void *>(observer);
     messages_.insert(std::make_pair(static_cast<void *>(observer), message));
     std::shared_ptr<void *> result(new void *, [this](void **ptr) {
@@ -47,16 +50,17 @@ class Observable {
 
 /// @brief helps to set up messaging between objects via the Observer pattern
 
-/// Use as a base class for any class that wants to receive messages from Observable objects.
+/// Use as a base class for any class that wants to receive messages from
+/// Observable objects.
 /// @sa Observable
 class Observer {
  public:
   /// The default ctor
-  /// @note using the default ctor requires you to call register_message manually
+  /// @note using the default ctor requires you to call register_message
+  /// manually
   Observer() = default;
 
   /// This constructor registers one message
-  /// @tparam Observee a class derived from Observable
   /// @param observee the Observee object to send us the \c message
   /// @param message the message to be sent
   template <typename Observee>
@@ -64,10 +68,18 @@ class Observer {
     register_message(observee, message);
   }
 
+  // cannot copy, must construct a "copy" manually
+  Observer(const Observer &other) = delete;
+  Observer &operator=(const Observer &other) = delete;
+
+  Observer(Observer &&other) = default;
+  Observer &operator=(Observer &&other) = default;
+
   virtual ~Observer() = default;
 
  protected:
-  /// registers a message with a Messenger object, will be called whenever MessengerBase::update()
+  /// registers a message with a Messenger object, will be called whenever
+  /// MessengerBase::update()
   /// is called on \c messenger
   /// @tparam Observee a class derived from Observable
   template <typename Observee>
@@ -78,12 +90,10 @@ class Observer {
   void clear_messages() { receipts_.clear(); }
 
  private:
-  std::vector<std::shared_ptr<void * const>> receipts_;
+  std::vector<std::shared_ptr<void *const>> receipts_;
 };
 
 }  // namespace utility
 }  // namespace mpqc
-
-
 
 #endif /* SRC_MPQC_UTIL_MISC_OBSERVER_H_ */

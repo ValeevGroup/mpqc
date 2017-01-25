469ba108ddde3f105e464138e34a95f7c5a07880
Chong Peng <pchong90@vt.edu>
2017-01-25 10:04:23 -0500

    explicitly set threads to 2 in tests @evaleev let me know if this is OK

--- tests/validation/test.cmake
+++ tests/validation/test.cmake
@@ -8,12 +8,12 @@ set(CHECK_ARGS "${srcDir}/check.py"
 "${OUTPUT_FILE_NAME}"
 "${srcDir}/reference/outputs/${testName}.out")
 
+set(ENV{MAD_NUM_THREADS} 2)
 if (NOT EXISTS "${OUTPUT_FILE_NAME}")
 
   set(MPQC_CMD "${CMAKE_BINARY_DIR}/../../src/bin/mpqc/mpqc")
   set(MPQC_ARGS "-p" "${srcDir}/reference/inputs"
   "${srcDir}/reference/inputs/${testName}.json")
-
   execute_process(COMMAND
                   ${mpiExec}
                   ${mpiNPFlags}

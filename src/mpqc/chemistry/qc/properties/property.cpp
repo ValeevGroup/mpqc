e56cf155872f4cd7f002417815e6be9673772f74
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-01-25 23:59:00 -0500

    shortened FinDispMolecularGradient -> FDGradient

--- src/mpqc/chemistry/qc/properties/property.cpp
+++ src/mpqc/chemistry/qc/properties/property.cpp
@@ -2,4 +2,5 @@
 #include "mpqc/chemistry/qc/properties/property.h"
 #include "mpqc/util/keyval/forcelink.h"
 
-MPQC_CLASS_EXPORT2("FinDispMolecularGradient", mpqc::MolecularFiniteDifferenceDerivative<1,double>);
+MPQC_CLASS_EXPORT2("FDGradient", mpqc::MolecularFiniteDifferenceDerivative<1,double>);
+//MPQC_CLASS_EXPORT2("FDHessian", mpqc::MolecularFiniteDifferenceDerivative<2,double>);

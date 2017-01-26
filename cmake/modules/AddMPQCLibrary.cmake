66d5fc2dcd4f715c0bcd616a5c17806050246285
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-01-26 13:20:50 -0500

    no message

--- cmake/modules/AddMPQCLibrary.cmake
+++ cmake/modules/AddMPQCLibrary.cmake
@@ -78,8 +78,8 @@ macro(add_mpqc_library _name _rawlist_source_files _rawlist_public_header_files 
       endif()      
 
       # import LINK_FLAGS from dependent, unless it's an interface library
-      get_target_property(${dep}_target_type ${dep} TYPE)
-      if (NOT ${_dep}_is_mpqc_hdr_lib AND NOT ${dep}_target_type STREQUAL "INTERFACE_LIBRARY")
+      get_property(${_dep}_target_type TARGET ${_dep} PROPERTY TYPE SET)
+      if (NOT ${_dep}_is_mpqc_hdr_lib AND NOT ${_dep}_target_type STREQUAL "INTERFACE_LIBRARY")
         get_property(_dep_LINK_FLAGS_SET TARGET ${_dep} PROPERTY LINK_FLAGS SET)
         if (_dep_LINK_FLAGS_SET)
           get_property(_dep_LINK_FLAGS TARGET ${_dep} PROPERTY LINK_FLAGS)

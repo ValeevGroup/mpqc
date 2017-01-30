4f667aa7cb0855591167df1de9a81058ecc81a9d
Drew Lewis <drew90@vt.edu>
2017-01-30 15:22:50 -0500

    Removed QQR screening and updated Schwarz screening for the new screener in ao_factory code

--- src/mpqc/chemistry/qc/lcao/integrals/integrals.h
+++ src/mpqc/chemistry/qc/lcao/integrals/integrals.h
@@ -6,11 +6,12 @@
 #include "mpqc/chemistry/qc/lcao/integrals/task_integrals.h"
 #include "mpqc/chemistry/qc/lcao/integrals/task_integral_kernels.h"
 
-#include "mpqc/chemistry/qc/lcao/integrals/screening/cached_shell_info.h"
 #include "mpqc/chemistry/qc/lcao/integrals/screening/screen_base.h"
-#include "mpqc/chemistry/qc/lcao/integrals/screening/qqr_screening.h"
-#include "mpqc/chemistry/qc/lcao/integrals/screening/qvl_screening.h"
 #include "mpqc/chemistry/qc/lcao/integrals/screening/schwarz_screen.h"
+
+// #include "mpqc/chemistry/qc/lcao/integrals/screening/qqr_screening.h"
+// #include "mpqc/chemistry/qc/lcao/integrals/screening/qvl_screening.h"
+// #include "mpqc/chemistry/qc/lcao/integrals/screening/cached_shell_info.h"
 
 #include "mpqc/chemistry/qc/lcao/integrals/direct_tile.h"
 #include "mpqc/chemistry/qc/lcao/integrals/direct_task_integrals.h"

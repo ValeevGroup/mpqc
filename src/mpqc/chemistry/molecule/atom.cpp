8d3190e0bfbfbd0aad2a52e8a64b73744b4b0286
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-01-31 12:35:35 -0500

    - Atom’s atomic number is distinct from charge (think: ghost atoms). - Atom has a KeyVal ctor

--- src/mpqc/chemistry/molecule/atom.cpp
+++ src/mpqc/chemistry/molecule/atom.cpp
@@ -10,7 +10,7 @@ namespace mpqc {
 
 namespace detail {
 
-std::string Z_to_element_name(int64_t Z) {
+std::string Z_to_element(int64_t Z) {
   using libint2::chemistry::element_info;
   for (const auto &e : element_info) {
     if (e.Z == Z) return e.symbol;
@@ -18,11 +18,19 @@ std::string Z_to_element_name(int64_t Z) {
   abort();
 }
 
+int64_t element_to_Z(const std::string& symbol) {
+  using libint2::chemistry::element_info;
+  for (const auto &e : element_info) {
+    if (e.symbol == symbol) return e.Z;
+  }
+  abort();
+}
+
 }  // namespace detail
 
 std::string Atom::xyz_string(bool convert_to_angstroms) const {
-  std::string name = detail::Z_to_element_name(atomic_number_);
-  name += ' ';
+  std::string result = detail::Z_to_element(atomic_number_);
+  result += ' ';
 
   auto unit_factory = UnitFactory::get_default();
   auto bohr_to_ang = unit_factory->make_unit("angstrom").from_atomic_units();
@@ -32,16 +40,31 @@ std::string Atom::xyz_string(bool convert_to_angstroms) const {
     center *= bohr_to_ang;
   }
 
-  name += (std::to_string(center[0]) + ' ');
-  name += (std::to_string(center[1]) + ' ');
-  name += std::to_string(center[2]);
+  result += (std::to_string(center[0]) + ' ');
+  result += (std::to_string(center[1]) + ' ');
+  result += std::to_string(center[2]);
 
-  return name;
+  return result;
+}
+
+Atom::Atom(const KeyVal& kv) {
+  auto element = kv.value<std::string>("element");
+  if (element.empty())
+    throw InputError("invalid element", __FILE__, __LINE__, "element");
+  atomic_number_ = detail::element_to_Z(element);
+
+  auto xyz = kv.value<std::array<double, 3>>("xyz");
+  center_(0) = xyz[0];
+  center_(1) = xyz[1];
+  center_(2) = xyz[2];
+
+  mass_ = kv.value<double>("mass", 0.0);
+  charge_ = kv.value<double>("charge", static_cast<double>(atomic_number_));
 }
 
 std::ostream &operator<<(std::ostream &os, Atom const &a) {
-  os << "Atom: {Z: " << a.charge() << ", mass: " << a.mass()
-     << ", pos: " << a.center().transpose() << "}";
+  os << "Atom: {Z: " << a.atomic_number() << ", charge: " << a.charge()
+     << ", mass: " << a.mass() << ", pos: " << a.center().transpose() << "}";
   return os;
 }
 

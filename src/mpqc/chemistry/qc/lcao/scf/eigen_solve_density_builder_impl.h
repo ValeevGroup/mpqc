342119656244d25b9ef3eb27d3a865cd9f9bf902
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-02-06 14:18:54 -0500

    eigen-based density build can provide all orbitals (not just occupied, since it has access to them)

--- src/mpqc/chemistry/qc/lcao/scf/eigen_solve_density_builder_impl.h
+++ src/mpqc/chemistry/qc/lcao/scf/eigen_solve_density_builder_impl.h
@@ -18,12 +18,12 @@ namespace scf {
 
 template <typename Tile, typename Policy>
 ESolveDensityBuilder<Tile,Policy>::ESolveDensityBuilder(
-    typename ESolveDensityBuilder<Tile,Policy>::array_type const &S, std::vector<typename ESolveDensityBuilder<Tile,Policy>::array_type> r_xyz, int64_t occ,
+    typename ESolveDensityBuilder<Tile,Policy>::array_type const &S, std::vector<typename ESolveDensityBuilder<Tile,Policy>::array_type> r_xyz, int64_t nocc,
     int64_t nclusters, double TcutC, std::string const &metric_decomp_type,
     bool localize)
     : S_(S),
       r_xyz_ints_(r_xyz),
-      occ_(occ),
+      nocc_(nocc),
       n_coeff_clusters_(nclusters),
       TcutC_(TcutC),
       metric_decomp_type_(metric_decomp_type),
@@ -47,39 +47,49 @@ template <typename Tile, typename Policy>
 std::pair<typename ESolveDensityBuilder<Tile,Policy>::array_type, typename ESolveDensityBuilder<Tile,Policy>::array_type>
 ESolveDensityBuilder<Tile,Policy>::operator()(
     typename ESolveDensityBuilder<Tile,Policy>::array_type const &F) {
-  typename ESolveDensityBuilder<Tile,Policy>::array_type Fp, C, Cao, D;
+  typename ESolveDensityBuilder<Tile,Policy>::array_type Fp, C_occ, C_occ_ao, D;
   auto &world = F.world();
 
   auto e0 = mpqc::fenced_now(world);
   Fp("i,j") = M_inv_("i,k") * F("k,l") * M_inv_("j,l");
 
   auto Fp_eig = array_ops::array_to_eigen(Fp);
-  Eigen::SelfAdjointEigenSolver<decltype(Fp_eig)> es(Fp_eig);
 
-  decltype(Fp_eig) C_eig = es.eigenvectors().leftCols(occ_);
+  // TODO must solve on rank-0 only, propagate to the rest
+  Eigen::SelfAdjointEigenSolver<decltype(Fp_eig)> es(Fp_eig);
+  auto eps = es.eigenvalues();
+  decltype(Fp_eig) C_eig = es.eigenvectors();
+  decltype(Fp_eig) C_occ_eig = C_eig.leftCols(nocc_);
   auto tr_ao = Fp.trange().data()[0];
 
-  auto tr_occ = scf::tr_occupied(n_coeff_clusters_, occ_);
-  C = array_ops::eigen_to_array<Tile,Policy>(Fp.world(), C_eig, tr_ao, tr_occ);
+  auto tr_occ = scf::tr_occupied(n_coeff_clusters_, nocc_);
+  C_occ = array_ops::eigen_to_array<Tile,Policy>(Fp.world(), C_occ_eig, tr_ao, tr_occ);
 
   // Get back to AO land
-  Cao("i,j") = M_inv_("k,i") * C("k,j");
-  Cao.truncate();
+  C_occ_ao("i,j") = M_inv_("k,i") * C_occ("k,j");
+  C_occ_ao.truncate();
+  if (!localize_) {
+    eps_ = eps;
+    auto nobs = eps.rows();
+    auto tr_obs = scf::tr_occupied(n_coeff_clusters_, nobs);
+    auto C = array_ops::eigen_to_array<Tile,Policy>(Fp.world(), C_eig, tr_ao, tr_obs);
+    C_("i,j") = M_inv_("k,i") * C("k,j");
+  }
   auto e1 = mpqc::fenced_now(world);
 
   // Compute D to full accuracy
-  D("i,j") = Cao("i,k") * Cao("j,k");
+  D("i,j") = C_occ_ao("i,k") * C_occ_ao("j,k");
   D.truncate();
   density_storages_.push_back(detail::array_storage(D));
 
   if (localize_) {
     auto l0 = mpqc::fenced_now(world);
-    auto U = mpqc::scf::BoysLocalization{}(Cao, r_xyz_ints_);
-    Cao("mu,i") = Cao("mu,k") * U("k,i");
+    auto U = mpqc::scf::BoysLocalization{}(C_occ_ao, r_xyz_ints_);
+    C_occ_ao("mu,i") = C_occ_ao("mu,k") * U("k,i");
     auto l1 = mpqc::fenced_now(world);
 
-    auto obs_ntiles = Cao.trange().tiles_range().extent()[0];
-    scf::clustered_coeffs(r_xyz_ints_, Cao, obs_ntiles);
+    auto obs_ntiles = C_occ_ao.trange().tiles_range().extent()[0];
+    scf::clustered_coeffs(r_xyz_ints_, C_occ_ao, obs_ntiles);
     auto c1 = mpqc::fenced_now(world);
     localization_times_.push_back(mpqc::duration_in_s(l0, l1));
     clustering_times_.push_back(mpqc::duration_in_s(l1, c1));
@@ -87,8 +97,8 @@ ESolveDensityBuilder<Tile,Policy>::operator()(
 
 #if TA_DEFAULT_POLICY == 1
   if (TcutC_ != 0) {
-    minimize_storage(Cao, TcutC_);
-    auto shape_c = Cao.shape();
+    minimize_storage(C_occ_ao, TcutC_);
+    auto shape_c = C_occ_ao.shape();
     auto &norms = shape_c.data();
     auto norm_mat = TA::eigen_map(norms, norms.range().extent_data()[0],
                                   norms.range().extent_data()[1]);
@@ -96,9 +106,9 @@ ESolveDensityBuilder<Tile,Policy>::operator()(
 #endif
 
   esolve_times_.push_back(mpqc::duration_in_s(e0, e1));
-  coeff_storages_.push_back(detail::array_storage(Cao));
+  coeff_storages_.push_back(detail::array_storage(C_occ_ao));
 
-  return std::make_pair(D, Cao);
+  return std::make_pair(D, C_occ_ao);
 }
 
 }  // namespace scf

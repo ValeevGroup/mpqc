95366d3960e32945de0c4eca1990f3efa3d3b802
Jinmei Zhang <jmzhang@vt.edu>
2017-02-21 14:33:19 -0800

    deleted all real PNO codes

--- src/mpqc/chemistry/qc/lcao/pno/ccsd_pno.h
+++ src/mpqc/chemistry/qc/lcao/pno/ccsd_pno.h
@@ -60,41 +60,6 @@ namespace lcao {
    double pno_simul();
    /// decompose T2 using PNO (eigen) decomposition or SVD
    void decom_t2(TA::DistArray<Tile, Policy> &t2_mp2, const DecomType = eigen_decom);
-   /// decompose T2 with MP2 and CCSD components
-   void decom_t2(TA::DistArray<Tile, Policy> &t2_mp2,
-       const TA::DistArray<Tile, Policy> &t2_ccsd);
-
-   /** compute PNO coefficients: d^ij_a a(ij)
-    * where each tile is the size of a * a(ij)
-    * and a(ij) represent PNO of pair ij
-    */
-   TA::DistArray<Tile, Policy>
-   compute_pno_coef(const TA::DistArray<Tile, Policy> &t2_mp2);
-   /** put the PNO coefficients in a vector indexed by ij pairs
-    * where each vector entry contain ab matrix
-    */
-   void comput_vec_pnocoef(const TA::DistArray<Tile, Policy>& t2_mp2,
-       const TA::TiledRange1& trange1_a);
-   /** construct vector<TArray> for ab^ij tensors (T2^ab_ij, g^ab_ij, and d^ab_ij)
-    * where in abij tensor tile is indexed by ij and has size a*b
-    * and each entry contains a reblocked ab matrix
-    */
-   void comput_vecij_ab(const TA::DistArray<Tile, Policy>& ab_ij,
-       const TA::TiledRange1& trange1_a,
-       std::vector<TA::DistArray<Tile, Policy>>& vecij_ab,
-       const bool compute_pno_coef = true);
-   /// compute PNO transformed F^a_b
-   void get_fab_pno(std::vector<TA::DistArray<Tile, Policy>>& vecij_fab);
-   /// compute PNO transformed G^ab_cd integrals
-   void get_abcd_pno(std::vector<TA::DistArray<Tile, Policy>>& vecij_gabcd);
-   /// compute PNO transformed T2^ab_ij amplitudes or G^ab_ij integrals
-   void get_abij_pno(const TA::DistArray<Tile, Policy>& ab_ij,
-       const TA::TiledRange1& trange1_a,
-       std::vector<TA::DistArray<Tile, Policy>>& vecij_pno);
-   /// perform CCD
-   double compute_ccd_df();
-   /// perform PNO CCD
-   double compute_ccd_pno();
 
   public:
    CCSD_PNO() = default;

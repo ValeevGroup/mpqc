13e2c4a8809e2770a9bc7c14f2a53da1e146d864
Xiao Wang <xiaowang862@gmail.com>
2017-05-25 18:04:33 -0400

    force shape of F to reduce cost

--- src/mpqc/chemistry/qc/lcao/scf/pbc/periodic_cadf_k_builder.h
+++ src/mpqc/chemistry/qc/lcao/scf/pbc/periodic_cadf_k_builder.h
@@ -5,6 +5,8 @@
 #include "mpqc/chemistry/qc/lcao/scf/builder.h"
 #include "mpqc/chemistry/qc/lcao/scf/pbc/periodic_three_center_contraction_builder.h"
 
+#include <unordered_set>
+
 namespace mpqc {
 namespace scf {
 
@@ -21,6 +23,7 @@ class PeriodicCADFKBuilder {
   PeriodicCADFKBuilder(Factory &ao_factory) : ao_factory_(ao_factory) {
     auto &world = ao_factory_.world();
     print_detail_ = ao_factory_.print_detail();
+    force_shape_threshold_ = 1.0e-12;
     mpqc::time_point t0, t1;
 
     // by-cluster orbital basis and df basis
@@ -29,11 +32,10 @@ class PeriodicCADFKBuilder {
 
     auto dcell = ao_factory_.unitcell().dcell();
     auto R_max = ao_factory_.R_max();
-    auto RJ_max = ao_factory_.RJ_max();
+    auto RJ_max = R_max;  // replace RJ range with R range
     auto RD_max = ao_factory_.RD_max();
     auto R_size = ao_factory_.R_size();
-    auto RJ_size = ao_factory_.RJ_size();
-    auto RD_size = ao_factory_.RD_size();
+    auto RJ_size = R_size;  // replace RJ range with R range
 
     const Vector3i ref_latt_range = {0, 0, 0};
     const Vector3i ref_latt_center = {0, 0, 0};
@@ -175,30 +177,14 @@ class PeriodicCADFKBuilder {
     t1 = mpqc::fenced_now(world);
     double t_E_ket = mpqc::duration_in_s(t0, t1);
 
-    // compute F(X, ν_R, σ_(Rj+Rd)) =
-    //           E(X, ν_R, σ_(Rj+Rd)) - M(X, Y) C(Y, ν_R, σ_(Rj+Rd))
-    t0 = mpqc::fenced_now(world);
-    {
-      if (F_.empty()) F_ = std::vector<array_type>(RJ_size, array_type());
-
-      for (auto RJ = 0; RJ != RJ_size; ++RJ) {
-        array_type &F = F_[RJ];
-        array_type &C = C_ket_[RJ];
-        DirectTArray &E = E_bra_[RJ];
-        F("X, nu, sig") = E("X, nu, sig") - M_("X, Y") * C("Y, nu, sig");
-      }
-    }
-    t1 = mpqc::fenced_now(world);
-    double t_F = mpqc::duration_in_s(t0, t1);
-
     if (print_detail_) {
       ExEnv::out0() << "\nCADF-K init time decomposition:\n"
                     << "\tC_bra:               " << t_C_bra << " s\n"
                     << "\tC_ket:               " << t_C_ket << " s\n"
                     << "\tM:                   " << t_M << " s\n"
                     << "\tE_bra:               " << t_E_bra << " s\n"
-                    << "\tE_ket:               " << t_E_ket << " s\n"
-                    << "\tF:                   " << t_F << " s" << std::endl;
+                    << "\tE_ket:               " << t_E_ket << " s"
+                    << std::endl;
     }
   }
 
@@ -211,6 +197,7 @@ class PeriodicCADFKBuilder {
  private:
   Factory &ao_factory_;
   bool print_detail_;
+  double force_shape_threshold_;
   std::unique_ptr<PTC_Builder> three_center_builder_;
 
   std::vector<array_type> C_bra_;
@@ -223,10 +210,44 @@ class PeriodicCADFKBuilder {
  private:
   array_type compute_K(const array_type &D, double target_precision) {
     auto &world = ao_factory_.world();
-    auto RJ_size = ao_factory_.RJ_size();
+    //    auto RJ_size = ao_factory_.RJ_size();
+    auto R_size = ao_factory_.R_size();
+    auto RJ_size = R_size;
 
     mpqc::time_point t0, t1;
     auto t0_k_builder = mpqc::fenced_now(world);
+
+    auto dump_shape = [](auto const &Q, int rj) {
+      std::cout << "Hi\n";
+      auto norms = Q.shape().data();
+      auto range = norms.range();
+      auto ext = range.extent_data();
+
+      Eigen::MatrixXd M(ext[0], ext[1] * ext[2]);
+      M.setZero();
+
+      for (auto X = 0; X < ext[0]; ++X) {
+        for (auto mu = 0; mu < ext[1]; ++mu) {
+          for (auto nu = 0; nu < ext[2]; ++nu) {
+            M(X, mu * ext[2] + nu) = norms(X, mu, nu);
+          }
+        }
+      }
+
+      std::ofstream outfile("Q_shape" + std::to_string(rj) + ".csv");
+      auto ncols = M.cols();
+      auto nrows = M.rows();
+      for (auto i = 0; i < nrows; ++i) {
+        for (auto j = 0; j < ncols; ++j) {
+          outfile << M(i, j);
+          if (j != ncols - 1) {
+            outfile << ", ";
+          } else {
+            outfile << "\n";
+          }
+        }
+      }
+    };
 
     // compute Q(X, μ_0, σ_(Rj+Rd)) = C(X, μ_0, ρ_Rj) D(ρ_0, σ_Rd)
     t0 = mpqc::fenced_now(world);
@@ -235,9 +256,38 @@ class PeriodicCADFKBuilder {
       array_type &Q = Q_bra[RJ];
       array_type &C = C_bra_[RJ];
       Q("X, mu, sig") = C("X, mu, rho") * D("rho, sig");
+      //      dump_shape(Q, RJ);
     }
     t1 = mpqc::fenced_now(world);
     double t_Q_bra = mpqc::duration_in_s(t0, t1);
+
+    t0 = mpqc::fenced_now(world);
+    array_type F, K_part1;
+    for (auto RJ = 0; RJ != RJ_size; ++RJ) {
+      array_type &C = C_ket_[RJ];
+      DirectTArray &E = E_bra_[RJ];
+
+      // force shape of F
+      auto norms = force_norms(Q_bra[RJ].shape().data(), 1, R_size);
+      auto trange = E.array().trange();
+      TA::SparseShape<float> forced_shape(world, norms, trange);
+
+      // compute F(X, ν_R, σ_(Rj+Rd)) =
+      //           E(X, ν_R, σ_(Rj+Rd)) - M(X, Y) C(Y, ν_R, σ_(Rj+Rd))
+      F("X, nu, sig") = (E("X, nu, sig")).set_shape(forced_shape);
+      F.truncate();
+      F("X, nu, sig") -= (M_("X, Y") * C("Y, nu, sig")).set_shape(forced_shape);
+
+      // compute K_part1
+      array_type &Q = Q_bra[RJ];
+      if (RJ == 0) {
+        K_part1("mu, nu") = Q("X, mu, sig") * F("X, nu, sig");
+      } else {
+        K_part1("mu, nu") += Q("X, mu, sig") * F("X, nu, sig");
+      }
+    }
+    t1 = mpqc::fenced_now(world);
+    double t_F_and_K1 = mpqc::duration_in_s(t0, t1);
 
     // compute Q(Y, ν_R, ρ_Rj) = C(Y, ν_R, σ_(Rj+Rd)) D(ρ_0, σ_Rd)
     t0 = mpqc::fenced_now(world);
@@ -250,17 +300,23 @@ class PeriodicCADFKBuilder {
     t1 = mpqc::fenced_now(world);
     double t_Q_ket = mpqc::duration_in_s(t0, t1);
 
+    // compute K_part2
+    t0 = mpqc::fenced_now(world);
+    array_type K_part2;
+    for (auto RJ = 0; RJ != RJ_size; ++RJ) {
+      if (RJ == 0) {
+        K_part2("mu, nu") = E_ket_[RJ]("Y, mu, rho") * Q_ket[RJ]("Y, nu, rho");
+      } else {
+        K_part2("mu, nu") += E_ket_[RJ]("Y, mu, rho") * Q_ket[RJ]("Y, nu, rho");
+      }
+    }
+    t1 = mpqc::fenced_now(world);
+    double t_K_part2 = mpqc::duration_in_s(t0, t1);
+
     // compute exchange
     t0 = mpqc::fenced_now(world);
     array_type K;
-    K("mu, nu") = Q_bra[0]("X, mu, sig") * F_[0]("X, nu, sig") +
-                  E_ket_[0]("Y, mu, rho") * Q_ket[0]("Y, nu, rho");
-    if (RJ_size > 1) {
-      for (auto RJ = 1; RJ != RJ_size; ++RJ) {
-        K("mu, nu") += Q_bra[RJ]("X, mu, sig") * F_[RJ]("X, nu, sig") +
-                       E_ket_[RJ]("Y, mu, rho") * Q_ket[RJ]("Y, nu, rho");
-      }
-    }
+    K("mu, nu") = K_part1("mu, nu") + K_part2("mu, nu");
     t1 = mpqc::fenced_now(world);
     double t_K = mpqc::duration_in_s(t0, t1);
 
@@ -270,12 +326,60 @@ class PeriodicCADFKBuilder {
     if (print_detail_) {
       ExEnv::out0() << "\nCADF-K time decomposition:\n"
                     << "\tQ_bra:                " << t_Q_bra << " s\n"
+                    << "\tF and Q F:            " << t_F_and_K1 << " s\n"
                     << "\tQ_ket:                " << t_Q_ket << " s\n"
-                    << "\tK = Q F - E Q:        " << t_K << " s\n"
+                    << "\tK_part2 = E Q:        " << t_K_part2 << " s\n"
+                    << "\tK = K1 + K2:          " << t_K << " s\n"
                     << "\nTotal K builder time: " << t_tot << " s" << std::endl;
     }
 
     return K;
+  }
+
+  TA::Tensor<float> force_norms(TA::Tensor<float> const &in_norms,
+                                int64_t const in_latt_range_size,
+                                int64_t const out_latt_range_size) {
+    auto &in_range = in_norms.range();
+    auto in_extent = in_range.extent_data();
+
+    TA::Range out_range;
+    out_range = TA::Range(std::array<size_t, 3>{
+        {in_extent[0], in_extent[1] / in_latt_range_size * out_latt_range_size,
+         in_extent[2]}});
+    auto out_extent = out_range.extent_data();
+    TA::Tensor<float> out_norms(out_range, 0.0);
+
+    std::unordered_set<int64_t> sig_sigma;
+    std::unordered_set<int64_t> sig_X;
+
+    sig_sigma.clear();
+    sig_X.clear();
+    for (auto X = 0ul; X < in_extent[0]; ++X) {
+      // For every ν, we determine save a list of important X and sigma
+      for (auto sigma = 0ul; sigma < in_extent[2]; ++sigma) {
+        for (auto mu = 0ul; mu < in_extent[1]; ++mu) {
+          const auto val = in_norms(X, mu, sigma);
+
+          if (val >= force_shape_threshold_) {
+            sig_X.insert(X);
+            sig_sigma.insert(sigma);
+            break;
+          }
+        }
+      }
+    }
+
+    for (auto nu = 0ul; nu < out_extent[1]; ++nu) {
+      // Then for every X we mark all important sigma. The output will have
+      // significant X sigma pairs that the input did not.
+      for (auto const &X : sig_X) {
+        for (auto const &sigma : sig_sigma) {
+          out_norms(X, nu, sigma) = std::numeric_limits<float>::max();
+        }
+      }
+    }
+
+    return out_norms;
   }
 
   norm_type<3> compute_eri3_norm(

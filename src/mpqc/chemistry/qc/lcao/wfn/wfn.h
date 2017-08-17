05cb559a3234ef42448cc840f67b191dce34935c
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-08-16 20:35:10 -0400

    dox + cleanup [skip ci]

--- src/mpqc/chemistry/qc/lcao/wfn/wfn.h
+++ src/mpqc/chemistry/qc/lcao/wfn/wfn.h
@@ -19,8 +19,7 @@ namespace mpqc {
 namespace lcao {
 
 /// Wavefunction computes a wave function (or a wave function-like quantity,
-/// like
-/// Green's function or reduced density matrix) in a Gaussian basis.
+/// like Green's function or reduced density matrix) in a Gaussian basis.
 
 /// \todo elaborate Wavefunction documentation
 class Wavefunction : public ::mpqc::Wavefunction {
@@ -29,18 +28,17 @@ class Wavefunction : public ::mpqc::Wavefunction {
   std::shared_ptr<WavefunctionWorld> wfn_world_;
 
  public:
+  // clang-format off
   /**
    *  \brief The KeyVal constructor
    *
    * The KeyVal object will be queried for the following keywords:
    * | Keyword | Type | Default| Description |
    * |---------|------|--------|-------------|
-   * | \c "wfn_world" OR \c "..:wfn_world" OR \c "$:wfn_world" |
-   * WavefunctionWorld | none | This specifies the WavefunctionWorld object in
-   * which this object will live initially. If not found, the contents of this
-   * KeyVal object will be used to construct a new WavefunctionWorld object |
+   * | \c "wfn_world" OR \c "..:wfn_world" OR \c "$:wfn_world" | WavefunctionWorld | none | This specifies the WavefunctionWorld object in which this object will live initially. If not found, the contents of this KeyVal object will be used to construct a new WavefunctionWorld object |
    *
    */
+  // clang-format on
   Wavefunction(const KeyVal& kv);
   virtual ~Wavefunction();
 

2fdc618b54e7c8b421b1e93698802fd251d1856e
varun rishi <rishi.iiser@gmail.com>
2017-08-22 19:34:51 -0400

    Attempt to code CCSDT-3, not finished yet. [skip ci]

---
+++ src/mpqc/chemistry/qc/lcao/cc/ccsdt3.cpp
@@ -0,0 +1,13 @@
+// CCSDT-3 by Varun Rishi, August 2017
+//
+
+#include "mpqc/chemistry/qc/lcao/cc/ccsdt1.h"
+#include "mpqc/util/keyval/forcelink.h"
+
+#if TA_DEFAULT_POLICY == 0
+template class mpqc::lcao::CCSDT3<TA::TensorD, TA::DensePolicy>;
+MPQC_CLASS_EXPORT2("CCSDT-3", mpqc::lcao::CCSDT3<TA::TensorD, TA::DensePolicy>);
+#elif TA_DEFAULT_POLICY == 1
+template class mpqc::lcao::CCSDT3<TA::TensorD, TA::SparsePolicy>;
+MPQC_CLASS_EXPORT2("CCSDT-3", mpqc::lcao::CCSDT3<TA::TensorD, TA::SparsePolicy>);
+#endif

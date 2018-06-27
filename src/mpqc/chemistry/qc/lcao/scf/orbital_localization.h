ValeevGroup/mpqc

commit 5b54105bdb9074538bd2c7878566d627c3a120a0

- Localizers can be constructed from KeyVal - mpqc::scf -> mpqc::lcao::scf (I do not believe there is anything in mpqc::scf that is representation independent) - KeyVal can throw is deprecated keywords are read, now default for validation tests

src/mpqc/chemistry/qc/lcao/scf/orbital_localization.h
@@ -2,18 +2,48 @@
 #ifndef MPQC4_SRC_MPQC_CHEMISTRY_QC_SCF_ORBITAL_LOCALIZATION_H_
 #define MPQC4_SRC_MPQC_CHEMISTRY_QC_SCF_ORBITAL_LOCALIZATION_H_
 
-#include <tiledarray.h>
-#include "mpqc/math/external/eigen/eigen.h"
-
 #include <cmath>
-
 #include <array>
 #include <iomanip>
 
+#include <tiledarray.h>
+
 #include "mpqc/math/external/eigen/eigen.h"
 #include "mpqc/math/tensor/clr/array_to_eigen.h"
+#include "mpqc/util/keyval/keyval.h"
+#include "mpqc/util/misc/assert.h"
 
 namespace mpqc {
+namespace lcao {
+
+/// Localizes orbitals using LCAO-specific info (e.g. AO-basis operators)
+template <typename Tile, typename Policy>
+class OrbitalLocalizer : public DescribedClass {
+ public:
+  /// @param[in] C input LCAOs
+  /// @param[in] ncols_of_C_to_skip the number of columns of C to keep
+  /// non-localized
+  /// @return transformation matrix U that converts C to localized LCAOs
+  virtual TA::DistArray<Tile, Policy> compute(
+      TA::DistArray<Tile, Policy> const &C,
+      size_t ncols_of_C_to_skip = 0) const = 0;
+
+  /// this must be called before compute()
+  OrbitalLocalizer &initialize(TA::DistArray<Tile, Policy> S_ao,
+                               std::vector<TA::DistArray<Tile, Policy>> mu_ao) {
+    ao_s_ = array_ops::array_to_eigen(S_ao);
+    ao_x_ = array_ops::array_to_eigen(mu_ao[0]);
+    ao_y_ = array_ops::array_to_eigen(mu_ao[1]);
+    ao_z_ = array_ops::array_to_eigen(mu_ao[2]);
+    initialized_ = true;
+    return *this;
+  }
+
+ protected:
+  array_ops::Matrix<typename Tile::value_type> ao_s_, ao_x_, ao_y_, ao_z_;
+  bool initialized_ = false;
+};
+
 namespace scf {
 
 using Mat =
@@ -32,29 +62,42 @@ void jacobi_sweeps(Mat &Cm, Mat &U, std::vector<Mat> const &ao_xyz,
 
 /// Performs Foster-Boys localization
 /// (see J. Foster and S. Boys, Rev Mod Phys 32, 300 (1960)).
-class FosterBoysLocalization {
+template <typename Tile, typename Policy>
+class FosterBoysLocalizer : public OrbitalLocalizer<Tile, Policy> {
  public:
+  // clang-format off
+  /**
+   * KeyVal constructor for FosterBoysLocalizer
+   *
+   * @param kv the KeyVal object; it will be queried for all keywords of AOWavefunction as well as the following additional keywords:
+   * | Keyword | Type | Default| Description |
+   * |---------|------|--------|-------------|
+   * | @c convergence | double | 1e-8 | the Jacobi solver is converged when the maximum rotation angle (in rad) does not exceed this value  |
+   * | @c max_iter | int | 50 | the maximum number of Jacobi iterations |
+   */
+  // clang-format on
+  FosterBoysLocalizer(const KeyVal &kv)
+      : jacobi_convergence_threshold_(kv.value<double>("convergence", 1e-8, KeyVal::is_nonnegative)),
+        jacobi_max_iter_(kv.value<std::size_t>("max_iter", 50)) {}
+
   /// @param C input LCAOs
   /// @param {x,y,z} electric dipole operator matrices, in AO basis
   /// @param[in] ncols_of_C_to_skip the number of columns of C to keep
   ///            non-localized, presumably because they are already localized
   /// @return transformation matrix U that converts C to localized LCAOs
-  template <typename Tile, typename Policy>
-  TA::DistArray<Tile, Policy> operator()(
+  TA::DistArray<Tile, Policy> compute(
       TA::DistArray<Tile, Policy> const &C,
-      std::vector<TA::DistArray<Tile, Policy>> const &r_ao,
-      size_t ncols_of_C_to_skip = 0) const {
-    auto ao_x = array_ops::array_to_eigen(r_ao[0]);
-    auto ao_y = array_ops::array_to_eigen(r_ao[1]);
-    auto ao_z = array_ops::array_to_eigen(r_ao[2]);
+      size_t ncols_of_C_to_skip = 0) const override {
+    MPQC_ASSERT(this->initialized_);
     auto c_eig = array_ops::array_to_eigen(C);
 
     auto trange = C.trange();
     return array_ops::eigen_to_array<Tile, Policy>(
-        C.world(), (*this)(c_eig, ao_x, ao_y, ao_z, ncols_of_C_to_skip),
-        trange.data()[1], trange.data()[1]);
+        C.world(), (*this)(c_eig, this->ao_x_, this->ao_y_, this->ao_z_, ncols_of_C_to_skip),
+        trange.data()[0], trange.data()[1]);
   }
 
+ private:
   /// @param[in,out] C on input: LCAO coefficients, on output: localized LCAO
   /// coefficients
   /// @param {x,y,z} electric dipole operator matrices, in AO basis
@@ -70,33 +113,33 @@ class FosterBoysLocalization {
         C.block(0, ncols_of_C_to_skip, C.rows(), C.cols() - ncols_of_C_to_skip);
 
     EigMat U_loc = EigMat::Identity(C_loc.cols(), C_loc.cols());
-    jacobi_sweeps(C_loc, U_loc, {ao_x, ao_y, ao_z});
+    jacobi_sweeps(C_loc, U_loc, {ao_x, ao_y, ao_z},
+                  jacobi_convergence_threshold_, jacobi_max_iter_);
 
     EigMat U = EigMat::Identity(C.cols(), C.cols());
     U.block(ncols_of_C_to_skip, ncols_of_C_to_skip, U_loc.rows(),
             U_loc.cols()) = U_loc;
     return U;
   }
+
+  double jacobi_convergence_threshold_;
+  size_t jacobi_max_iter_;
 };
 
 /// Performs Rank Revealing QR localization
 /// (see Damle, A. et al., J. Chem. Theory Comput. 2015, 11 (4), 1463.)
-class RRQRLocalization {
+template <typename Tile, typename Policy>
+class RRQRLocalizer : public OrbitalLocalizer<Tile,Policy> {
  public:
-  /// @param[in] C Input LCAOs
-  /// @param[in] S Overlap matrix
-  /// @param[in] ncols_of_C_to_skip the number of columns of C to keep
-  /// non-localized
-  /// @return transformation matrix U that converts C to localized LCAOs
-  template <typename Tile, typename Policy>
-  TA::DistArray<Tile, Policy> operator()(TA::DistArray<Tile, Policy> &C,
-                                         TA::DistArray<Tile, Policy> const &S,
-                                         size_t ncols_of_C_to_skip = 0) const {
+  RRQRLocalizer(const KeyVal &) {}
+
+  TA::DistArray<Tile, Policy> compute(TA::DistArray<Tile, Policy> const &C,
+                                      size_t ncols_of_C_to_skip = 0) const override {
+    MPQC_ASSERT(this->initialized_);
     auto c_eig = array_ops::array_to_eigen(C);
-    auto s_eig = array_ops::array_to_eigen(S);
     auto trange = C.trange();
     return array_ops::eigen_to_array<Tile, Policy>(
-        C.world(), (*this)(c_eig, s_eig, ncols_of_C_to_skip), trange.data()[1],
+        C.world(), (*this)(c_eig, this->ao_s_, ncols_of_C_to_skip), trange.data()[0],
         trange.data()[1]);
   }
 
@@ -128,6 +171,7 @@ class RRQRLocalization {
 };
 
 }  // namespace scf
+}  // namespace lcao
 }  // namespace mpqc
 
 #endif  // MPQC4_SRC_MPQC_CHEMISTRY_QC_SCF_ORBITAL_LOCALIZATION_H_

src/bin/mpqc/mpqc_init.cpp
@@ -292,6 +292,8 @@ std::shared_ptr<GetLongOpt> make_options() {
   options->enroll("u", GetLongOpt::MandatoryValue, "the units system");
   options->enroll("d", GetLongOpt::NoValue,
                   "start the program and attach a debugger");
+  options->enroll("t", GetLongOpt::NoValue,
+                  "throw if a deprecated keyword is read");
   options->enroll("D", GetLongOpt::OptionalValue,
                   " if \"debugger\" keyword is not given, create a default "
                   "debugger; an optional JSON string can be given to provide "
@@ -363,6 +365,10 @@ std::tuple<std::string, std::string> process_options(
       ExEnv::out0() << indent << entry.first << std::endl;
     }
     std::exit(0);
+  }
+
+  if (options->retrieve("t")) {
+    KeyVal::set_throw_if_deprecated_path(true);
   }
 
   // get input file name ... can be given as the last option

src/bin/mpqc/mpqc_init.h
@@ -162,6 +162,7 @@ to_string(MPQCInit::InputFormat f) {
     | <tt>-k</tt>     | no           | print all registered (KeyVal-constructible) DescribedClass classes |
     | <tt>-h</tt>     | no           | print the usage info and exit                         |
     | <tt>-d</tt>     | no           | start the program and attach a debugger               |
+    | <tt>-t</tt>     | no           | throw if a deprecated keyword is read                 |
 
     \return a smart pointer to the newly created options parser object (see mpqc::GetLongOpt )
     */

src/bin/mpqc/mpqcrunning.dox
@@ -34,6 +34,7 @@ different environments, as well as optimization hints:
 | <tt>-k</tt>     | no           | print all registered (KeyVal-constructible) DescribedClass classes |
 | <tt>-h</tt>     | no           | print the usage info and exit                         |
 | <tt>-d</tt>     | no           | start the program and attach a debugger               |
+| <tt>-t</tt>     | no           | throw if a deprecated keyword is read                 |
 
 \subsection mpqccomlineexamples MPQC Usage Examples
 

src/mpqc/chemistry/qc/lcao/scf/builder.h
@@ -10,12 +10,13 @@
 #include "mpqc/math/external/eigen/eigen.h"
 
 namespace mpqc {
+namespace lcao {
 namespace scf {
 
 /// FockBuilder computes the (2-e part) of the Fock matrix using either the
 /// (1-particle) density matrix
 /// or the occupied orbital AO coefficients.
-template <typename Tile, typename Policy>
+template<typename Tile, typename Policy>
 class FockBuilder {
  public:
   using array_type = TA::DistArray<Tile, Policy>;
@@ -40,7 +41,7 @@ class FockBuilder {
  * PeriodicFockBuilder computes the (2-e part of) the Fock matrix in periodic HF
  * using the (1-particle) density matrix
  */
-template <typename Tile, typename Policy>
+template<typename Tile, typename Policy>
 class PeriodicFockBuilder {
  public:
   using array_type = TA::DistArray<Tile, Policy>;
@@ -71,6 +72,7 @@ class PeriodicFockBuilder {
 };
 
 }  // namespace scf
+}  // namespace lcao
 }  // namespace mpqc
 
 #endif  // MPQC4_SRC_MPQC_CHEMISTRY_QC_SCF_BUILDER_H_

src/mpqc/chemistry/qc/lcao/scf/cadf_builder.h
@@ -17,9 +17,10 @@
 #include <vector>
 
 namespace mpqc {
+namespace lcao {
 namespace scf {
 
-template <typename Tile, typename Policy, typename DirectArray>
+template<typename Tile, typename Policy, typename DirectArray>
 class CADFFockBuilder : public FockBuilder<Tile, Policy> {
  public:
   using ArrayType = TA::DistArray<Tile, Policy>;
@@ -31,7 +32,7 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
   ArrayType C_;          // CADF fitting coeffs
 
   // seCADF builder
-  std::unique_ptr<ExactKDiagonalBuilder<Tile, Policy>> exactK_;
+  std::unique_ptr<ExactKDiagonalBuilder < Tile, Policy>> exactK_;
 
   float force_threshold_ = 0.0;
   double LMO_chop_threshold_ = 0.0;
@@ -90,7 +91,7 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
  public:
   using BasisFactory = lcao::gaussian::Basis::Factory;
 
-  template <typename Factory>
+  template<typename Factory>
   CADFFockBuilder(Factory &ao_factory, double force_threshold,
                   double lmo_chop_threshold, bool do_secadf)
       : force_threshold_(force_threshold),
@@ -110,7 +111,8 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
       auto screen_threshold = ao_factory_ref.screen_threshold();
       auto obs = ao_factory.basis_registry()->retrieve(L"κ");
 
-      exactK_ = std::make_unique<ExactKDiagonalBuilder<Tile, Policy>>(
+      exactK_ = std::make_unique<ExactKDiagonalBuilder < Tile, Policy>>
+      (
           world, obs, obs, obs, screen, screen_threshold);
     }
 
@@ -133,7 +135,7 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
 
   ArrayType operator()(ArrayType const &D, ArrayType const &LMO,
                        double target_precision =
-                           std::numeric_limits<double>::epsilon()) override {
+                       std::numeric_limits<double>::epsilon()) override {
     ArrayType G;
     G("m, n") =
         2 * compute_J(D)("m, n") - compute_K(LMO, D, target_precision)("m, n");
@@ -158,8 +160,8 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
     auto j0 = mpqc::fenced_now(world);
     ArrayType J;
     J("mu, nu") = E_("X, mu, nu") *
-                  (Mchol_inv_("Z, X") *
-                   (Mchol_inv_("Z, Y") * (E_("Y, sig, rho") * D("sig, rho"))));
+        (Mchol_inv_("Z, X") *
+            (Mchol_inv_("Z, Y") * (E_("Y, sig, rho") * D("sig, rho"))));
     auto j1 = mpqc::fenced_now(world);
     j_times_.push_back(mpqc::duration_in_s(j0, j1));
 
@@ -170,6 +172,8 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
 
   ArrayType compute_K(ArrayType const &LMO_in, ArrayType const &D,
                       double target_precision) {
+    using ::mpqc::detail::array_storage;
+
     auto &world = M_.world();
     auto Exch0 = mpqc::fenced_now(world);
     ArrayType L, K;  // Matrices
@@ -192,7 +196,7 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
       auto chop1 = mpqc::fenced_now(world);
       LMO_chop_times_.push_back(mpqc::duration_in_s(chop0, chop1));
 
-      auto chop_sizes = detail::array_storage(LMO);
+      auto chop_sizes = array_storage(LMO);
       LMO_chopped_sizes_.push_back(
           std::array<double, 2>{{chop_sizes[0], chop_sizes[1]}});
     }
@@ -204,7 +208,7 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
     auto z1 = mpqc::fenced_now(world);
     Z_times_.push_back(mpqc::duration_in_s(z0, z1));
 
-    auto Z_sizes = detail::array_storage(Z);
+    auto Z_sizes = array_storage(Z);
     Z_sizes_.push_back(std::array<double, 2>{{Z_sizes[0], Z_sizes[1]}});
 
     // Get forced output shape
@@ -268,7 +272,7 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
     auto f1 = mpqc::fenced_now(world);
     F_times_.push_back(mpqc::duration_in_s(f0, f1));
 
-    auto F_sizes = detail::array_storage(F);
+    auto F_sizes = array_storage(F);
     F_sizes_.push_back(std::array<double, 2>{{F_sizes[0], F_sizes[1]}});
 
     // Construct L
@@ -323,6 +327,7 @@ class CADFFockBuilder : public FockBuilder<Tile, Policy> {
 };
 
 }  // namespace scf
+}  // namespace lcao
 }  // namespace mpqc
 
 #endif  // MPQC4_SRC_MPQC_CHEMISTRY_QC_SCF_CADF_BUILDER_H_

src/mpqc/chemistry/qc/lcao/scf/cadf_builder_forced_shape.h
@@ -19,6 +19,7 @@
 #include <vector>
 
 namespace mpqc {
+namespace lcao {
 namespace scf {
 
 class CADFForcedShapeFockBuilder : public FockBuilder {
@@ -47,7 +48,7 @@ class CADFForcedShapeFockBuilder : public FockBuilder {
   std::vector<std::array<double, 3>> f_df_storages_;
 
  public:
-  template <typename Integral>
+  template<typename Integral>
   CADFForcedShapeFockBuilder(array_type const &M, Integral const &eri3,
                              darray_type const &C_df, darray_type const &G_df,
                              double clr_thresh, double j_clr_thresh)
@@ -97,7 +98,7 @@ class CADFForcedShapeFockBuilder : public FockBuilder {
     }
   }
 
-  ~CADFForcedShapeFockBuilder() { }
+  ~CADFForcedShapeFockBuilder() {}
 
   array_type operator()(array_type const &D, array_type const &C) override {
     array_type G;
@@ -125,7 +126,7 @@ class CADFForcedShapeFockBuilder : public FockBuilder {
                 << leader << "\tdL to K time: " << dl_to_k_times_.back()
                 << "\n";
       auto total_k_time = c_mo_times_.back() + f_df_times_.back() +
-                          dl_times_.back() + dl_to_k_times_.back();
+          dl_times_.back() + dl_to_k_times_.back();
       std::cout << leader << "\ttotal K time: " << total_k_time << "\n";
     }
   }
@@ -192,6 +193,7 @@ class CADFForcedShapeFockBuilder : public FockBuilder {
 };
 
 }  // namespace scf
+}  // namespace lcao
 }  // namespace mpqc
 
 #endif  // MPQC4_SRC_MPQC_CHEMISTRY_QC_SCF_CADF_BUILDER_FORCED_SHAPE_H_

src/mpqc/chemistry/qc/lcao/scf/cadf_fitting_coeffs.h
@@ -15,6 +15,7 @@
 #include <unordered_map>
 
 namespace mpqc {
+namespace lcao {
 namespace scf {
 
 namespace cadf {
@@ -427,6 +428,7 @@ compute_atomic_fitting_coeffs(
 }
 
 }  // namespace scf
+}  // namespace lcao
 }  // namespace mpqc
 
 #endif  // MPQC4_SRC_MPQC_CHEMISTRY_QC_SCF_CADF_FITTING_COEFFS_H_

commit e03899b3e7b43a44ec134d61566be52948ce748d

fixed silly bug introduced in 5b54105bdb9074538bd2c7878566d627c3a120a0

src/mpqc/chemistry/qc/lcao/scf/orbital_localization.h
@@ -96,7 +96,7 @@ class FosterBoysLocalizer : public OrbitalLocalizer<Tile, Policy> {
     auto trange = C.trange();
     return array_ops::eigen_to_array<Tile, Policy>(
         C.world(), (*this)(c_eig, this->ao_x_, this->ao_y_, this->ao_z_, ncols_of_C_to_skip),
-        trange.data()[0], trange.data()[1]);
+        trange.data()[1], trange.data()[1]);
   }
 
  private:
@@ -147,7 +147,7 @@ class RRQRLocalizer : public OrbitalLocalizer<Tile,Policy> {
     auto c_eig = array_ops::array_to_eigen(C);
     auto trange = C.trange();
     return array_ops::eigen_to_array<Tile, Policy>(
-        C.world(), (*this)(c_eig, this->ao_s_, ncols_of_C_to_skip), trange.data()[0],
+        C.world(), (*this)(c_eig, this->ao_s_, ncols_of_C_to_skip), trange.data()[1],
         trange.data()[1]);
   }
 
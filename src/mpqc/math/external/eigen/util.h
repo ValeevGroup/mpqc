e189d4952a039a4badd01cd2837ffad5ed7fd53c
Eduard Valeyev <eduard@valeyev.net>
2018-07-13 15:11:05 -0400

    added formatting utils for Eigen matrices

--- src/mpqc/math/external/eigen/util.h
+++ src/mpqc/math/external/eigen/util.h
@@ -15,6 +15,27 @@ namespace detail {
  */
 void sort_eigen(VectorZ &eigVal, MatrixZ &eigVec);
 
+template <typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+auto format_commainit(const Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>& matrix,
+                      int precision = -1, bool align_cols = true) {
+  return matrix.format(Eigen::IOFormat(precision > 0 ? precision : Eigen::FullPrecision,
+                                       align_cols ? 0 : Eigen::DontAlignCols, ", ", ", ", "", "", " << ", ";"));
+}
+
+template <typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+void write_commainit(std::ostream& os,
+                     const Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>& matrix,
+                     const std::string name = "matrix",
+                     int precision = -1, bool align_cols = true) {
+  os << name << "(" << matrix.rows() << "," << matrix.cols() << "); "
+     << name << format_commainit(matrix, precision, align_cols);
+}
+
+template <typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+auto format_cpp(const Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>& matrix) {
+  return matrix.format(Eigen::IOFormat(Eigen::FullPrecision, 0, ", ", ",\n", "{", "}", "{", "}"));
+}
+
 }  // namespace detail
 }  // namespace mpqc
 

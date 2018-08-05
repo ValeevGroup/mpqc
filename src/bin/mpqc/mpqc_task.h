b33bd11ed92cb362cfa31d148743dbcb14a26020
Eduard Valeyev <eduard@valeyev.net>
2018-08-05 14:57:42 -0400

    factored out implementation from mpqc_task.h to reduce header coupling

--- src/bin/mpqc/mpqc_task.h
+++ src/bin/mpqc/mpqc_task.h
@@ -13,8 +13,6 @@
 #include <madness/world/world.h>
 
 #include "mpqc/util/keyval/keyval.h"
-#include "mpqc/chemistry/qc/lcao/wfn/wfn.h"
-#include "mpqc/chemistry/qc/properties/property.h"
 
 namespace mpqc {
 
@@ -23,33 +21,13 @@ namespace mpqc {
 /// A computation is specified by a KeyVal object and a World object
 class MPQCTask {
  public:
-  MPQCTask(madness::World &world, std::shared_ptr<KeyVal> kv)
-      : world_(world), keyval_(kv) {}
-  ~MPQCTask() { }
+  MPQCTask(madness::World &world, std::shared_ptr<KeyVal> kv);
+  ~MPQCTask();
 
-  madness::World& world() const {
-    return world_;
-  }
-  const std::shared_ptr<KeyVal>& keyval() const {
-    return keyval_;
-  }
+  madness::World& world() const;
+  const std::shared_ptr<KeyVal>& keyval() const;
 
-  void run() {
-    auto world_popper = TA::push_default_world(world_);
-
-    // set the sparse_threshold
-    const double threshold = keyval_->value<double>("sparse_threshold", 1e-20);
-    TiledArray::SparseShape<float>::threshold(threshold);
-
-    auto property = keyval_->class_ptr<Property>("property");
-    if (property != nullptr) {
-      property->evaluate();
-      auto kv_prop = keyval_->keyval("property");
-      property->write(kv_prop);
-    } else {
-      throw InputError("invalid property", __FILE__, __LINE__, "property");
-    }
-  }
+  void run();
 
  private:
   madness::World& world_;
